97c7004da49738791be0193603e474404f45e7e3
Ilya Kozhevnikov <ilya.kozhevnikov@gmail.com>
2012-02-14 16:48:01 +0000

    jira attribute

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.BankGateway/HsbcTests.cs
@@ -0,0 +1,16 @@
+﻿using MbUnit.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.BankGateway
+{
+    [Parallelizable(TestScope.All)]
+    public class HsbcTests
+    {
+        [Test, AUT(AUT.Uk), JIRA("UK-494")]
+        public void SortCodeUpdates()
+        {
+
+        }
+    }
+}
---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Core/JIRAAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Gallio.Framework.Pattern;
+
+namespace Wonga.QA.Tests.Core
+{
+    [AttributeUsage(PatternAttributeTargets.TestComponent, AllowMultiple = true)]
+    public class JIRAAttribute : MetadataPatternAttribute
+    {
+        private String _issue;
+
+        public JIRAAttribute(String issue)
+        {
+            _issue = issue;
+        }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetMetadata()
+        {
+            yield return new KeyValuePair<String, String>("Issue", _issue);
+        }
+    }
+}
