6f2bfd53c25bc5a6ea07172a9a8f7805ae84c7a6
kostaswonga <konstantinos.konstantinidis@wonga.com>
2012-02-15 13:46:37 +0000

    Regenerating the API

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Requests/GetBusinessAccountSummaryWbUkQuery.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Requests/GetBusinessAccountSummaryWbUkQuery.cs
@@ -6,7 +6,6 @@ namespace Wonga.QA.Framework.Api
     [XmlRoot("GetBusinessAccountSummary")]
     public partial class GetBusinessAccountSummaryWbUkQuery : ApiRequest<GetBusinessAccountSummaryWbUkQuery>
     {
-        public Object ApplicationId { get; set; }
         public Object AccountId { get; set; }
     }
 }
