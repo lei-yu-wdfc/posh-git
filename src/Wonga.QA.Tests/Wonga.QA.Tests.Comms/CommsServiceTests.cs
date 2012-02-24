ae10af5fa74e35500449a32c2263324998ac94ec
Gennady Kostrov <gena.kostrov@gmail.com>
2012-02-24 15:27:22 +0200

    UK-583 merged into Unignored Secci generation test.

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Comms/CommsServiceTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Comms/CommsServiceTests.cs
@@ -1,5 +1,11 @@
-﻿using MbUnit.Framework;
+﻿using System;
+using System.Linq;
+using MbUnit.Framework;
 using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.Msmq;
+using Wonga.QA.Framework.Msmq.Payments;
 using Wonga.QA.Tests.Core;
 
 namespace Wonga.QA.Tests.Comms
@@ -7,10 +13,35 @@ namespace Wonga.QA.Tests.Comms
     [Parallelizable(TestScope.All)]
     public class CommsServiceTests
     {
-        [Test]
+        [Test, AUT]
         public void CommsServiceIsRunning()
         {
             Assert.IsTrue(Driver.Svc.Comms.IsRunning());
         }
+
+		[Test, AUT(AUT.Uk)]
+		public void LoanExtensionSecciEmail()
+		{
+			Customer cust = CustomerBuilder.New().Build();
+			Application app = ApplicationBuilder.New(cust)
+				.WithExpectedDecision(ApplicationDecisionStatusEnum.Accepted)
+				.Build();
+			var ftApp = Driver.Db.Payments.FixedTermLoanApplications.Single(a => a.ApplicationEntity.ExternalId == app.Id);
+			Assert.IsTrue(Driver.Svc.DocumentGeneration.IsRunning());
+			Assert.IsTrue(Driver.Svc.Payments.IsRunning());
+			Driver.Msmq.Payments.Send(new ExtendLoanStartedInternalCommand
+			                           	{
+			                           		AccountId = cust.Id, 
+											ApplicationId = app.Id, 
+											PartPaymentRequired = 10m,
+											ExtendDate = ftApp.NextDueDate ?? ftApp.PromiseDate,
+											ExtensionId = Data.GetId(),
+											NewFinalBalance = ftApp.LoanAmount
+			                           	});
+
+			var secciDocument = Do.Until(() => Driver.Db.Comms.LegalDocuments.Single(ld => ld.ApplicationId == app.Id && ld.DocumentType == 2));//ExtensionSeccii
+			
+		}
+
     }
 }
