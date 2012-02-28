ad2a29f3cf02343cd3f62c1a80b66c4715ab2c21
Alex Sloat <alex.sloat@wonga.com>
2012-02-28 13:37:00 +0000

    UK-795 Added test for GetAccountSummary, ignored until UK-937 is completed to get the correct amount.

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/GetApplicationSummaryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Payments
+{
+	public class GetApplicationSummaryTests
+	{
+		[Test, AUT(AUT.Uk), JIRA("UK-795")]
+		[Ignore]
+		public void GetAccountSummaryTest()
+		{
+			Customer customer = CustomerBuilder.New().Build();
+			Do.Until(customer.GetBankAccount);
+			Do.Until(customer.GetPaymentCard);
+			ApplicationBuilder.New(customer).Build();
+
+			var response = Driver.Api.Queries.Post(new GetAccountSummaryQuery {AccountId = customer.Id});
+			//£100 loan for 10 days.
+			Assert.AreEqual(115.91M, decimal.Parse(response.Values["CurrentLoanRepaymentAmountOnDueDate"].Single()));
+			Assert.AreEqual(DateTime.Today.AddDays(10), DateTime.Parse(response.Values["CurrentLoanDueDate"].Single()));
+		}
+	}
+}
