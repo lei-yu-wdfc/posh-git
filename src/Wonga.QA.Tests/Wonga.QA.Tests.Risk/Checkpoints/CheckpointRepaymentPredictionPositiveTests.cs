867859fe96e5e1c50b228b02ba43fa6d65c62bf2
Adrian Murphy <adrian.murphy@wonga.com>
2012-02-28 11:24:41 +0000

    Skeleton tests for Scorecard

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointRepaymentPredictionPositiveTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointRepaymentPredictionPositiveTests.cs
@@ -7,6 +7,7 @@ using Wonga.QA.Framework;
 using Wonga.QA.Framework.Api;
 using Wonga.QA.Framework.Core;
 using Wonga.QA.Framework.Db;
+using Wonga.QA.Framework.Msmq;
 using Wonga.QA.Tests.Core;
 using Wonga.QA.Tests.Risk.Enums;
 
@@ -22,27 +23,29 @@ namespace Wonga.QA.Tests.Risk.Checkpoints
 		[Test, AUT(AUT.Za)]
 		public void CheckpointRepaymentPredictionPositiveL0Accept()
 		{
+			var customer = CustomerBuilder.New().WithEmployer(TestMask).Build();
+			var application = ApplicationBuilder.New(customer).Build();
+
+			var repaymentPredictionScore = GetRepaymentPredictionScore(application);
+			Assert.GreaterThan(ScoreCutoffNewUsers, repaymentPredictionScore);
 		}
 
 		[Test, AUT(AUT.Za)]
 		public void CheckpointRepaymentPredictionPositiveL0Decline()
 		{
 			var customer = CustomerBuilder.New().WithEmployer(TestMask).Build();
-			var application =
-				ApplicationBuilder.New(customer)
-				.Build();
+			var application = ApplicationBuilder.New(customer).WithLoanTerm(30).WithLoanAmount(2000).WithExpectedDecision(ApplicationDecisionStatusEnum.Declined).Build();
 
-			var db = new DbDriver();
-			var repaymentPredictionScore = (from ra in db.Risk.RiskApplications
-											join dd in db.Risk.RiskDecisionDatas
-												on ra.RiskApplicationId equals dd.RiskApplicationId
-											where ra.ApplicationId == application.Id
-											select dd.ValueDouble).First();
+			var repaymentPredictionScore = GetRepaymentPredictionScore(application);
+			Assert.LessThan(ScoreCutoffNewUsers, repaymentPredictionScore);
 		}
 
 		[Test, AUT(AUT.Za)]
 		public void CheckpointRepaymentPredictionPositiveLnAccept()
 		{
+			var customer = CustomerBuilder.New().WithEmployer("test:IsEmployed").Build();
+			ApplicationBuilder.New(customer).Build().Repay();
+
 		}
 
 		[Test, AUT(AUT.Za)]
@@ -52,6 +55,21 @@ namespace Wonga.QA.Tests.Risk.Checkpoints
 
 
 		#region Helpers
+
+		private double GetRepaymentPredictionScore(Application application)
+		{
+			var db = new DbDriver();
+			return (double)(from ra in db.Risk.RiskApplications
+					join dd in db.Risk.RiskDecisionDatas
+						on ra.RiskApplicationId equals dd.RiskApplicationId
+					where ra.ApplicationId == application.Id
+					select dd.ValueDouble).First();
+		}
+
+		private void AssertFactorsStoredInTable(Application application)
+		{
+			
+		}
 
 		#endregion
 	}
