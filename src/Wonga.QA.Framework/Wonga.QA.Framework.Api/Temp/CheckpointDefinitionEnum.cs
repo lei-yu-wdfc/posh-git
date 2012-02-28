79078bc05e46421d58abebb1b6fdc07f9bb3cf49
Pedro Silva <pedro.silva@wonga.com>
2012-02-28 18:32:04 +0000

    keeping checkpoint definition as same in DB

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Temp/CheckpointDefinitionEnum.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Temp/CheckpointDefinitionEnum.cs
@@ -12,7 +12,7 @@ namespace Wonga.QA.Framework.Api
         [Description("CIFAS fraud check")] CIFASFraudCheck,
         [Description("Hardware blacklist check")] Applicationdatablacklistcheck,
         [Description("Fraud list check")] FraudListCheck,
-        [Description("Bank account is valid")] BankAccountMatchesTheApplicant,
+		[Description("Bank account matches the applicant")]BankAccountMatchesTheApplicant,
         [Description("Credit bureau data is available")] CreditBureauDataIsAvailable,
         [Description("Date of birth is correct")] DateOfBirthIsCorrect,
         [Description("Applicant history is acceptable")] ApplicantHistoryIsAcceptable,
