5e83b1bdf6cd5e247a9e46a7ab7d4b23e4ef0cc6
Ivan Goor <ivan.goor@wonga.com>
2012-03-01 11:04:03 +0000

    UK-935 Create Acceptance Tests to verify data has been pushed to SF

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Salesforce/SalesforcePushFixedTermLoanApplicationDataTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Salesforce
+{
+	[TestFixture, AUT(AUT.Uk), Parallelizable(TestScope.All)]
+	public class SalesforcePushFixedTermLoanApplicationDataTest
+	{
+		[Test, Ignore("needs work.")]
+        [AUT(AUT.Uk), JIRA("UK-808")]
+		[Description("Verify that the application data has been pushed to SF")]
+		public void FixedTermLoanApplicationDataPushedToSalesforce()
+		{
+			Customer customer = CustomerBuilder.New().Build();
+			Do.Until(customer.GetBankAccount);
+			Do.Until(customer.GetPaymentCard);
+			var app = ApplicationBuilder.New(customer).Build();
+
+			// query saleforce to see if application exists.
+			var salesforce = Driver.ThirdParties.Salesforce;
+
+			var sfUsername = Driver.Db.Ops.ServiceConfigurations.Single(sc => sc.Key == "Salesforce.UserName");
+			var sfPassword = Driver.Db.Ops.ServiceConfigurations.Single(sc => sc.Key == "Salesforce.Password");
+			var sfUrl = Driver.Db.Ops.ServiceConfigurations.Single(sc => sc.Key == "Salesforce.Url");
+
+			salesforce.SalesforceUsername = sfUsername.Value;
+			salesforce.SalesforcePassword = sfPassword.Value;
+			salesforce.SalesforceUrl = sfUrl.Value;
+
+			//Do.Until(() => salesforce.ApplicationExists(app.Id));
+			//Assert.IsTrue(salesforce.ApplicationExists(app.Id));
+			Assert.IsTrue(salesforce.ApplicationExists(Guid.Parse("f505d320-50fd-48a6-9915-95af4ad8072d")));
+		}
+	}
+}
