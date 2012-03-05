671ce7972eae7aa99b352c710bf453a2911741e2
Charlie Barker <charlie.barker@wonga.com>
2012-03-05 14:53:22 +0000

    UK-823 Added Test for GetAccountOptions Scenario1

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Queries/GetAccountOptionsTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Queries/GetAccountOptionsTests.cs
@@ -1,11 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
 
 namespace Wonga.QA.Tests.Payments.Queries
 {
-    class GetAccountOptionsTests
+    [Parallelizable]
+    public class GetAccountOptionsTests
     {
+        [Test, AUT(AUT.Uk), JIRA("UK-823"),]
+        public void Scenario1ExistingCustomerWithoutLiveLoan()
+        {
+            Customer customer = CustomerBuilder.New().Build();
+            Do.Until(customer.GetBankAccount);
+            Do.Until(customer.GetPaymentCard);
+            Application application = ApplicationBuilder.New(customer).Build();
+
+            var response = Driver.Api.Queries.Post(new GetAccountOptionsUkQuery { AccountId = customer.Id });
+            Assert.GreaterThan(int.Parse(response.Values["ScenarioId"].Single()), 1);
+            // ToDo: Assert Options
+            //Assert.GreaterThan(int.Parse(response.Values["DaysTillRepaymentDate"].Single()), 0);
+        }
+
     }
 }
