6340e890933a596179c9b834512f5bcc76aac5ef
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-03-06 16:26:35 +0200

    Add LoginSection

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Sections/LoginSection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Wonga.QA.Framework.UI.Mappings.Sections
+{
+    
+    public sealed class LoginSection
+    {
+        public String LoginTrigger { get; set; }
+        public String FormId { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String LoginButton { get; set; }
+    }
+}
---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/UiElements/LoginElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using OpenQA.Selenium;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.UI.UiElements.Pages;
+using Wonga.QA.Framework.UI.UiElements.Pages.Interfaces;
+using Wonga.QA.Framework.UI.Mappings;
+
+namespace Wonga.QA.Framework.UI.UiElements
+{
+    //TODO finish this
+    public class LoginElement : BaseElement
+    {
+        private readonly IWebElement _loginTrigger;
+        private readonly IWebElement _form;
+        private readonly IWebElement _email;
+        private readonly IWebElement _password;
+        private readonly IWebElement _loginButton;
+
+        public LoginElement(BasePage page)
+            : base(page)
+        {
+            _loginTrigger = Page.Client.Driver.FindElement(By.CssSelector(Elements.Get.LoginSection.LoginTrigger));
+            _form = Page.Client.Driver.FindElement(By.CssSelector(Elements.Get.LoginSection.FormId));
+            _email = _form.FindElement(By.CssSelector(Elements.Get.LoginSection.Email));
+            _password = _form.FindElement(By.CssSelector(Elements.Get.LoginSection.Password));
+            _loginButton = _form.FindElement(By.CssSelector(Elements.Get.LoginSection.LoginButton));
+        }
+
+        public String SetEmail
+        {
+            set { _email.SendKeys(value); }
+        }
+        public String SetPassword
+        {
+            set { _password.SendKeys(value); }
+        }
+        public void MouseOverLoginTrigger()
+        {
+           _loginTrigger.MouseOver();
+        }
+    }
+}
