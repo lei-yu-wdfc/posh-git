0052bcadde9001715ed7e427e34b158e21989696
Alexander Luetjen <alexander.luetjen@wonga.com>
2012-03-07 14:49:44 +0000

    CA-1441 Added happy path RepayViaOnlineBillPayment test.

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/RepayViaOnlineBillPaymentTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.Db.Payments;
+using Wonga.QA.Framework.Msmq;
+using Wonga.QA.Framework.Msmq.Payments;
+using Wonga.QA.Tests.Core;
+using Wonga.QA.Tests.Payments.Helpers;
+
+namespace Wonga.QA.Tests.Payments
+{
+	[TestFixture]
+	public class RepayViaOnlineBillPaymentTests
+	{
+		[Test, AUT(AUT.Ca), JIRA("CA-1441")]
+		public void RepayViaOnlineBillPayment_ShouldCloseLoanWhenPaidInFull()
+		{
+			var customer = CustomerBuilder.New().Build();
+			var application = ApplicationBuilder.New(customer).Build();
+
+			var accountPreferences = Do.Until(() => Driver.Db.Payments.AccountPreferences.Single(a => a.AccountId == customer.Id));
+
+			decimal repaymentAmount = application.GetBalance();
+
+			var cmd = new RepayLoanInternalViaOnlineBillPaymentCommand
+			                                                   	{
+			                                                   		PaymentDate = DateTime.UtcNow,
+																	PayorAccountNumber = accountPreferences.Ccin,
+																	PayorAmount = repaymentAmount,
+																	PayorName = "PayorName",
+																	RemittanceTraceNumber = "TraceNumber"
+			                                                   	};
+
+			Driver.Msmq.Payments.Send(cmd);
+
+			Do.With().Timeout(60).Until(() => application.IsClosed);
+			VerifyPaymentFunctions.VerifyDirectBankPaymentOfAmount(application.Id, -repaymentAmount);
+		}
+	}
+}
