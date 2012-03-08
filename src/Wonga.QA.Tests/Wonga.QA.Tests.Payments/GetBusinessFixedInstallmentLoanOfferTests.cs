b4237cefa84aab5e54293728c29f6071288629f2
Guergana Yordanova <guergana.yordanova@wonga.com>
2012-03-08 12:40:45 +0000

    SME-889 Rewriting old acceptance tests to using new framework.

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/GetBusinessFixedInstallmentLoanOfferTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Payments
+{
+	[TestFixture]
+	public class GetBusinessFixedInstallmentLoanOfferTests
+	{
+		[Test, AUT(AUT.Wb), JIRA("SME-889")]
+		public void GetBusinessFixedInstallmentLoanOffer()
+		{
+			var response = Driver.Api.Queries.Post(new GetBusinessFixedInstallmentLoanOfferWbUkQuery());
+
+			Assert.IsNotNull(response);
+			Assert.AreEqual("3000.00", response.Values["AmountMin"].SingleOrDefault(),"Expected AmountMin is incorrect.");
+			Assert.AreEqual("10000.00", response.Values["AmountMax"].SingleOrDefault(), "Expected AmountMax is incorrect.");
+			Assert.AreEqual("9000", response.Values["AmountDefault"].SingleOrDefault(), "Expected AmountDefault is incorrect.");
+			Assert.AreEqual("1", response.Values["TermMin"].SingleOrDefault(), "Expected TermMin is incorrect."); 
+			Assert.AreEqual("52", response.Values["TermMax"].SingleOrDefault(), "Expected TermMax is incorrect.");
+			Assert.AreEqual("16", response.Values["TermDefault"].SingleOrDefault(), "Expected TermDefault is incorrect");
+		}
+
+		[Test, AUT(AUT.Wb), JIRA("SME-889")]
+		public void GetBusinessFixedInstallmentLoanOfferAccountIsNull()
+		{
+			var response = Driver.Api.Queries.Post(new GetBusinessFixedInstallmentLoanOfferWbUkQuery{ AccountId = Guid.NewGuid()});
+
+			Assert.IsNotNull(response);
+			Assert.AreEqual("3000.00", response.Values["AmountMin"].SingleOrDefault(), "Expected AmountMin is incorrect.");
+			Assert.AreEqual("10000.00", response.Values["AmountMax"].SingleOrDefault(), "Expected AmountMax is incorrect.");
+			Assert.AreEqual("9000", response.Values["AmountDefault"].SingleOrDefault(), "Expected AmountDefault is incorrect.");
+			Assert.AreEqual("1", response.Values["TermMin"].SingleOrDefault(), "Expected TermMin is incorrect.");
+			Assert.AreEqual("52", response.Values["TermMax"].SingleOrDefault(), "Expected TermMax is incorrect.");
+			Assert.AreEqual("16", response.Values["TermDefault"].SingleOrDefault(), "Expected TermDefault is incorrect");
+		}
+		
+	}
+}
