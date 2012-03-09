16f10106e31f095f37378c82a0abdd2f0f0d60b5
vaclavwonga <vaclav.haken@wonga.com>
2012-03-09 14:41:07 +0000

    SME-1128 added validators and tests to test their functionality

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.ContactManagement/AddDirectorCommandTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Api.Exceptions;
+using Wonga.QA.Framework.Core;
+
+namespace Wonga.QA.Tests.ContactManagement
+{
+    [TestFixture]
+    public class AddDirectorCommandTests
+    {
+        [Test]
+        public void TestPrimaryDirectorNonUniqueEmail()
+        {
+            String email = Data.RandomEmail();
+            var req= AddPrimaryOrganisationDirectorCommand.New();
+
+            req.Email = email;  
+
+            ApiResponse resp = Driver.Api.Commands.Post(req);
+
+            bool errorDetected = false;
+            try
+            {
+                resp = Driver.Api.Commands.Post(req);
+            }
+            catch (ValidatorException e)
+            {                        
+                foreach (String s in e.Errors)
+                {
+                    if (s == "Comms_EmailAddress_NotUnique")
+                    {
+                        errorDetected = true;
+                    }
+                }
+            }
+            
+
+            Assert.IsTrue(errorDetected, "Email not unique error was expected");
+        }
+
+        [Test]
+        public void TestSecondaryDirectorNonUniqueEmail()
+        {
+            String email = Data.RandomEmail();
+            var req = AddSecondaryOrganisationDirectorCommand.New();
+
+            req.Email = email;
+
+            ApiResponse resp = Driver.Api.Commands.Post(req);
+
+            bool errorDetected = false;
+            try
+            {
+                resp = Driver.Api.Commands.Post(req);
+            }
+            catch (ValidatorException e)
+            {
+                foreach (String s in e.Errors)
+                {
+                    if (s == "Comms_EmailAddress_NotUnique")
+                    {
+                        errorDetected = true;
+                    }
+                }
+            }
+
+
+            Assert.IsTrue(errorDetected, "Email not unique error was expected");
+        }
+    }
+}
