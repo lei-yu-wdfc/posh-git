0b40341b151770a0698dae617c95983aba0717c2
Ilya Kozhevnikov <ilya.kozhevnikov@gmail.com>
2012-03-09 16:51:56 +0000

    little scotia mock maintenance

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Mocks/MockDriver.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Mocks/MockDriver.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using Wonga.QA.Framework.Core;
-
-namespace Wonga.QA.Framework.Mocks
+﻿namespace Wonga.QA.Framework.Mocks
 {
     public class MockDriver
     {
-        /// <summary>
-        /// Lazy Scotia object
-        /// </summary>
         private Scotia _scotia;
 
-        /// <summary>
-        /// Scotia. Instantiated lazily.
-        /// </summary>
         public Scotia Scotia
         {
             get { return _scotia ?? (_scotia = new Scotia()); }
