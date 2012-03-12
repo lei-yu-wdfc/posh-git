0694094d7bb4036997ad31fa3e93c75b2faafb50
Ilya Kozhevnikov <ilya.kozhevnikov@gmail.com>
2012-03-12 14:08:34 +0000

    fixed wb app builder test

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Meta/BuilderTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Meta/BuilderTests.cs
@@ -1,5 +1,7 @@
 ﻿using MbUnit.Framework;
 using Wonga.QA.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
 
 namespace Wonga.QA.Tests.Meta
 {
@@ -7,17 +9,27 @@ namespace Wonga.QA.Tests.Meta
     public class BuilderTests
     {
         private Customer _customer;
+        private Organisation _organisation;
 
-        [Test]
+        [Test(Order = 1)]
         public void CustomerBuilderTest()
         {
             Assert.DoesNotThrow(() => _customer = CustomerBuilder.New().Build());
         }
 
-        [Test, DependsOn("CustomerBuilderTest")]
+        [Test(Order = 2), AUT(AUT.Wb)]
+        public void OrganisationBuilderTest()
+        {
+            Assert.DoesNotThrow(() => _organisation = OrganisationBuilder.New(_customer).Build());
+        }
+
+        [Test(Order = 3)]
         public void ApplicationBuilderTest()
         {
-            Assert.DoesNotThrow(() => ApplicationBuilder.New(_customer).Build());
+            ApplicationBuilder builder = Config.AUT == AUT.Wb ?
+                ApplicationBuilder.New(_customer, _organisation) :
+                ApplicationBuilder.New(_customer);
+            Assert.DoesNotThrow(() => builder.Build());
         }
     }
 }
