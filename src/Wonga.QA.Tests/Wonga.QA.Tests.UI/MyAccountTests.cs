9e90ef1c237c3f5e53e149c2d98ce9363a3f3770
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-03-12 15:42:05 +0200

    QA-218

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/MyAccountTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Ui
+{
+    class MyAccountTests : UiTest
+    {
+        [Test, AUT(AUT.Za), JIRA("QA-218")]
+        public void CustomerWithLiveLoanShouldNotBeAbleToAddBankAccount()
+        {
+            var loginPage = Client.Login();
+            string email = Data.RandomEmail();
+            var customer = CustomerBuilder.New().WithEmailAddress(email).Build();
+            var application = ApplicationBuilder.New(customer).Build();
+            var mySummaryPage = loginPage.LoginAs(email);
+            Assert.IsNotNull(mySummaryPage);
+        }
+    }
+}
