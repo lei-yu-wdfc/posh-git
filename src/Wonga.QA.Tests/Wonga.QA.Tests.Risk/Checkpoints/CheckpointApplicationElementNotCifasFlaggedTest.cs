7b2d6300ef2b02d3436fe37107dfb46792922d7a
OBurlachenko <burlachenkoo@mail.ru>
2012-03-13 11:26:14 +0200

    UK-851 As a risk analyst I do not want to lend to a new cusomer where we cannot find their details at a credit bureau so that we can avoid a customer defaulting on a loan

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointApplicationElementNotCifasFlaggedTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Risk.Checkpoints
+{
+	[Parallelizable(TestScope.All), AUT(AUT.Uk)]
+	class CheckpointApplicationElementNotCifasFlaggedTest
+	{
+		private const string TestMask = "test:ApplicationElementNotCIFASFlagged";
+
+		[Test, AUT(AUT.Uk), JIRA("UK-852")]
+		public void Accepted()
+		{
+			var customer =
+				CustomerBuilder.New()
+				.WithEmployer(TestMask)
+				.WithForename(CreditBureauCustomerUk.ForeName)
+				.WithSurname(CreditBureauCustomerUk.SurName)
+				.WithDateOfBirth(CreditBureauCustomerUk.DateOfBirth)
+				.Build();
+
+			var application = ApplicationBuilder.New(customer).Build();
+
+			// wait until workflow will be created and started
+			Do.Until(() => Driver.Db.Risk.RiskWorkflows.Single(x => x.ApplicationId == application.Id));
+
+			var riskWorkflows = Application.GetWorkflowsForApplication(application.Id);
+			Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
+
+			// wait until decision made
+			Do.Until(() => Driver.Db.Risk.RiskApplications.Single(x => x.ApplicationId == application.Id && x.Decision != 0));
+
+			Assert.Contains(
+				Application.GetExecutedCheckpointDefinitionsForRiskWorkflow(riskWorkflows[0].WorkflowId, CheckpointStatus.Verified), 
+				Data.EnumToString(CheckpointDefinitionEnum.CIFASFraudCheck));
+
+		}
+	}
+}
---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/CreditBureauCustomerUk.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using Wonga.QA.Framework.Core;
+
+namespace Wonga.QA.Tests.Risk
+{
+	public static class CreditBureauCustomerUk
+	{
+		public static string ForeName = "Janet";
+		public static string MiddleName = "Bernadette";
+		public static string SurName = "";
+		public static Date DateOfBirth = new Date(new DateTime(1963, 07, 26));
+		public static string HouseNumber = "14";
+		public static string Street = "Mather Avenue";
+		public static string District = "Weston Point";
+		public static string Town = "Runcorn";
+		public static string County = "Cheshire";
+		public static string Postcode = "WA7 4JJ";
+		public static string CountryCode = "UK";
+	}
+}
