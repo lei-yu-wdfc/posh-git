13c45da8381bba0306084eb88a63dbd44dd6b3d1
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-03-14 12:26:53 +0200

    QA-160

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Ui/Pages/Common/MySummary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.Framework.UI.Mappings.Pages
+{
+    public sealed class MySummary
+    {
+        public String Title { get; set; }
+    }
+}
