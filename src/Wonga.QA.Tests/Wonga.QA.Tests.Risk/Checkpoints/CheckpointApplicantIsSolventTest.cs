c0ee0add7348df63efde37896d072b288b11c7f9
OBurlachenko <burlachenkoo@mail.ru>
2012-03-15 16:48:50 +0200

    UK-854 As a risk analyst I do not wany to lend to a new customer when the customer is marked as not solvent by a bureau so that I can minimise the risk of default

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointApplicantIsSolventTest.cs
@@ -0,0 +1,25 @@
+﻿using MbUnit.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Risk.Checkpoints
+{
+	[Parallelizable(TestScope.All)]
+	public class CheckpointApplicantIsSolventTest : BaseCheckpointTest
+	{
+		private const string TestMask = "test:IsSolvent";
+
+		[Test, AUT(AUT.Uk), JIRA("UK-854")]
+		public void AcceptForNormalCustomer()
+		{
+			RunSingleWorkflowTest(TestMask, new CustomerKathleenUk(), CheckpointDefinitionEnum.CustomerIsSolvent, CheckpointStatus.Verified);
+		}
+
+		[Test, AUT(AUT.Uk), JIRA("UK-854")]
+		public void DeclineForSolvent()
+		{
+			RunSingleWorkflowTest(TestMask, new CustomerLauraUk(), CheckpointDefinitionEnum.CustomerIsSolvent, CheckpointStatus.Failed);
+		}
+	}
+}
---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/CustomerLauraUk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Wonga.QA.Framework.Core;
+
+namespace Wonga.QA.Tests.Risk
+{
+	public class CustomerLauraUk : CustomerData
+	{
+		public CustomerLauraUk()
+		{
+			ForeName = "Laura";
+			MiddleName = "";
+			SurName = "Insolvent";
+			DateOfBirth = new Date(new DateTime(1987, 09, 18));
+			Flat = "1";
+			HouseNumber = "6";
+			HouseName = "1";
+			Street = "Mather Avenue";
+			District = "";
+			Town = "Shanklin";
+			County = "";
+			Postcode = "BB12 0NL";
+			CountryCode = "UK";
+		}
+	}
+}
