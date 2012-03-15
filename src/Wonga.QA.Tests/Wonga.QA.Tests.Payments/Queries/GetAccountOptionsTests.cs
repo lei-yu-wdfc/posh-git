ffe92c1c6ea3307c371e5b1ed690c67f008b6792
Charlie Barker <charlie.barker@wonga.com>
2012-03-15 13:18:09 +0000

    UK-1005 Acceptance test for scenario1

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Queries/GetAccountOptionsTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Queries/GetAccountOptionsTests.cs
@@ -13,12 +13,32 @@ using AddBankAccountUkCommand = Wonga.QA.Framework.Msmq.AddBankAccountUkCommand;
 using CreateFixedTermLoanApplicationCommand = Wonga.QA.Framework.Msmq.CreateFixedTermLoanApplicationCommand;
 using SignApplicationCommand = Wonga.QA.Framework.Msmq.SignApplicationCommand;
 
+
 namespace Wonga.QA.Tests.Payments.Queries
 {
-    [Parallelizable]
+    [TestFixture, Parallelizable(TestScope.All)]
     public class GetAccountOptionsTests
     {
-        [Test, AUT(AUT.Uk), JIRA("UK-823"),]
+        private string _resetExtendLoanDays = null;
+        [SetUp]
+        public void Setup()
+        {
+            // Record value
+            var cfg = Driver.Db.Ops.ServiceConfigurations.Single(a => a.Key == "Payments.ExtendLoanMinDays");
+            _resetExtendLoanDays = cfg.Value;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Reset value
+            var cfg = Driver.Db.Ops.ServiceConfigurations.Single(a => a.Key == "Payments.ExtendLoanMinDays");
+            //  if (cfg.Value != _resetExtendLoanDays)
+            cfg.Value = _resetExtendLoanDays;
+            cfg.Submit();
+        }
+
+        [Test, AUT(AUT.Uk), JIRA("UK-823")]
         public void Scenario1ExistingCustomerWithoutLiveLoan()
         {
             var accountId = Guid.NewGuid();
@@ -26,8 +46,7 @@ namespace Wonga.QA.Tests.Payments.Queries
             var paymentCardId = Guid.NewGuid();
             var appId = Guid.NewGuid();
 
-            // Create Account
-            //Driver.Msmq.Payments.Send(new IAccountCreatedEvent() { AccountId = accountId });
+
        /*     Driver.Msmq.Payments.Send(new AddBankAccountUkCommand() { BankCode = "161017",
                                                                       AccountId = accountId,
                                                                       AccountNumber = "10062650",
@@ -51,43 +70,22 @@ namespace Wonga.QA.Tests.Payments.Queries
                 CreatedOn = DateTime.UtcNow
             });
 
+            // Check App Exists in DB
+            Do.Until(() => Driver.Db.Payments.Applications.Single(a => a.ExternalId == appId));
+
             Driver.Msmq.Payments.Send(new SignApplicationCommand() { AccountId = accountId, ApplicationId = appId,CreatedOn = DateTime.Now.AddHours(-1)});
             Driver.Msmq.Payments.Send(new IApplicationAcceptedEvent() { AccountId = accountId, ApplicationId = appId,CreatedOn = DateTime.Now.AddHours(-1) });
 
             // Check App Exists in DB
-            Do.Until(() => Driver.Db.Payments.Applications.Single(a => a.ExternalId == appId));
-
+            Do.Until(() => Driver.Db.Payments.Applications.Single(a => a.ExternalId == appId && a.SignedOn != null && a.AcceptedOn != null));
+      
             // Go to DB and set Application to closed
             ApplicationEntity app = Driver.Db.Payments.Applications.Single(a => a.ExternalId == appId);
             app.ClosedOn = DateTime.UtcNow;
-            app.Submit();
+            app.Submit(true);
             
             /*
-            Driver.Msmq.Payments.Send(new CreateTransactionCommand(){ ApplicationId = appId,
-                                                                        ExternalId = Guid.NewGuid(),
-                                                                        Amount = 100.00M, 
-                                                                        Type = PaymentTransactionEnum.CashAdvance, 
-                                                                        Currency = CurrencyCodeIso4217Enum.GBP,
-                                                                        Mir = 30.0M,
-                                                                        PostedOn = DateTime.Now,
-                                                                        Scope = PaymentTransactionScopeEnum.Debit,
-                                                                        Reference = "Test Cash Advance"
-                                                                    });
-
-
-            Driver.Msmq.Payments.Send(new CreateTransactionCommand()
-                                                                    {
-                                                                        ApplicationId = appId,
-                                                                        ExternalId = Guid.NewGuid(),
-                                                                        Amount = 5.50M,
-                                                                        Type = PaymentTransactionEnum.Fee,
-                                                                        Currency = CurrencyCodeIso4217Enum.GBP,
-                                                                        Mir = 30.0M,
-                                                                        PostedOn = DateTime.Now,
-                                                                        Scope = PaymentTransactionScopeEnum.Debit,
-                                                                        Reference = "Test Transmission Fee"
-                                                                    });
-
+           
             Driver.Msmq.Payments.Send(new CreateTransactionCommand()
                                                                     {
                                                                         ApplicationId = appId,
@@ -109,13 +107,22 @@ namespace Wonga.QA.Tests.Payments.Queries
         }
 
         [Test, AUT(AUT.Uk), JIRA("UK-823"),]
-        [Ignore("NRE: GetAccountOptionsHandler.cs:line 100")]
         public void Scenario02CustomerWithLiveLoanWithAvailableCreditTooEarlyToExtend()
         {
             var accountId = Guid.NewGuid();
             var bankAccountId = Guid.NewGuid();
             var paymentCardId = Guid.NewGuid();
             var appId = Guid.NewGuid();
+            const string extendMinLoanDays = "11";
+            
+
+            // Create Account so that time zone can be looked up
+            Driver.Msmq.Payments.Send(new IAccountCreatedEvent() { AccountId = accountId });
+
+            // Override setting in ServiceConfig Db for ExtendDaysMins
+            var app = Driver.Db.Ops.ServiceConfigurations.Single(a => a.Key == "Payments.ExtendLoanMinDays");
+            app.Value = extendMinLoanDays;
+            app.Submit();
 
             // Create Application 
             Driver.Msmq.Payments.Send(new CreateFixedTermLoanApplicationCommand()
@@ -136,6 +143,37 @@ namespace Wonga.QA.Tests.Payments.Queries
 
             // Check App Exists in DB
             Do.Until(() => Driver.Db.Payments.Applications.Single(a => a.ExternalId == appId));
+
+            var trnGuid1 = Guid.NewGuid();
+            Driver.Msmq.Payments.Send(new CreateTransactionCommand()
+            {
+                ApplicationId = appId,
+                ExternalId = trnGuid1,
+                Amount = 100.00M,
+                Type = PaymentTransactionEnum.CashAdvance,
+                Currency = CurrencyCodeIso4217Enum.GBP,
+                Mir = 30.0M,
+                PostedOn = DateTime.Now,
+                Scope = PaymentTransactionScopeEnum.Debit,
+                Reference = "Test Cash Advance"
+            });
+
+            var trnGuid2 = Guid.NewGuid();
+            Driver.Msmq.Payments.Send(new CreateTransactionCommand()
+            {
+                ApplicationId = appId,
+                ExternalId = trnGuid2,
+                Amount = 5.50M,
+                Type = PaymentTransactionEnum.Fee,
+                Currency = CurrencyCodeIso4217Enum.GBP,
+                Mir = 30.0M,
+                PostedOn = DateTime.Now,
+                Scope = PaymentTransactionScopeEnum.Debit,
+                Reference = "Test Transmission Fee"
+            });
+
+            Do.Until(() => Driver.Db.Payments.Transactions.Count(itm => itm.ExternalId == trnGuid1 || itm.ExternalId == trnGuid2) == 2);
+            
             var response = Driver.Api.Queries.Post(new GetAccountOptionsUkQuery { AccountId = accountId, TrustRating = 400.00M });
             Assert.GreaterThan(int.Parse(response.Values["ScenarioId"].Single()), 2);
         }
