b132da6dc715f767195ab360027d28713fca9639
Mihail Podobivsky <mihail.podobivsky@softheme.com>
2012-03-16 14:37:52 +0200

    QA-203

--- src/Wonga.QA.Tests/Wonga.QA.Tests.UI/MyAccountTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/MyAccountTests.cs
@@ -1,7 +1,10 @@
-﻿using MbUnit.Framework;
+﻿using Gallio.Framework.Assertions;
+using MbUnit.Framework;
 using OpenQA.Selenium;
 using Wonga.QA.Framework;
 using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.UI;
+using Wonga.QA.Framework.UI.UiElements.Pages.Common;
 using Wonga.QA.Tests.Core;
 using Wonga.QA.Framework.UI.Mappings;
 
@@ -22,5 +25,36 @@ namespace Wonga.QA.Tests.Ui
             Assert.IsFalse(myPaymentDetailsPage.IsAddBankAccountButtonExists());
         }
 
+        [Test, AUT(AUT.Za), JIRA("QA-203")]
+        public void L0JourneyInvalidAccountNumberShouldCauseWarningMessageOnNextPage()
+        {
+            var journey1 = new Journey(Client.Home());
+            var bankDetailsPage1 = journey1.ApplyForLoan(200, 10)
+                                      .FillPersonalDetails("test:EmployedMask")
+                                      .FillAddressDetails()
+                                      .FillAccountDetails()
+                                      .CurrentPage as PersonalBankAccountPage;
+
+            bankDetailsPage1.BankAccountSection.BankName = "Capitec";
+            bankDetailsPage1.BankAccountSection.BankAccountType = "Current";
+            bankDetailsPage1.BankAccountSection.AccountNumber = "7434567";
+            bankDetailsPage1.BankAccountSection.BankPeriod = "2 to 3 years";
+            bankDetailsPage1.PinVerificationSection.Pin = "0000";
+            Assert.Throws<AssertionFailureException>(() => { var processingPage = bankDetailsPage1.Next(); });
+
+            var journey2 = new Journey(Client.Home());
+            var bankDetailsPage2 = journey2.ApplyForLoan(200, 10)
+                                      .FillPersonalDetails("test:EmployedMask")
+                                      .FillAddressDetails()
+                                      .FillAccountDetails()
+                                      .CurrentPage as PersonalBankAccountPage;
+
+            bankDetailsPage2.BankAccountSection.BankName = "Capitec";
+            bankDetailsPage2.BankAccountSection.BankAccountType = "Current";
+            bankDetailsPage2.BankAccountSection.AccountNumber = "7534567";
+            bankDetailsPage2.BankAccountSection.BankPeriod = "2 to 3 years";
+            bankDetailsPage2.PinVerificationSection.Pin = "0000";
+            Assert.Throws<AssertionFailureException>(() => { var processingPage = bankDetailsPage2.Next(); });
+        }
     }
 }
