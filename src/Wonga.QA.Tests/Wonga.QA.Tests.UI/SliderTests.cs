60f663cc5274aae57f88cf4ef7e463aab9a96cb7
Adrian Murphy <adrian.murphy@wonga.com>
2012-03-20 16:29:29 +0000

    Fixed broken L0 Slider Tests. Removed AUT.ZA attribute from Ln tests as RepayOnDueDate has been broken for ZA

--- src/Wonga.QA.Tests/Wonga.QA.Tests.UI/SliderTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/SliderTests.cs
@@ -133,7 +133,7 @@ namespace Wonga.QA.Tests.Ui
         [Test, AUT(AUT.Ca, AUT.Za), JIRA("QA-156", "QA-238")]
         public void DefaultAmountSliderValueShouldBeCorrectLn()
         {
-            var loginPage = Client.Login();
+            var loginPage = Client.Login();            var loginPage = Client.Login();
             string email = Data.RandomEmail();
             Customer customer = CustomerBuilder.New().WithEmailAddress(email).Build();
             Application application = ApplicationBuilder.New(customer)
@@ -157,25 +157,13 @@ namespace Wonga.QA.Tests.Ui
         public void DefaultDurationSliderValueShouldBeCorrectL0()
         {
             var page = Client.Home();
-            switch (Config.AUT)
-            {
-                case AUT.Za:
-                    string[] dateArray = page.Sliders.GetRepaymentDate.Split(' ');
-                    string day = Char.IsDigit(dateArray[1].ElementAt(1)) ? dateArray[1].Remove(2, 2) : dateArray[1].Remove(1, 2);
-                    _repaymentDate = day + " " + dateArray[2] + " " + dateArray[3];
 
-                    var today = DateTime.Today;
-                    var nextPayDate = today.Day <= 25
-                                           ? new DateTime(today.Year, today.Month, 25)
-                                           : new DateTime(today.Year, today.Month, 25).AddMonths(1);
-                    var expectedDate = Driver.Db.GetPreviousWorkingDay(new Date(nextPayDate));
-                    Assert.AreEqual(String.Format("{0:d MMM yyyy}", expectedDate.DateTime), _repaymentDate);
-                    break;
-                case AUT.Ca:
-                    Assert.AreEqual(page.Sliders.HowLong, "11");
-                    break;
-            }
+			string[] dateArray = page.Sliders.GetRepaymentDate.Split(' ');
+			string day = Char.IsDigit(dateArray[1].ElementAt(1)) ? dateArray[1].Remove(2, 2) : dateArray[1].Remove(1, 2);
+			_repaymentDate = day + " " + dateArray[2] + " " + dateArray[3];
 
+			var expectedDate = GetExpectedDefaultPromiseDateL0();
+            Assert.AreEqual(String.Format("{0:d MMM yyyy}", expectedDate), _repaymentDate);
         }
 
         [Test, AUT(AUT.Ca), JIRA("QA-241", "QA-159")]
@@ -184,9 +172,8 @@ namespace Wonga.QA.Tests.Ui
             var loginPage = Client.Login();
             string email = Data.RandomEmail();
             Customer customer = CustomerBuilder.New().WithEmailAddress(email).Build();
-            Application application = ApplicationBuilder.New(customer)
-                .Build();
-            application.RepayOnDueDate();
+        	Application application = ApplicationBuilder.New(customer).Build().RepayOnDueDate();
+
             loginPage.LoginAs(email);
 
             var page = Client.Home();
@@ -211,7 +198,7 @@ namespace Wonga.QA.Tests.Ui
 
         }
 
-        [Test, AUT(AUT.Ca, AUT.Za), JIRA("QA-237", "QA-153")]
+        [Test, AUT(AUT.Ca), JIRA("QA-237", "QA-153")]
         public void ChangingAmountBeyondMinIsNotAllowedByFrontEnd()
         {
             var product = Driver.Db.Payments.Products.FirstOrDefault();
@@ -224,11 +211,10 @@ namespace Wonga.QA.Tests.Ui
             Assert.AreEqual(minAmountValue.ToString(CultureInfo.InvariantCulture), page.Sliders.HowMuch);
         }
 
-        [Test, AUT(AUT.Ca), JIRA("QA-239", "QA-158")]
+        [Test, AUT(AUT.Ca, AUT.Za), JIRA("QA-239", "QA-158")]
         public void MaxDurationSliderValueShouldBeCorrectL0()
         {
-            var product = Driver.Db.Payments.Products.FirstOrDefault();
-            int maxLoanDuration = product.TermMax;
+        	int maxLoanDuration = GetExpectedMaxTermL0();
             int setLoanDuration = maxLoanDuration + 1;
             var page = Client.Home();
             page.Sliders.HowLong = setLoanDuration.ToString(CultureInfo.InvariantCulture);
@@ -262,8 +248,7 @@ namespace Wonga.QA.Tests.Ui
         [Test, AUT(AUT.Ca, AUT.Za), JIRA("QA-157")]
         public void MinDurationSliderValueShouldBeCorrectL0()
         {
-            var product = Driver.Db.Payments.Products.FirstOrDefault();
-            int minDurationValue = (int)product.TermMin;
+            int minDurationValue = GetExpectedMinTerm();
             int setDurationValue = minDurationValue - 1;
             var page = Client.Home();
             page.Sliders.HowLong = setDurationValue.ToString(CultureInfo.InvariantCulture);
@@ -272,7 +257,7 @@ namespace Wonga.QA.Tests.Ui
             Assert.AreEqual(minDurationValue.ToString(CultureInfo.InvariantCulture), page.Sliders.HowLong);
         }
 
-        [Test, AUT(AUT.Ca, AUT.Za), JIRA("QA-157")]
+        [Test, AUT(AUT.Ca), JIRA("QA-157")]
         public void MinDurationSliderValueShouldBeCorrectLn()
         {
             var loginPage = Client.Login();
@@ -303,6 +288,87 @@ namespace Wonga.QA.Tests.Ui
                 .Build();
             loginPage.LoginAs(email);
             Assert.Throws<Exception>(() => { var page = Client.Home(); });
+		}
+
+
+		#region Helpers
+
+		private DateTime GetExpectedDefaultPromiseDateL0()
+		{
+			DateTime defaultPromiseDate;
+			var now = DateTime.Today;
+
+			switch (Config.AUT)
+			{
+					case AUT.Za:
+					{
+						var iMonth = DateTime.UtcNow.Month - 1;
+
+						var payDayPerMonths = Driver.Db.Ops.ServiceConfigurations.Single(a => a.Key == "Payments.PayDayPerMonth").Value.Split(',');
+						var sliderTermAddDays = Driver.Db.Ops.ServiceConfigurations.Single(a => a.Key == "Payments.SliderTermAddDays").Value;
+
+						var payDayPerMonth = Int32.Parse(payDayPerMonths[iMonth]);
+						var minimumTerm = Int32.Parse(sliderTermAddDays);
+
+						defaultPromiseDate = new DateTime(now.Year, now.Month, payDayPerMonth);
+						var expectedTermDefault = defaultPromiseDate.Subtract(now).Days;
+
+						//Check if we should snap to the next month's payday
+						if (expectedTermDefault < minimumTerm)
+						{
+							iMonth = iMonth + 1 >= 12 ? 1 : iMonth + 1;
+							payDayPerMonth = Int32.Parse(payDayPerMonths[iMonth]);
+							defaultPromiseDate = defaultPromiseDate.AddMonths(1);
+							defaultPromiseDate = new DateTime(defaultPromiseDate.Year, defaultPromiseDate.Month, payDayPerMonth);
+						}
+
+						defaultPromiseDate = Driver.Db.GetPreviousWorkingDay(new Date(defaultPromiseDate));
+					}
+					break;
+
+				default:
+					{
+						defaultPromiseDate = now.AddDays(11 + 1); //CA starts all loans from the next working day
+					}
+					break;
+			}
+			return defaultPromiseDate;
+		}
+
+		private int GetExpectedMinTerm()
+		{
+			return Driver.Db.Payments.Products.FirstOrDefault().TermMin;
+		}
+
+		private int GetExpectedMaxTermL0()
+		{
+			int maxTerm = 0;
+
+			switch (Config.AUT)
+			{
+				case AUT.Za:
+					{
+						var promiseDate = GetExpectedDefaultPromiseDateL0();
+						var iMonth = promiseDate.Month - 1;
+
+						var payDayPlusToMaxTerm = Int32.Parse(Driver.Db.Ops.ServiceConfigurations.Single(a => a.Key == "Payments.PayDayPlusToMaxTerm").Value.Split(',')[iMonth]);
+
+						maxTerm = (promiseDate.AddDays(payDayPlusToMaxTerm) - DateTime.Today).Days;
+					}
+					break;
+
+				default:
+					{
+						return (int) Driver.Db.Payments.Products.FirstOrDefault().TermMax;
+					}
+					break;
+			}
+
+			return maxTerm;
+		}
+
+		#endregion
+	}
         }
 
          [Test, AUT(AUT.Ca, AUT.Za), JIRA("QA-155")]
