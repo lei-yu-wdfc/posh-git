1927c24f84c46410db97341603ffb6dd8126ecd4
Michael.Nowicki <michael.nowicki@wonga.com>
2012-03-21 18:04:59 +0000

    SurveyElement standardization

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Elements/SurveyElement.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Elements/SurveyElement.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using OpenQA.Selenium;
 using Wonga.QA.Framework.UI.Elements;
+using Wonga.QA.Framework.UI.Mappings;
 using Wonga.QA.Framework.UI.UiElements.Pages;
 
 namespace Wonga.QA.Framework.UI.Elements
@@ -14,7 +15,7 @@ namespace Wonga.QA.Framework.UI.Elements
 
         public SurveyElement(BasePage page) : base(page)
         {
-            _surveyBox = page.Client.Driver.FindElement(By.XPath("//div[@id='wonga_survey_box']"));
+            _surveyBox = page.Client.Driver.FindElement(By.CssSelector(Ui.Get.SurveyElement.SurveyBox));
         }
 
         public bool IsVisible
