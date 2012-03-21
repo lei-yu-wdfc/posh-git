021483f27cbeedcc3b93ecf7bd63b619b052cf23
Michael.Nowicki <michael.nowicki@wonga.com>
2012-03-21 15:27:19 +0000

    SurveyTest

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Elements/SurveyElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using Wonga.QA.Framework.UI.Elements;
+using Wonga.QA.Framework.UI.UiElements.Pages;
+
+namespace Wonga.QA.Framework.UI.Elements
+{
+    public class SurveyElement : BaseElement
+    {
+        private IWebElement _surveyBox;
+
+        public SurveyElement(BasePage page) : base(page)
+        {
+            _surveyBox = page.Client.Driver.FindElement(By.XPath("//div[@id='wonga_survey_box']"));
+        }
+
+        public bool IsVisible
+        {
+            get { return _surveyBox.Displayed; }
+        }
+    }
+
+}
---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/SurveyTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using OpenQA.Selenium;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+using System.Threading;
+
+namespace Wonga.QA.Tests.Ui
+{
+    class SurveyTest : UiTest
+    {
+        [Test, AUT(AUT.Ca), JIRA("CA-1826")]
+        public void IsSurveyAvailableOnHomePage()
+        {
+            var page = Client.Home();
+            Assert.IsNotNull(page.Survey);
+        }
+
+        [Test, AUT(AUT.Ca),JIRA("CA-1826")]
+        public void IsSurveyHiddenByDefaultOnHomePage()
+        {
+            var page = Client.Home();
+            Assert.IsFalse(page.Survey.IsVisible);
+        }
+
+        [Test, AUT(AUT.Ca),JIRA("CA-1826")]
+        public void IsSurveyVisibleAfter15SecondsOnHomePage()
+        {
+            var page = Client.Home();
+            Thread.Sleep(18000);
+            Assert.IsTrue(page.Survey.IsVisible);
+        }
+
+    
+    }
+}
