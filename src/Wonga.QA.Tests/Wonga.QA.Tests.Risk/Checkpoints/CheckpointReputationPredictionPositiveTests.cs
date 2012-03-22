7e75024314586d4022c4fa7ae6a4d8b030b5cc27
Adrian Murphy <adrian.murphy@wonga.com>
2012-03-22 17:15:06 +0000

    Added tests for ReputationPredictionPositive checkpoint

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointReputationPredictionPositiveTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointReputationPredictionPositiveTests.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
 using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.Db;
 using Wonga.QA.Tests.Core;
 
 namespace Wonga.QA.Tests.Risk.Checkpoints
@@ -11,6 +14,53 @@ namespace Wonga.QA.Tests.Risk.Checkpoints
 	[Parallelizable(TestScope.All)]
 	class CheckpointReputationPredictionPositiveTests
 	{
+		private const RiskMask TestMask = RiskMask.TESTReputationtPredictionPositive;
 
+		private string _forename;
+		private string _surname;
+		private Date _dateOfBirth;
+		private string _nationalNumber;
+
+		[FixtureSetUp]
+		public void FixtureSetUp()
+		{
+			switch (Config.AUT)
+			{
+				case AUT.Za:
+					{
+						_forename = "ANITHA";
+						_surname = "ESSACK";
+						_dateOfBirth = new Date(new DateTime(1957, 12, 19));
+						_nationalNumber = "5712190106083";
+
+						Drive.Db.SetServiceConfiguration("FeatureSwitch.ZA.ReputationPredictionCheckpoint", "true");
+					}
+					break;
+				default:
+					{
+						throw new NotImplementedException(Config.AUT.ToString());
+					}
+			}
+		}
+
+		[Test, AUT(AUT.Za), JIRA("ZA-1938"), Pending("Work in progress")]
+		public void CheckpointReputationPredictionPositiveAccept()
+		{
+			var customer = CustomerBuilder.New()
+				.WithEmployer(TestMask)
+				.WithForename(_forename)
+				.WithSurname(_surname)
+				.WithDateOfBirth(_dateOfBirth)
+				.WithNationalNumber(_nationalNumber)
+				.Build();
+
+			var application = ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatusEnum.Accepted).Build();
+		}
+
+		[Test, AUT(AUT.Za), JIRA("ZA-1938"), Pending("Work in progress")]
+		public void CheckpointReputationPredictionPositiveDecline()
+		{
+
+		}
 	}
 }
