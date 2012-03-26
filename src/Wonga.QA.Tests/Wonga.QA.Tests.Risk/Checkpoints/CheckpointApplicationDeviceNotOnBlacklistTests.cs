7fd9204325e16fdd6fcd77d2104f3e6bfa196664
alexpricope <alex.pricope@wonga.com>
2012-03-26 18:13:24 +0100

    SME-130

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointApplicationDeviceNotOnBlacklistTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointApplicationDeviceNotOnBlacklistTests.cs
@@ -9,27 +9,103 @@ namespace Wonga.QA.Tests.Risk.Checkpoints
 	[Parallelizable(TestScope.All)]
     public class CheckpointApplicationDeviceNotOnBlacklistTests
     {
-        private readonly string _testMask = RiskMask.TESTDeviceNotOnBlacklist.ToString();
-
-        [Test, AUT(AUT.Ca, AUT.Uk), JIRA("CA-1735")]
-		public void CheckpointApplicationDeviceNotOnBlacklistDecline()
+        [Test, AUT(AUT.Ca, AUT.Uk, AUT.Wb), JIRA("CA-1735", "SME-130")]
+		public void CheckpointApplicationFailsIovation_LoanIsDeclined()
         {
-            var customer = CustomerBuilder.New().WithEmployer(_testMask).Build();
-            var application = ApplicationBuilder.New(customer).WithIovationBlackBox("Deny").WithExpectedDecision(ApplicationDecisionStatus.Declined).Build();
-            var riskWorkflows = Application.GetWorkflowsForApplication(application.Id);
+            var customerBuilder = CustomerBuilder.New();
+            Application application;
+
+            var customer = Config.AUT == AUT.Wb
+                               ? customerBuilder.WithMiddleName(RiskMask.TESTApplicationDeviceNotOnBlacklist).Build()
+                               : customerBuilder.WithEmployer(RiskMask.TESTDeviceNotOnBlacklist).Build();
+            
+            if (Config.AUT == AUT.Wb)
+            {
+                var organisation = OrganisationBuilder.New(customer).Build();
+                application =ApplicationBuilder.New(customer,organisation).WithIovationBlackBox("Deny").WithExpectedDecision(ApplicationDecisionStatus.Declined).Build();
+            }
+            else
+                application =
+                    ApplicationBuilder.New(customer).WithIovationBlackBox("Deny").WithExpectedDecision(
+                        ApplicationDecisionStatus.Declined).Build();
+            var riskWorkflows = Application.GetWorkflowsForApplication(application.Id,RiskWorkflowTypes.MainApplicant);
             Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
             Assert.Contains(Application.GetExecutedCheckpointDefinitionsForRiskWorkflow(riskWorkflows[0].WorkflowId, RiskCheckpointStatus.Failed), Get.EnumToString(RiskCheckpointDefinitionEnum.HardwareBlacklistCheck));
         }
 
-        [Test, AUT(AUT.Ca, AUT.Uk), JIRA("CA-1735")]
-		public void CheckpointApplicationDeviceNotOnBlacklistAccept()
+        [Test, AUT(AUT.Ca, AUT.Uk, AUT.Wb), JIRA("CA-1735", "SME-130")]
+		public void CheckpointApplicationPassesIovation_LoanIsAccepted()
         {
-            Customer customer = CustomerBuilder.New().WithEmployer(_testMask).Build();
-            Application application = ApplicationBuilder.New(customer).WithIovationBlackBox("Allow").WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
-            var riskWorkflows = Application.GetWorkflowsForApplication(application.Id);
+            var customerBuilder = CustomerBuilder.New();
+            Application application;
+
+            var customer = Config.AUT == AUT.Wb
+                               ? customerBuilder.WithMiddleName(RiskMask.TESTApplicationDeviceNotOnBlacklist).Build()
+                               : customerBuilder.WithEmployer(RiskMask.TESTDeviceNotOnBlacklist).Build();
+
+            if (Config.AUT == AUT.Wb)
+            {
+                var organisation = OrganisationBuilder.New(customer).Build();
+                application = ApplicationBuilder.New(customer, organisation).WithIovationBlackBox("Allow").WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
+            }
+            else
+                application =
+                    ApplicationBuilder.New(customer).WithIovationBlackBox("Allow").WithExpectedDecision(
+                        ApplicationDecisionStatus.Accepted).Build();
+
+            var riskWorkflows = Application.GetWorkflowsForApplication(application.Id,RiskWorkflowTypes.MainApplicant);
             Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
             Assert.Contains(Application.GetExecutedCheckpointDefinitionsForRiskWorkflow(riskWorkflows[0].WorkflowId, RiskCheckpointStatus.Verified), Get.EnumToString(RiskCheckpointDefinitionEnum.HardwareBlacklistCheck));
         }
 
+        [Test, AUT(AUT.Ca, AUT.Uk, AUT.Wb), JIRA("CA-1735", "SME-130")]
+        public void CheckpointApplicationGetsIovationReview_LoanIsAccepted()
+        {
+            var customerBuilder = CustomerBuilder.New();
+            Application application;
+
+            var customer = Config.AUT == AUT.Wb
+                               ? customerBuilder.WithMiddleName(RiskMask.TESTApplicationDeviceNotOnBlacklist).Build()
+                               : customerBuilder.WithEmployer(RiskMask.TESTDeviceNotOnBlacklist).Build();
+
+            if (Config.AUT == AUT.Wb)
+            {
+                var organisation = OrganisationBuilder.New(customer).Build();
+                application = ApplicationBuilder.New(customer, organisation).WithIovationBlackBox("Review").WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
+            }
+            else
+                application =
+                    ApplicationBuilder.New(customer).WithIovationBlackBox("Review").WithExpectedDecision(
+                        ApplicationDecisionStatus.Accepted).Build();
+
+            var riskWorkflows = Application.GetWorkflowsForApplication(application.Id, RiskWorkflowTypes.MainApplicant);
+            Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
+            Assert.Contains(Application.GetExecutedCheckpointDefinitionsForRiskWorkflow(riskWorkflows[0].WorkflowId, RiskCheckpointStatus.Verified), Get.EnumToString(RiskCheckpointDefinitionEnum.HardwareBlacklistCheck));
+        }
+
+        [Test, AUT(AUT.Ca, AUT.Uk, AUT.Wb), JIRA("CA-1735", "SME-130")]
+        public void CheckpointApplicationGetsIovationUnknown_LoanIsAccepted()
+        {
+            var customerBuilder = CustomerBuilder.New();
+            Application application;
+
+            var customer = Config.AUT == AUT.Wb
+                               ? customerBuilder.WithMiddleName(RiskMask.TESTApplicationDeviceNotOnBlacklist).Build()
+                               : customerBuilder.WithEmployer(RiskMask.TESTDeviceNotOnBlacklist).Build();
+
+            if (Config.AUT == AUT.Wb)
+            {
+                var organisation = OrganisationBuilder.New(customer).Build();
+                application = ApplicationBuilder.New(customer, organisation).WithIovationBlackBox("Unknown").WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
+            }
+            else
+                application =
+                    ApplicationBuilder.New(customer).WithIovationBlackBox("Unknown").WithExpectedDecision(
+                        ApplicationDecisionStatus.Accepted).Build();
+
+            var riskWorkflows = Application.GetWorkflowsForApplication(application.Id, RiskWorkflowTypes.MainApplicant);
+            Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
+            Assert.Contains(Application.GetExecutedCheckpointDefinitionsForRiskWorkflow(riskWorkflows[0].WorkflowId, RiskCheckpointStatus.Verified), Get.EnumToString(RiskCheckpointDefinitionEnum.HardwareBlacklistCheck));
+        }
     }
 }
