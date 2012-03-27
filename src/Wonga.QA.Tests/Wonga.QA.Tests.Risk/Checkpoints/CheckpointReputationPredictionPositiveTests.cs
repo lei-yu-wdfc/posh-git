635a1f2477a7f87ccd6a9b06750245a0c8c6f4bf
Adrian Murphy <adrian.murphy@wonga.com>
2012-03-27 12:17:45 +0100

    Removed Pending attribute from CheckpointReputationPredictionPositiveTests

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointReputationPredictionPositiveTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointReputationPredictionPositiveTests.cs
@@ -12,7 +12,7 @@ using Wonga.QA.Tests.Core;
 namespace Wonga.QA.Tests.Risk.Checkpoints
 {
 	
-    [Pending, AUT(AUT.Za)]
+    [AUT(AUT.Za)]
 	class CheckpointReputationPredictionPositiveTests
 	{
 		private const RiskMask TestMask = RiskMask.TESTReputationtPredictionPositive;
