7e1b0a43e9442245c14e6a87e27af4bec27988e4
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-03-28 08:31:43 +0100

    CaLnJourney

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Journey/CaLnJourney.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.UI.UiElements.Pages;
+
+namespace Wonga.QA.Framework.UI
+{
+    class CaLnJourney : ILnConsumerJourney
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public BasePage CurrentPage { get; set; }
+
+        public CaLnJourney(BasePage basePage)
+        {
+            FirstName = Get.GetName();
+            LastName = Get.RandomString(10);
+        }
+        public ILnConsumerJourney ApplyForLoan(int amount, int duration)
+        {
+            
+
+        }
+
+        public ILnConsumerJourney FillApplicationDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILnConsumerJourney WaitForAcceptedPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILnConsumerJourney WaitForDeclinedPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILnConsumerJourney FillAcceptedPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILnConsumerJourney GoToMySummaryPage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
