ba915115de6165d545d92b4552645fa6e84e9da9
Micky Doyle <michael.doyle@wonga.com>
2012-03-28 16:32:01 +0100

    (re) Auto-generated QAF MSMQ Framework

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Msmq/Messages/HsbcCashOutStopUkCommand.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Wonga.QA.Framework.Msmq
+{
+    /// <summary> Wonga.BankGateway.InternalMessages.HSBC.Uk.HsbcCashOutStopMessage </summary>
+    [XmlRoot("HsbcCashOutStopMessage", Namespace = "Wonga.BankGateway.InternalMessages.HSBC.Uk", DataType = "")]
+    public partial class HsbcCashOutStopUkCommand : MsmqMessage<HsbcCashOutStopUkCommand>
+    {
+        public Byte ServiceId { get; set; }
+    }
+}
