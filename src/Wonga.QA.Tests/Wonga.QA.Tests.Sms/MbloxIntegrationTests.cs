50c818d497334fda4e23b0c2333d903677fd17ee
svyatoslavkravchenko <svyatoslavkravchenko@gmail.com>
2012-03-29 18:43:40 +0300

    UK-510 - As a business I want to integrate with mBlox SMS provider for all SMS's so that we have the realiability to send SMS's during peak times - Acceptance Tests.

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Sms/MbloxIntegrationTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Sms/MbloxIntegrationTests.cs
@@ -16,10 +16,11 @@ namespace Wonga.QA.Tests.Sms
         private ServiceConfigurationEntity _configurationEntity = null;
 
         private const string TEST_PHONE_NUMBER = "4407786777486";
-        private const string TEST_MESSAGE_TEXT = "Testing request for sms message send";
-        private const string RESERVE_PROVIDER_KEY = "SMS.MBLOX_USE_RESERVE";
-        private const string USE_ANOTHER_PROVIDER = "true";
-        private const string USE_MBLOX_PROVIDER = "false";
+        private const string MBLOX_MESSAGE_TEXT_TEST = "Sending test message from acceptance tests with using Mblox provider";
+        private const string ZONG_MESSAGE_TEXT_TEST = "Sending test message from acceptance tests with using another  provider";
+        private const string RESERVE_PROVIDER_KEY = "SMS.PROVIDER_NAME";
+        private const string USE_ANOTHER_PROVIDER = "ZONG";
+        private const string USE_MBLOX_PROVIDER = "MBLOX";
 
         [SetUp]
         public void init()
@@ -32,7 +33,7 @@ namespace Wonga.QA.Tests.Sms
         {
             var request = new SendSimpleSmsCommand()
             {
-                MessageText = MbloxIntegrationTests.TEST_MESSAGE_TEXT,
+                MessageText = MbloxIntegrationTests.MBLOX_MESSAGE_TEXT_TEST,
                 ToNumber = MbloxIntegrationTests.TEST_PHONE_NUMBER
             };
             Drive.Msmq.Sms.Send(request);
@@ -42,14 +43,16 @@ namespace Wonga.QA.Tests.Sms
 
             Assert.IsTrue(lastMessage.MobilePhoneNumber.Equals(request.ToNumber));
             Assert.IsTrue(lastMessage.MessageText.Equals(request.MessageText));
+            Assert.IsTrue(_configurationEntity.Value.Equals(MbloxIntegrationTests.USE_MBLOX_PROVIDER));
             Assert.IsNull(lastMessage.ErrorMessage);
+            Assert.IsNull(lastMessage);
         }
 
         [Test, AUT(AUT.Uk), JIRA("UK-510"),]
         public void SwitchToAnotherProviderAndSendSmsRequest()
         {
 
-            if (_configurationEntity.Value.Equals(MbloxIntegrationTests.USE_MBLOX_PROVIDER))
+            if (!_configurationEntity.Value.Equals(MbloxIntegrationTests.USE_ANOTHER_PROVIDER))
             {
                 _configurationEntity.Value = MbloxIntegrationTests.USE_ANOTHER_PROVIDER;
                 _configurationEntity.Submit();
@@ -57,7 +60,7 @@ namespace Wonga.QA.Tests.Sms
 
             var request = new SendSimpleSmsCommand()
             {
-                MessageText = MbloxIntegrationTests.TEST_MESSAGE_TEXT,
+                MessageText = MbloxIntegrationTests.ZONG_MESSAGE_TEXT_TEST,
                 ToNumber = MbloxIntegrationTests.TEST_PHONE_NUMBER
             };
             Drive.Msmq.Sms.Send(request);
@@ -68,6 +71,7 @@ namespace Wonga.QA.Tests.Sms
             Assert.IsTrue(lastMessage.MobilePhoneNumber.Equals(request.ToNumber));
             Assert.IsTrue(lastMessage.MessageText.Equals(request.MessageText));
             Assert.IsNull(lastMessage.ErrorMessage);
+            Assert.IsNull(lastMessage);
         }
     }
 }
