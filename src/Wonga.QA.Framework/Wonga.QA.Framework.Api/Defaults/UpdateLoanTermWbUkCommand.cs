fe6370c6e8eec6240ae9dc964502bf74ef80fa79
vaclavwonga <vaclav.haken@wonga.com>
2012-03-29 16:08:40 +0100

    SME-232 tests

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Defaults/UpdateLoanTermWbUkCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.Framework.Api
+{
+    public partial class UpdateLoanTermWbUkCommand
+    {
+        public override void Default()
+        {
+            Term = 25; //make the default here different from the term in CreateBusinessFixedInstallmentLoanApplicationWbUkCommand
+        }
+    }
+}
