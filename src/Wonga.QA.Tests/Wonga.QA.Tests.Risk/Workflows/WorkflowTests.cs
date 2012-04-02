eadd2b2ef4f844829bc9bd2ae3a9d9d1e6cc95eb
Adrian Murphy <adrian.murphy@wonga.com>
2012-04-02 15:57:24 +0100

    Finished WorkflowApplicationL0Accepted()

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Workflows/WorkflowTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Workflows/WorkflowTests.cs
@@ -15,6 +15,35 @@ namespace Wonga.QA.Tests.Risk.Workflows
 	[AUT(AUT.Za)]
 	public partial class WorkflowTests
 	{
+		private Dictionary<string, string> _originalServiceConfiguration = new Dictionary<string, string>();
+		
+		[FixtureSetUp]
+		public void FixtureSetUp()
+		{
+			switch (Config.AUT)
+			{
+				case (AUT.Za):
+					{
+						var mock = Drive.Db.GetServiceConfiguration("Mocks.HyphenAHVWebServiceEnabled");
+
+						_originalServiceConfiguration.Add("Mocks.HyphenAHVWebServiceEnabled", mock.Value);
+						Drive.Db.SetServiceConfiguration("Mocks.HyphenAHVWebServiceEnabled", "false");
+					}
+					break;
+
+				default:
+					{
+						return;
+					}
+			}
+		}
+
+		[FixtureTearDown]
+		public void FixtureTearDown()
+		{
+			Drive.Db.SetServiceConfigurations(_originalServiceConfiguration);
+		}
+
 		[Test, AUT(AUT.Za), Explicit]
 		public void WorkflowL0SingleWorkflowUsed()
 		{
@@ -34,7 +63,7 @@ namespace Wonga.QA.Tests.Risk.Workflows
 		{
 			var customer = BuildCustomerToBeAccepted();
 
-			ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Pending).WithIovationBlackBox("Accept").WithExpectedDecision(ApplicationDecisionStatus.Declined).Build();
+			ApplicationBuilder.New(customer).WithIovationBlackBox("Allow").WithExpectedDecision(ApplicationDecisionStatus.ReadyToSign).Build();
 		}
 
 		#region Helpers
@@ -51,7 +80,11 @@ namespace Wonga.QA.Tests.Risk.Workflows
 							.WithForename("ANITHA")
 							.WithSurname("ESSACK")
 							.WithDateOfBirth(new Date(new DateTime(1957, 12, 19)))
-							.WithNationalNumber("5712190106083").Build();
+							.WithNationalNumber("5712190106083")
+							.WithMobileNumber(Get.GetMobilePhone())
+							.WithBankAccountNumber(Get.GetBankAccountNumber())
+							.WithPostcodeInAddress(Get.GetPostcode())
+							.Build();
 					}
 					break;
 
