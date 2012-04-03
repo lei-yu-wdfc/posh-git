5d804f2b811f05ddca58fa7d2ba05db317f689f1
Piotr Walat <piotr.walat@wonga.com>
2012-04-03 10:35:36 +0100

    Redundant With() removed from Customer.cs

--- src/Wonga.QA.Framework/Wonga.QA.Framework/Customer.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework/Customer.cs
@@ -81,7 +81,7 @@ namespace Wonga.QA.Framework
                 r.CardType = cardType;
             });
             Drive.Api.Commands.Post(cmd);
-            Do.With.Until(() => Drive.Db.Payments.PersonalPaymentCards
+            Do.Until(() => Drive.Db.Payments.PersonalPaymentCards
                          .Single(c => c.AccountId == this.Id
                                       && c.PaymentCardsBaseEntity.Type == cardType
                                       && c.PaymentCardsBaseEntity.MaskedNumber == cardNumber.MaskedCardNumber()));
