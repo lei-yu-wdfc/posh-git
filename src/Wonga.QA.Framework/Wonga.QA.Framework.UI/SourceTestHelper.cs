df768ecc7346a5bb83759d71be27ec01b79c40c3
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-04-03 14:08:53 +0100

    .F.Ui.SourceTestHelper added
    .F.Ui.Journey deleted

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/SourceTestHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.Framework.Assertions;
+using MbUnit.Framework;
+
+namespace Wonga.QA.Framework.UI
+{
+    public class SourceTestHelper
+    {
+        public static void VerifyDoubleclickTagValues(UiClient client, List<KeyValuePair<string, string>> list)
+        {
+            foreach(var pair in list)
+            {
+                try
+                {
+                    Assert.IsTrue(client.Source().Contains(String.Format("{0}: {1}", pair.Key, pair.Value)));
+                }
+                catch(AssertionException exception)
+                {
+                    throw new AssertionException(String.Format("{0} - failed on {1}: {2}",exception.Message,pair.Key,pair.Value));
+                }
+            }
+        }
+    }
+}
