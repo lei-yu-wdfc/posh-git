ea323a921e13f3505886d03733f0f6acca43abc1
Mihail Podobivsky <mihail.podobivsky@softheme.com>
2012-04-04 13:38:45 +0300

    QA-170

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Pages/Common/HowItWorksPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wonga.QA.Framework.UI.Elements;
+using Wonga.QA.Framework.UI.UiElements.Pages;
+
+namespace Wonga.QA.Framework.UI.Ui.Pages.Common
+{
+    class HowItWorksPage : BasePage
+    {
+        public SlidersElement Sliders { get; set; }
+
+        public HowItWorksPage(UiClient client)
+            : base(client)
+        {
+            Sliders = new SlidersElement(this);
+        }
+    }
+}
