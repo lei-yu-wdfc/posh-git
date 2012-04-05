28cd60f87196f63fff7ed2e07dd3b91ce2004868
Gus Geraghty <gus.geraghty@wonga.com>
2012-04-05 09:21:53 +0100

    Main topup page and small sliders complete but not tested

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Pages/Common/TopupRequestPage.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Pages/Common/TopupRequestPage.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using MbUnit.Framework;
 using OpenQA.Selenium;
 using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.UI.Elements;
 using Wonga.QA.Framework.UI.Mappings;
 
 namespace Wonga.QA.Framework.UI.UiElements.Pages.Common
@@ -15,7 +16,7 @@ namespace Wonga.QA.Framework.UI.UiElements.Pages.Common
         private IWebElement _interestAndFees;
         private IWebElement _grandTotal;
         private IWebElement _topupAmount;
-        private IWebElement TopupRequestPageSliders { get; set; }
+        private SmallTopupSliders Sliders { get; set; }
         public TopupRequestPage(UiClient client) : base(client)
         {
             _submitButton = Content.FindElement(By.CssSelector(Ui.Get.TopupRequestPage.TopupRequestPageSubmitButton));
@@ -34,43 +35,30 @@ namespace Wonga.QA.Framework.UI.UiElements.Pages.Common
         public void SubmitButtonClick()
         {
             _submitButton.Click();
+            //return new TopupProcessingPage(Client);
         }
+       
 
-        public String GetTotalToRepay()
-        {
-            _grandTotal = Content.FindElement(By.CssSelector(Ui.Get.TopupRequestPage.TopupRequestPageNewGrandTotal));
-            return _grandTotal.Text;
-        }
-
-        public String GetInterestAndFees()
-        {
-            _interestAndFees = Content.FindElement(By.CssSelector(Ui.Get.TopupRequestPage.TopupRequestPageNewInterestAndFees));
-            return _interestAndFees.Text;
-        }
-
-        public String GetTopupAmount()
-        {
-            _topupAmount = Content.FindElement(By.CssSelector(Ui.Get.TopupRequestPage.TopupRequestPageNewCreditRequest));
-            return _topupAmount.Text;
-        }
-/*
         public void IsTopupRequestPageSliderReturningCorrrectValuesOnChange(string applicationId)
         {
-            var topupRequestAmount = 50;
-            //TopupRequestPage.TopupRequestPageSliders.HowMuch = topupRequestAmount;
+            const string topupRequestAmount = "50";
+            Sliders.HowMuch = topupRequestAmount;
             
             //Expected values
-           _response = Drive.Api.Queries.Post(new GetFixedTermLoanTopupCalculationQuery {ApplicationId = applicationId, TopupAmount = topupRequestAmount});
+            var api = new ApiDriver();
+            _response = api.Queries.Post(new GetFixedTermLoanTopupCalculationQuery
+                                 {ApplicationId = applicationId, TopupAmount = topupRequestAmount});
+           
 
             var totalRepayable = _response.Values["TotalRepayable"].Single();
             var interestAndFees = _response.Values["InterestAndFeesAmount"].Single();
             
             //check the output matches the returned values for 50 quid topupRequestAmount
-            Assert.AreEqual(expectedValue: TopupRequestPage.GetTotalToRepay().Remove(0, 1), actualValue: totalRepayable);
-            Assert.AreEqual(expectedValue: TopupRequestPage.GetTopupAmount().Remove(0, 1), actualValue: topupRequestAmount);
-            Assert.AreEqual(expectedValue: TopupRequestPage.GetInterestAndFees().Remove(0, 1), actualValue: interestAndFees);
+            Assert.AreEqual(expectedValue: Sliders.GetTotalToRepay.Remove(0, 1), actualValue: totalRepayable);
+            Assert.AreEqual(expectedValue: Sliders.GetTotalAmount.Remove(0, 1), actualValue: topupRequestAmount);
+            Assert.AreEqual(expectedValue: Sliders.GetTotalFees.Remove(0, 1), actualValue: interestAndFees);
         }
- */
+
 
     }
 
