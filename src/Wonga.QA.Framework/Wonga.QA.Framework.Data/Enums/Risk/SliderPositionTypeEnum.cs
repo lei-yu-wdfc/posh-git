e97213a1dbeb6a2bf0f320748063fab7e5e31968
alexpricope <alex.pricope@wonga.com>
2012-04-11 16:35:22 +0100

    Added Panni's Nightmare test for payments calculations

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Data/Enums/Risk/SliderPositionTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.Framework.Data.Enums.Risk
+{
+    public enum SliderPositionType
+    {
+        Custom,
+        Default,
+        Max,
+        Min,
+    }
+}
