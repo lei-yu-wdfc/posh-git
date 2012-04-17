b4bbc8d7883f9a282b93c99adbd301f60e634da7
Ziming Wang <ziming.wang@wonga.com>
2012-04-17 12:46:25 +0100

    ZA-1960 Add AT for repayment arrangement

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Cs/Requests/CsExtendFixedTermLoanCommand.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Cs/Requests/CsExtendFixedTermLoanCommand.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.Xml.Serialization;
 
@@ -15,3 +16,22 @@ namespace Wonga.QA.Framework.Cs
         public Object ExtensionDate { get; set; }
     }
 }
+=======
+using System;
+using System.Xml.Serialization;
+
+namespace Wonga.QA.Framework.Cs
+{
+    /// <summary> Wonga.Payments.Csapi.Commands.CsExtendFixedTermLoan </summary>
+    [XmlRoot("CsExtendFixedTermLoan")]
+    public partial class CsExtendFixedTermLoanCommand : CsRequest<CsExtendFixedTermLoanCommand>
+    {
+        public Object ApplicationId { get; set; }
+        public Object PaymentCardId { get; set; }
+        public Object CV2 { get; set; }
+        public Object PartPaymentAmount { get; set; }
+        public Object AgentId { get; set; }
+        public Object ExtensionDate { get; set; }
+    }
+}
+>>>>>>> ZA-1960 Add AT for repayment arrangement
---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Cs/RepaymentArrangementTests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Linq;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api.Exceptions;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.Cs;
+using Wonga.QA.Framework.Db.Payments;
+using Wonga.QA.Framework.Msmq;
+using Wonga.QA.Tests.Core;
+using Wonga.QA.Tests.Payments.Helpers;
+
+
+namespace Wonga.QA.Tests.Cs
+{
+    public class RepaymentArrangementTests
+    {
+        private bool _bankGatewayTestModeOriginal;
+
+        [FixtureSetUp]
+        public void FixtureSetup()
+        {
+            _bankGatewayTestModeOriginal = ConfigurationFunctions.GetBankGatewayTestMode();
+            ConfigurationFunctions.SetBankGatewayTestMode(false);
+        }
+
+        [FixtureTearDown]
+        public void FixtureTearDown()
+        {
+            ConfigurationFunctions.SetBankGatewayTestMode(_bankGatewayTestModeOriginal);
+        }
+
+        [Test, AUT(AUT.Za)]
+        public void IsAllowedTest()
+        {
+            Customer customer = CustomerBuilder.New().Build();
+            Application application = ApplicationBuilder.New(customer).Build();
+
+            application.PutApplicationIntoArrears(20);
+
+            var response = Drive.Cs.Queries.Post(new CsGetRepaymentArrangementAllowedQuery()
+                    {
+                        ApplicationId = application.Id
+                    });
+
+            Assert.IsNotNull(response);
+            Assert.IsTrue(bool.Parse(response.Values["IsAllowed"].Single()));    
+        }
+
+        [Test, AUT(AUT.Za)]
+        public void CreateTest()
+        {
+            Customer customer = CustomerBuilder.New().Build();
+            Application application = ApplicationBuilder.New(customer).Build();
+
+            application.PutApplicationIntoArrears(20);
+            Drive.Cs.Commands.Post(new Framework.Cs.CreateRepaymentArrangementCommand()
+                                       {
+                                           AccountId = customer.Id,
+                                           ApplicationId = application.Id,
+                                           EffectiveBalance = 100,
+                                           RepaymentAmount = 100,
+                                           ArrangementDetails = new[]
+											                   	{
+											                   		new RepaymentArrangementDetailCsapi{Amount = 49, Currency = CurrencyCodeIso4217Enum.ZAR, DueDate = DateTime.Today},
+																	new RepaymentArrangementDetailCsapi{Amount = 51, Currency = CurrencyCodeIso4217Enum.ZAR, DueDate = DateTime.Today.AddDays(7)}
+											                   	}
+                                       });
+
+            var dbApplication = Drive.Db.Payments.Applications.Single(a => a.ExternalId == application.Id);
+            Do.Until(() => Drive.Db.Payments.RepaymentArrangements.Single(x => x.ApplicationId == dbApplication.ApplicationId));
+            var repaymentArrangement = Drive.Db.Payments.RepaymentArrangements.Single(x => x.ApplicationId == dbApplication.ApplicationId);
+            Assert.AreEqual(2, repaymentArrangement.RepaymentArrangementDetails.Count);
+            Assert.IsNotNull(Drive.Db.Payments.Transactions.Where(x => x.ApplicationId == dbApplication.ApplicationId && x.Type == "SuspendInterestAccrual"));
+        }
+
+        [Test, AUT(AUT.Za)]
+        public void GetTest()
+        {
+            Customer customer = CustomerBuilder.New().Build();
+            Application application = ApplicationBuilder.New(customer).Build();
+
+            application.PutApplicationIntoArrears(20);
+            Drive.Cs.Commands.Post(new Framework.Cs.CreateRepaymentArrangementCommand()
+            {
+                AccountId = customer.Id,
+                ApplicationId = application.Id,
+                EffectiveBalance = 100,
+                RepaymentAmount = 100,
+                ArrangementDetails = new[]
+											                   	{
+											                   		new RepaymentArrangementDetailCsapi{Amount = 49, Currency = CurrencyCodeIso4217Enum.ZAR, DueDate = DateTime.Today},
+																	new RepaymentArrangementDetailCsapi{Amount = 51, Currency = CurrencyCodeIso4217Enum.ZAR, DueDate = DateTime.Today.AddDays(7)}
+											                   	}
+            });
+
+            var response = Drive.Cs.Queries.Post(new GetRepaymentArrangementsQuery() {ApplicationId = application.Id});
+            Assert.IsNotNull(response);
+        }
+
+        [Test, AUT(AUT.Za)]
+        public void GetParametersTest()
+        {
+            Customer customer = CustomerBuilder.New().Build();
+            Application application = ApplicationBuilder.New(customer).Build();
+            application.PutApplicationIntoArrears(20);
+
+            var response = Drive.Cs.Queries.Post(new GetRepaymentArrangementParametersQuery() {AccountId = customer.Id});
+            Assert.IsNotNull(response);
+        }
+
+        [Test, AUT(AUT.Za)]
+        public void GetCalculationTest()
+        {
+            Customer customer = CustomerBuilder.New().Build();
+            Application application = ApplicationBuilder.New(customer).Build();
+            application.PutApplicationIntoArrears(20);
+
+            var response = Drive.Cs.Queries.Post(new GetRepaymentArrangementCalculationQuery()
+                                                     {
+                                                         AccountId = customer.Id,
+                                                         RepaymentFrequency = PaymentFrequency.Monthly,
+                                                         NumberOfMonths = 4,
+                                                         FirstRepaymentDate = DateTime.Today.AddDays(7)
+                                                     });
+            Assert.IsNotNull(response);            
+        }
+
+        [Test, AUT(AUT.Za)]
+        public void CancelTest()
+        {
+            Customer customer = CustomerBuilder.New().Build();
+            Application application = ApplicationBuilder.New(customer).Build();
+            application.PutApplicationIntoArrears(20);
+            Drive.Cs.Commands.Post(new Framework.Cs.CreateRepaymentArrangementCommand()
+            {
+                AccountId = customer.Id,
+                ApplicationId = application.Id,
+                EffectiveBalance = 100,
+                RepaymentAmount = 100,
+                ArrangementDetails = new[]
+											                   	{
+											                   		new RepaymentArrangementDetailCsapi{Amount = 49, Currency = CurrencyCodeIso4217Enum.ZAR, DueDate = DateTime.Today},
+																	new RepaymentArrangementDetailCsapi{Amount = 51, Currency = CurrencyCodeIso4217Enum.ZAR, DueDate = DateTime.Today.AddDays(7)}
+											                   	}
+            });
+
+            var dbApplication = Drive.Db.Payments.Applications.Single(a => a.ExternalId == application.Id);
+            Do.Until(() => Drive.Db.Payments.RepaymentArrangements.Single(x => x.ApplicationId == dbApplication.ApplicationId));
+            var repaymentArrangement = Drive.Db.Payments.RepaymentArrangements.Single(x => x.ApplicationId == dbApplication.ApplicationId);
+
+            Drive.Cs.Commands.Post(new Framework.Cs.CancelRepaymentArrangementCommand()
+                                       {
+                                           RepaymentArrangementId = repaymentArrangement.ExternalId
+                                       });
+
+            Do.Until(() => Drive.Db.Payments.RepaymentArrangements.Single(x => x.ApplicationId == dbApplication.ApplicationId).CanceledOn != null);
+        }
+
+        public enum PaymentFrequency
+        {
+            Every2Weeks,
+            Every4Weeks,
+            Monthly,
+            Weekly
+        }
+
+        //Needed for serialization in CreateExtendedRepaymentArrangementCommand
+        public class RepaymentArrangementDetailCsapi
+        {
+            public decimal Amount { get; set; }
+            public CurrencyCodeIso4217Enum Currency { get; set; }
+            public DateTime DueDate { get; set; }
+        }
+    }
+}
