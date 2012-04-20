1b1d5d42e22d8477fa8a1e4e5215296ac23fd214
ragnarekmix <mihail.podobivsky@softheme.com>
2012-04-20 12:11:03 +0300

    QA-186
    done for Ca and Za

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Sections/AccountDetailsSection.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Sections/AccountDetailsSection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using OpenQA.Selenium;
+using Wonga.QA.Framework.Core;
 using Wonga.QA.Framework.UI.UiElements.Pages;
 using Wonga.QA.Framework.UI.Mappings;
 
@@ -31,6 +32,11 @@ namespace Wonga.QA.Framework.UI.UiElements.Sections
         {
             var passwordWarning = Section.FindElement(By.CssSelector(Ui.Get.AccountDetailsSection.PasswordErrorForm));
             return passwordWarning.Text.Equals("Passwords must match");
+        }
+        public bool IsPasswordInvalidFormatWarningOccured()
+        {
+            var passwordWarning = Section.FindElement(By.CssSelector(Ui.Get.AccountDetailsSection.PasswordErrorForm));
+            return passwordWarning.Text.Equals("Your password must be 8 or more characters and must include a capital letter and a number.");
         }
     }
 }
