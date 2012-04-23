3637c33f3aa427f3f4ba55e3a38bb5a2623aefbb
unknown <orizu.nwokeji@QB-WK-160.wonga.com>
2012-04-23 16:47:07 +0100

    Extension Error Page object started

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Pages/Common/ExtensionErrorPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using NHamcrest.Core;
+using OpenQA.Selenium;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.UI.UiElements.Pages.Interfaces;
+using Wonga.QA.Framework.UI.Mappings;
+
+namespace Wonga.QA.Framework.UI.Ui.Pages.Common
+{
+    class ExtensionErrorPage
+    {
+        private IWebElement _accountLink;
+
+        public ExtensionErrorPage(UiClient client) : base(client)
+        {
+            Assert.That(Headers, Has.Item("Ouch! We're sorry"));
+            _accountLink = Content.FindElement(By.CssSelector(Ui.Get.ExtensionErrorPage.AccountLink));
+        }
+
+        public bool IsDealDonePageJiffyNotPresent()
+        {
+            bool tokenResult = Content.Driver().PageSource.Contains("jiffy");
+            return !tokenResult;
+        }
+
+        public IApplyPage ContinueToMyAccount()
+        {
+            _accountLink.Click();
+            return new MySummaryPage(Client);
+        }
+    }
+}
