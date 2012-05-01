3e64b8f835e9e772a151a9bd29482a4a3f95e8f8
svyatoslav <svyatoslavkravchenko@gmail.com>
2012-05-01 01:45:25 -0700

    PP - 1 - As a business I want to display a prepaid card landing page to eligible customers so we can advertise the product - Acceptance Tests

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/Prepaid/PrepaidAccountTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Ui.Prepaid
+{
+    class PrepaidAccountTests : UiTest
+    {
+        [Test, AUT(AUT.Uk), JIRA("PP-1")]
+        public void DisplayPrepaidCardSubnavForEligibleCustomer()
+        {
+            Customer eligibleCustomer = CustomerBuilder.New().Build();
+            CustomerOperations.CreateMarketingEligibility(eligibleCustomer.Id, true);
+
+            var loginPage = Client.Login();
+            var summaryPage = loginPage.LoginAs(eligibleCustomer.GetEmail());
+            summaryPage.IsPrepaidCardButtonExist();
+        }
+    }
+}
