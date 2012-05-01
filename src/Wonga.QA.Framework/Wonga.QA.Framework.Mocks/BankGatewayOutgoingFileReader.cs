8d76f857ab16df88d4f47039c532a07a8d1d2897
Luke Rickard <luke.rickard@wonga.com>
2012-05-01 10:13:25 +0100

    added new functionality to read from new bank gateway mock tables

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Mocks/BankGatewayOutgoingFileReader.cs
@@ -0,0 +1,24 @@
+﻿using Wonga.QA.Framework.Data;
+
+namespace Wonga.QA.Framework.Mocks
+{
+    public class BankGatewayOutgoingFileReader
+    {
+        private readonly DataDriver _dataDriver;
+
+        public BankGatewayOutgoingFileReader()
+        {
+            _dataDriver = new DataDriver();
+
+        }
+
+        public bool VerifyPinSentForBankAccountNumber(string bankAccountNumber, string pinNumber)
+        {
+            byte[] fileData = _dataDriver.QaData.Db.OutgoingBankGatewayFile.FindByBankAccountNumber(bankAccountNumber).FileData;
+
+            var fileDataString = System.Text.Encoding.ASCII.GetString(fileData);
+
+            return fileDataString.Contains(pinNumber);
+        }
+    }
+}
