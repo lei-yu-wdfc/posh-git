d74de87c2bd5955550acbb2ef55c22beabede0fe
Charlie Barker <charlie.barker@wonga.com>
2012-05-03 11:00:50 +0100

    Ran generators for the API, commented out tests that refernced deleted cmds and queries

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/GetRepaymentDetailsTest.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/GetRepaymentDetailsTest.cs
@@ -11,38 +11,38 @@ namespace Wonga.QA.Tests.Payments
 {
     class GetRepaymentDetailsTest
     {
-        [Test, AUT(AUT.Uk), JIRA("UK-1112")]
+        [Test, AUT(AUT.Uk), JIRA("UK-1112"), Pending("Commented out until query is added to the API")]
         public void GetRepaymentDetailsHandler()
         {
-            Guid aplId = Guid.Parse("AC36B516-6096-41D9-BD13-BC2661EC5A5B");
+            //Guid aplId = Guid.Parse("AC36B516-6096-41D9-BD13-BC2661EC5A5B");
 
-            var parm =  Drive.Api.Queries.Post(new GetRepaymentDetailsQuery { ApplicationId = aplId });
+            //var parm =  Drive.Api.Queries.Post(new GetRepaymentDetailsQuery { ApplicationId = aplId });
 
-            var repDate = parm.Values["RepaymentDate"];
-            var repAmount = (parm.Values["RepaymentAmount"]);
+            //var repDate = parm.Values["RepaymentDate"];
+            //var repAmount = (parm.Values["RepaymentAmount"]);
 
-            List<string > repDateArray = new List<string>();
-            foreach (var item in repDate)
-            {
-                repDateArray.Add(item);
-            }
+            //List<string > repDateArray = new List<string>();
+            //foreach (var item in repDate)
+            //{
+            //    repDateArray.Add(item);
+            //}
 
-            List<string> repAmountArray = new List<string>();
-            foreach (var it in repAmount)
-            {
-                repAmountArray.Add(it);
-            }
+            //List<string> repAmountArray = new List<string>();
+            //foreach (var it in repAmount)
+            //{
+            //    repAmountArray.Add(it);
+            //}
 
-            Assert.IsNotNull(parm);
-            Assert.AreEqual("2012-05-27", repDateArray[0]);
-            Assert.AreEqual("2012-06-27", repDateArray[1]);
-            Assert.AreEqual("2012-07-27", repDateArray[2]);
+            //Assert.IsNotNull(parm);
+            //Assert.AreEqual("2012-05-27", repDateArray[0]);
+            //Assert.AreEqual("2012-06-27", repDateArray[1]);
+            //Assert.AreEqual("2012-07-27", repDateArray[2]);
 
-            Assert.AreEqual("50", repAmountArray[0]);
-            Assert.AreEqual("50", repAmountArray[1]);
-            Assert.AreEqual("50", repAmountArray[2]);
+            //Assert.AreEqual("50", repAmountArray[0]);
+            //Assert.AreEqual("50", repAmountArray[1]);
+            //Assert.AreEqual("50", repAmountArray[2]);
 
-            Assert.AreEqual(10.17m, Decimal.Parse(parm.Values["TransactionFee"].Single()));
+            //Assert.AreEqual(10.17m, Decimal.Parse(parm.Values["TransactionFee"].Single()));
         }
     }
 }
