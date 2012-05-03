f84e2b492c9ada4f7f2410ea25a1a9391392b4b1
ragnarekmix <mihail.podobivsky@softheme.com>
2012-05-03 11:40:55 +0300

    small fix

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Ui/Sections/AccountDetailsSection.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Ui/Sections/AccountDetailsSection.cs
@@ -14,5 +14,6 @@ namespace Wonga.QA.Framework.UI.Mappings.Sections
         public String SecretAnswer { get; set; }
         public String PasswordErrorForm { get; set; }
         public String PasswordConfirmErrorForm { get; set; }
+        public String PasswordWarningMessage { get; set; }
     }
 }
