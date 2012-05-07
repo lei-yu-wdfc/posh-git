2684239acbbab8c73bb60614cb5634d9deff0df1
ragnarekmix <mihail.podobivsky@softheme.com>
2012-05-07 10:53:35 +0300

    QA-280

--- src/Wonga.QA.Tests/Wonga.QA.Tests.UI/L0JourneyTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/L0JourneyTests.cs
@@ -1279,19 +1279,25 @@ namespace Wonga.QA.Tests.Ui
                                  .CurrentPage as ProcessingPage;
         }
 
-        [Test, AUT(AUT.Ca), Category(TestCategories.Smoke), JIRA("QA-280")]
+        [Test, AUT(AUT.Ca), Category(TestCategories.Smoke), JIRA("QA-280"), Pending]
         public void L0CustomerEntersInappropriatePostcodeToPreviousAddressSectionShouldNotGoFurther()
         {
             var journey = JourneyFactory.GetL0Journey(Client.Home());
-            var processingPage = journey.ApplyForLoan(200, 10)
-                                 .FillPersonalDetails(Get.EnumToString(RiskMask.TESTEmployedMask))
-                                 .FillAddressDetails()
-                                 .FillAccountDetails()
-                                 .FillBankDetails()
-                                 .CurrentPage as ProcessingPage;
-            var acceptedPage = processingPage.WaitFor<AcceptedPage>() as AcceptedPage;
-            acceptedPage.SignConfirmCaL0(DateTime.Now.ToString("d MMM yyyy"), journey.FirstName, journey.LastName);
-            var dealDone = acceptedPage.Submit();
+            var addressPage = journey.ApplyForLoan(200, 10)
+                                 .FillPersonalDetails(Get.EnumToString(RiskMask.TESTEmployedMask)).CurrentPage as AddressDetailsPage;
+            addressPage.HouseNumber = "1403";
+            addressPage.Street = "Edward";
+            addressPage.Town = "Hearst";
+            addressPage.PostCode = "V4F3A9";
+            addressPage.PostOfficeBox = "C12345";
+            addressPage.AddressPeriod = "Less than 4 months";
+            
+            addressPage.PreviousAddresDetails.FlatNumber = "4";
+            addressPage.PreviousAddresDetails.Street = "Edward";
+            addressPage.PreviousAddresDetails.Town = "Hearst";
+            addressPage.PreviousAddresDetails.Province = "Alberta";
+            addressPage.PreviousAddresDetails.PostCode = "Q0K0K4";
+            addressPage.Next();
         }
     }
 }
