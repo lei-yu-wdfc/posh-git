938ca02c2e088fd5f7962b059a0efd63ec2ac119
Daniel Luxembourger <daniel.luxembourger@wonga.com>
2012-05-08 12:30:14 +0100

    CA-2113 Fast Payments tests (pin)

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Mocks/BankGatewayOutgoingFileReader.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Mocks/BankGatewayOutgoingFileReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Simple.Data;
 using Wonga.QA.Framework.Data;
 
 namespace Wonga.QA.Framework.Mocks
@@ -14,17 +15,53 @@ namespace Wonga.QA.Framework.Mocks
 
         public bool VerifyPinContainedInBankGatewayFileSent(string bankAccountNumber, string pinNumber)
         {
-            // There will be two files, one for cash-out and one for cash-in. Find the cash-in file, this will, be the one with the highest id
-            byte[] fileData = _dataDriver.QaData.Db.OutgoingBankGatewayFile.FindByBankAccountNumber(bankAccountNumber).FileData;
+            // There will be two files, one for cash-out and one for cash-in. The cash-in file contains the pin. It is one one with the highest id.
+            var files = _dataDriver.QaData.Db.OutgoingBankGatewayFile.FindAllByBankAccountNumber(bankAccountNumber);
 
-            var fileDataString = System.Text.Encoding.ASCII.GetString(fileData);
+            int maxId = 0;
+            dynamic maxFile = null;
+
+            foreach (var file in files)
+            {
+                if (file.Id > maxId)
+                {
+                    maxId = file.Id;
+                    maxFile = file;
+                }
+            }
+
+            if (maxFile == null)
+            {
+                return false;
+            }
+
+            string fileDataString = System.Text.Encoding.ASCII.GetString(maxFile.FileData);
 
             // look for Wonga.ca (%pin%)
-            var startIndex = fileDataString.IndexOf("Wonga.ca (", StringComparison.InvariantCultureIgnoreCase);
-            var endIndex = fileDataString.IndexOf(")", StringComparison.InvariantCultureIgnoreCase);
-            var filePin = fileDataString.Substring(startIndex, endIndex - startIndex);
+            const int originatorShortNameStartIndex = 89;
+            const int originatorShortNameLenght = 15;
+            const string originatorShortNameFormat = "Wonga.ca ({0})";
 
-            return pinNumber.Equals(filePin);
+            const int originatorLongNameStartIndex = 134;
+            const int originatorLongNameLenght = 19;
+            const string originatorLongNameFormat = "Wonga Canada ({0})";
+
+            var detail = fileDataString.Split(new [] { Environment.NewLine }, StringSplitOptions.None)[1];
+
+            string originatorShortName = detail.Substring(originatorShortNameStartIndex, originatorShortNameLenght);
+            string originatorLongName = detail.Substring(originatorLongNameStartIndex, originatorLongNameLenght);
+
+            if (originatorShortName != string.Format(originatorShortNameFormat, pinNumber))
+            {
+                return false;
+            }
+
+            if (originatorLongName != string.Format(originatorLongNameFormat, pinNumber))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
