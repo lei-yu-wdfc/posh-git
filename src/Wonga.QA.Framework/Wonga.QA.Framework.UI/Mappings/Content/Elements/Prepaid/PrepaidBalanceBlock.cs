09f1eb36a6b0305a53e828e371ed9323f66ae823
svyat <svyatoslavkravchenko@gmail.com>
2012-05-09 10:05:33 -0700

    PrepaidBalanceBloxk fix (PP-16)

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Content/Elements/Prepaid/PrepaidBalanceBlock.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Content/Elements/Prepaid/PrepaidBalanceBlock.cs
@@ -5,8 +5,8 @@ namespace Wonga.QA.Framework.UI.Mappings.Content.Elements
 {
    public  class PrepaidBalanceBlock
     {
+       public String CurrentBalance { get; set; }
        public String CashbackEarned { get; set;}
-       public String CurrentBalance { get; set;}
        public String MoneyIn { get; set;}
        public String MoneyOut { get; set; }
 
