553da3e337c346fc0ad97c068d7e558afabe023e
Adrian Murphy <adrian.murphy@wonga.com>
2012-05-14 10:14:10 +0100

    Marked tests as pending until fixed

--- src/Wonga.QA.Tests/Wonga.QA.Tests.UI/SecondLoanTest.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/SecondLoanTest.cs
@@ -23,7 +23,7 @@ namespace Wonga.QA.Tests.Ui
 {
     class SecondLoanTest : UiTest
     {
-        [Test, AUT(AUT.Za, AUT.Ca), JIRA("QA-195")]
+        [Test, AUT(AUT.Za, AUT.Ca), JIRA("QA-195"), Pending("Refreshing the page shows the correct value")]
         public void InformationAboutSecondLoanShouldBeDisplayed()
         {
             string actualRepaymentDate;
