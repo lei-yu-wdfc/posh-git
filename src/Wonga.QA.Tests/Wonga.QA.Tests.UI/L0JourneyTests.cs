7925bd66cc4b31170319efb11bc51c2bfa590f86
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-05-14 12:21:21 +0300

    Pending added to tests that uses DragAndDrop method

--- src/Wonga.QA.Tests/Wonga.QA.Tests.UI/L0JourneyTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/L0JourneyTests.cs
@@ -214,7 +214,7 @@ namespace Wonga.QA.Tests.Ui
             }
         }
 
-        [Test, AUT(AUT.Za, AUT.Ca), JIRA("QA-175")]
+        [Test, AUT(AUT.Za, AUT.Ca), JIRA("QA-175"), Pending("Wierd selenium problem")]
         public void ChangeLoanAmountAndDurationOnPersonalDetailsViaSlidersMotion()
         {
             var journey = JourneyFactory.GetL0Journey(Client.Home());
--- src/Wonga.QA.Tests/Wonga.QA.Tests.UI/SliderTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/SliderTests.cs
@@ -79,7 +79,7 @@ namespace Wonga.QA.Tests.Ui
             //maximum charge is 21$ for each 100$ borrowed for 30 days.
         }
 
-        [Test, AUT(AUT.Za, AUT.Ca), JIRA("QA-149")]
+        [Test, AUT(AUT.Za, AUT.Ca), JIRA("QA-149"), Pending("Wierd selenium problem")]
         public void ChooseLoanAmountAndDurationViaSlidersMotion()
         {
             var homePage = Client.Home();
