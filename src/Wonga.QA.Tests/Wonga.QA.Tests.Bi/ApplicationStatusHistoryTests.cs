386864d7091e731de7206cf7e6b9e6d3cd92537e
Seamus Hoban <seamus.hoban@wonga.com>
2012-05-16 16:53:42 +0100

    UK-1477 - QA tests.

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Bi/ApplicationStatusHistoryTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Bi/ApplicationStatusHistoryTests.cs
@@ -17,21 +17,22 @@ namespace Wonga.QA.Tests.Bi
         private dynamic appStatusHistoryRepo = Drive.Data.BiCustomerManagement.Db.ApplicationStatusHistory;
         private Guid _applicationId;
         private Guid _accountId;
-
+        private Customer _customer;
+        private Application _application;
         [SetUp]
         public void SetUp()
         {
-            Customer customer = CustomerBuilder.New().Build();
-            Do.Until(customer.GetBankAccount);
-            Do.Until(customer.GetPaymentCard);
+            _customer = CustomerBuilder.New().Build();
+            Do.Until(_customer.GetBankAccount);
+            Do.Until(_customer.GetPaymentCard);
             const decimal loanAmount = 222.22m;
-            Application application = ApplicationBuilder.New(customer)
-                                                        .WithLoanAmount(loanAmount)
-                                                        .WithLoanTerm(7)
-                                                        .Build();
+            _application = ApplicationBuilder.New(_customer)
+                                             .WithLoanAmount(loanAmount)
+                                             .WithLoanTerm(7)
+                                             .Build();
 
-            _applicationId = application.Id;
-            _accountId = application.AccountId;
+            _applicationId = _application.Id;
+            _accountId = _application.AccountId;
         }
 
         [Test]
@@ -44,30 +45,28 @@ namespace Wonga.QA.Tests.Bi
                                                                   ApplicationId = _applicationId,
                                                                   TransactionId = Guid.NewGuid() });
 
-            ConfirmStatusHistory(_applicationId, new string[] { "Accepted", "TermsAgreed", "Live" });
+            ConfirmStatusHistory(new string[] { "Accepted", "TermsAgreed", "Live" });
         }
 
         [Test]
         [AUT(AUT.Uk), JIRA("UK-1477")]
-        [Description("Verifies that when an application status is 'Live' the status history will have entries for the pre-live events")]
-        public void Due_Application_Has_Due_Status_History()
+        [Description("Verifies that when an application has been paid off the status history will have PaidInFull as the current status.")]
+        public void Paid_Application_Has_Paid_In_FullStatus()
         {
             Live_Application_Has_PreLive_Status_History();
 
-            Drive.Msmq.Payments.Send(new ILoanIsDueEvent { AccountId = _accountId, 
-                                                           ApplicationId = _applicationId, 
-                                                           LoanDueDate = DateTime.UtcNow + TimeSpan.FromDays(7) });
-
-            ConfirmStatusHistory(_applicationId, new string[] { "DueToday" });
+            _application.MakeDueToday(); //This will pay off the loan
+            
+            ConfirmStatusHistory(new string[] { "DueToday", "PaidInFull" });
         }
 
         //Checks to ensure that each of the supplied status values appears at least once for that application as the current status.
         //There must also be at least one record that has a PreviousStatus with a value of New.
-        private void ConfirmStatusHistory(Guid appId, IEnumerable<string> statuses)
+        private void ConfirmStatusHistory(IEnumerable<string> statuses)
         {
-            Do.With.Timeout(2).Interval(5).Until(() => appStatusHistoryRepo.FindAll(ApplicationId: appId, PreviousStatus: "New") != null);
+            Do.With.Timeout(2).Interval(5).Until(() => appStatusHistoryRepo.FindAll(ApplicationId: _applicationId, PreviousStatus: "New") != null);
 
-            Do.With.Timeout(2).Interval(5).Until(() => statuses.All(stat => { var records = appStatusHistoryRepo.FindAll(appStatusHistoryRepo.ApplicationId == appId &&
+            Do.With.Timeout(2).Interval(5).Until(() => statuses.All(stat => { var records = appStatusHistoryRepo.FindAll(appStatusHistoryRepo.ApplicationId == _applicationId &&
                                                                                                                          appStatusHistoryRepo.CurrentStatus == stat);
                                                                               return records.Count() >= 1; }));
         }
