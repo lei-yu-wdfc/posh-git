d529f69e2b447ac406fe8a95f3e0fccde01c4a36
Konstantinos Konstantinidis <konstantinos.konstantinidis@wonga.com>
2012-05-16 12:24:38 +0100

    adding tools

---
+++ src/Wonga.QA.Tools/Wonga.QA.Tools.NonParallelHunter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using MbUnit.Framework;
+
+namespace Wonga.QA.Tools.NonParallelHunter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string binFolder = Path.Combine(Environment.CurrentDirectory);
+            var files = Directory.GetFiles(binFolder, "Wonga.QA.Tests.*.dll");
+            foreach (var file in files)
+            {
+                Console.WriteLine(Path.GetFileName(file));
+                var assembly = Assembly.LoadFile(file);
+                var testFixtures = assembly.GetTypes().Where(
+                    t => !t.IsDefined(typeof (ParallelizableAttribute), true)
+                    && t.IsDefined(typeof(TestFixtureAttribute), true)
+                    && t.IsClass
+                    && !t.IsAbstract);
+                foreach(var testFixture in testFixtures)
+                    Console.WriteLine(string.Format("\t {0}", testFixture.Name));
+            }
+            Console.Read();
+        }
+    }
+}
