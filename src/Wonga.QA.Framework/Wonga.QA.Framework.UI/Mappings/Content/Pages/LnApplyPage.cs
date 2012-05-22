fbf6cf1a0112514858ae36f9ba8a9029b13c54fb
Stanislav Desyatnikov <stasde@gmail.com>
2012-05-22 09:36:22 +0100

    UK-886 Added missing file

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Content/Pages/LnApplyPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.Framework.UI.Mappings.Content
+{
+    public class LnApplyPage
+    {
+        public String MobileNumberHasNotChangedMessage { get; set; }
+    }
+}
