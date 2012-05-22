06120d82607fc8f1be8e934c9138e9a866f2f8c6
whytepaul <paul.whyte@wonga.com>
2012-05-22 09:44:55 +0100

    Rolledback UK-847 Change in QA Framework because it was causing issues with UK tests. Commit Hash 2337d7878ef31a54de5db70fd60204408e6f7431

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Core/Get.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Core/Get.cs
@@ -186,7 +186,7 @@ namespace Wonga.QA.Framework.Core
                     }
                 case AUT.Uk:
                     {
-                        return RandomLong(10000000, 99999999).ToString();
+                        return "42368003";
                     }
                 case AUT.Wb:
                     {
