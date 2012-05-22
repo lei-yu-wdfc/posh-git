e481a734357dcc49a1ba6f8aa3671f217c88d8a3
Roman Tertychnyi <celenia_rot@hotmail.com>
2012-05-22 16:28:05 +0300

    Update defult telephone numbers for UK area.
    Minor fix to Apijourneys.cs

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Defaults/SaveCustomerDetailsUkCommand.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Defaults/SaveCustomerDetailsUkCommand.cs
@@ -14,8 +14,8 @@ namespace Wonga.QA.Framework.Api
 			MiddleName = Get.GetName();
 			Surname = Get.GetName();
 			Gender = Get.RandomEnum<GenderEnum>();
-			HomePhone = "0210000000";
-			WorkPhone = "0210000000";
+			HomePhone = "0120000000";
+			WorkPhone = "0120000000";
 		}
 	}
 }
