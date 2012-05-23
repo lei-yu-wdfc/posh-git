8771a04a24ca7ab699190bd970b31abd7fd8b0ba
Piotr Walat <piotr.walat@wonga.com>
2012-05-23 13:18:20 +0100

    UK-1558: Acceptance tests

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/AccruedInterestSuspensionTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/AccruedInterestSuspensionTests.cs
@@ -8,34 +8,37 @@ using Wonga.QA.Tests.Core;
 namespace Wonga.QA.Tests.Payments
 {
     [TestFixture, AUT(AUT.Uk), Parallelizable(TestScope.All)]
-    [Ignore("Work in progress")]
     public class AccruedInterestSuspensionTests
     {
+        private const string InArrearsMaxInterestDaysKey = "Payments.InArrearsMaxInterestDays";
         /// <summary>
         /// Transactions db table
         /// </summary>
         private dynamic _transactionsDb;
 
+        private dynamic _serviceConfigDb;
+
         private Application _application;
         private Customer _customer;
-        private DateTime _promiseDate; 
         [SetUp]
         public void Setup()
         {
             _customer = CustomerBuilder.New().Build();
-            _promiseDate = DateTime.Now.AddDays(12);
-            _application = ApplicationBuilder.New(_customer).WithPromiseDate(new Date(_promiseDate)).Build();
+            _application = ApplicationBuilder.New(_customer).WithLoanTerm(12).Build();
             _transactionsDb = Drive.Data.Payments.Db.Transactions;
+            _serviceConfigDb = Drive.Data.Ops.Db.ServiceConfigurations;
         }
 
         [Test]
-        public void GoingIntoArrears_Creates_SuspendInterestTransaction()
+        public void GoingIntoArrears_Creates_SuspendInterestTransaction_InTheFuture()
         {
-            _application.PutApplicationIntoArrears(1);
+            _application.PutApplicationIntoArrears();
             dynamic suspendTransaction = null;
+            string offset = _serviceConfigDb.FindBy(Key: InArrearsMaxInterestDaysKey).Value.ToString();
+            int intOffset = Int32.Parse(offset);
             Do.Until(() => suspendTransaction = _transactionsDb
                                                     .FindBy(Type: PaymentTransactionEnum.SuspendInterestAccrual,
-                                                            PostedOn: _promiseDate.AddDays(60)));
+                                                            PostedOn: DateTime.Today.AddDays(intOffset)));
             Assert.IsNotNull(suspendTransaction);
         }
     }
