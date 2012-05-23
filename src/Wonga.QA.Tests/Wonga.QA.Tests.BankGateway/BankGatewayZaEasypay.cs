194e124b8a1016d295200fa2afe7f1d7ac5b8aa0
rmhwong21 <raymond.wong@wonga.com>
2012-05-23 15:47:06 +0100

    ZA-2394 Fix AT

--- src/Wonga.QA.Tests/Wonga.QA.Tests.BankGateway/BankGatewayZaEasypay.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.BankGateway/BankGatewayZaEasypay.cs
@@ -26,6 +26,7 @@ namespace Wonga.QA.Tests.BankGateway
 		private dynamic _filesTable = Drive.Data.BankGateway.Db.Files;
 		private dynamic _acknowledgesTable = Drive.Data.BankGateway.Db.Acknowledges;
 		private dynamic _bankIntegrationsTable = Drive.Data.BankGateway.Db.BankIntegrations;
+		private dynamic _incomingBankGatewayFileTable = Drive.Data.QaData.Db.IncomingBankGatewayFile;
 
 		private int _pollingInterval;
 		private const string BANK_INT_DESC = "Easypay";
@@ -75,21 +76,16 @@ namespace Wonga.QA.Tests.BankGateway
 		public void ProcessEasypayFile_WillCreate_Acknowledgements()
 		{
 			//Arrange	
-			string fileContent =
-							"0120120417000001539004                                                    093398\n" + //Header
-							"4020120417082909006001007036034 0                                         098568\n" + //Transaction
-							"50201204170829090000000000000000000000953901098645686240+000004000+0000538138522\n" + //Payment
-							"64201204170829090000000000000000000000000000000000012345+000004000+0000000134192\n" + //Tender
-							"99000001+00000004000+00000000538 000001+00000004000+00000000000           121113\n";  //Trailer
+			int fileSequence = 1;
+			string repayNumber = "953901098645686240";
+			string fileContent = CreateEasypayTestFile(DateTime.Now, fileSequence, 40.00M, 5.38m, repayNumber, "12345");
 
-			IncomingBankGatewayFile incomingBankGatewayFile = new IncomingBankGatewayFile();
+			dynamic incomingBankGatewayFile = new ExpandoObject();
 			incomingBankGatewayFile.FileData = new ASCIIEncoding().GetBytes(fileContent);
 			incomingBankGatewayFile.FileName = TEST_FILE1;
 			incomingBankGatewayFile.Gateway = TEST_GATEWAY;
-
 			//Act
-			Drive.Db.QaData.IncomingBankGatewayFiles.InsertOnSubmit(incomingBankGatewayFile);
-			incomingBankGatewayFile.Submit();
+			_incomingBankGatewayFileTable.Insert(incomingBankGatewayFile);
 
 			//Assert
 			var acknowledgedfile = Do.With.Timeout(20).Until(() => _filesTable.FindAll(_filesTable.FileName == TEST_FILE1).FirstOrDefault());
@@ -99,7 +95,7 @@ namespace Wonga.QA.Tests.BankGateway
 			var acknowledgeTransaction = Do.With.Timeout(20).Until(() => _acknowledgesTable.FindAll(_acknowledgesTable.FileName == TEST_FILE1)).ToList();
 			Assert.AreEqual(1, acknowledgeTransaction.Count);
 			Assert.AreEqual(TEST_FILE1, acknowledgeTransaction[0].FileName);
-			Assert.AreEqual("953901098645686240", acknowledgeTransaction[0].IncomingReference);
+			Assert.AreEqual(repayNumber, acknowledgeTransaction[0].IncomingReference);
 		}
 
 		[Test, AUT(AUT.Za), JIRA("ZA-2394")]
@@ -114,25 +110,16 @@ namespace Wonga.QA.Tests.BankGateway
 			dynamic repaymentAccount = Drive.Data.Payments.Db.RepaymentAccount;
 			var ra = Do.Until(() => repaymentAccount.FindAll(repaymentAccount.AccountId == customer.Id)
 													.FirstOrDefault());
+			int fileSequence = 2;
+			string repayNumber = ra.RepaymentNumber;
+			string fileContent = CreateEasypayTestFile(DateTime.Now, fileSequence, 40.00M, 5.38m, repayNumber, "12345");
 
-			string fileContentTemplate =
-						"0120120417000002539004                                                    093398\n" + //Header
-						"4020120417082909006001007036034 0                                         098568\n" + //Transaction
-						"502012041708290900000000000000000000{0}+000004000+0000538138522\n" + //Payment
-						"64201204170829090000000000000000000000000000000000012345+000004000+0000000134192\n" + //Tender
-						"99000001+00000004000+00000000538 000001+00000004000+00000000000           121113\n";  //Trailer
-
-			string fileContent = string.Format(fileContentTemplate, ra.RepaymentNumber);
-
-			IncomingBankGatewayFile incomingBankGatewayFile = new IncomingBankGatewayFile();
-
+			dynamic incomingBankGatewayFile = new ExpandoObject();
 			incomingBankGatewayFile.FileData = new ASCIIEncoding().GetBytes(fileContent);
 			incomingBankGatewayFile.FileName = TEST_FILE2;
 			incomingBankGatewayFile.Gateway = TEST_GATEWAY;
-
 			//Act
-			Drive.Db.QaData.IncomingBankGatewayFiles.InsertOnSubmit(incomingBankGatewayFile);
-			incomingBankGatewayFile.Submit();
+			_incomingBankGatewayFileTable.Insert(incomingBankGatewayFile);
 
 			//Assert
 			var transaction = Do.With.Timeout(20).Until(() => Drive.Db.Payments.Transactions.Single(r => r.Amount == -40M && 
@@ -155,35 +142,16 @@ namespace Wonga.QA.Tests.BankGateway
 			var ra = Do.Until(() => repaymentAccount.FindAll(repaymentAccount.AccountId == customer.Id)
 													.FirstOrDefault());
 
-			string fileContentTemplate =
-			"0120120417{5}539004                                                    093398\n" + //Header
-			"4020120417082909006001007036034 0                                         098568\n" + //Transaction
-			"5020120417082909{0}+{1}+{3}138522\n" + //Payment
-			"64201204170829090000000000000000000000000000000000012345+{1}+0000000134192\n" + //Tender
-			"99000001+{2}+{4} 000001+{2}+00000000000           121113\n";  //Trailer
+			int fileSequence = 3;
+			string repayNumber = ra.RepaymentNumber;
+			string fileContent = CreateEasypayTestFile(DateTime.Now, fileSequence, 642.50M, 5.38m, repayNumber, "12345");
 
-			string fileSequence = "000003";
-			string fees = "538";
-			string repayAmount = "64250";
-			string repaymentNumber = ra.RepaymentNumber;
-
-			string fileContent = string.Format(fileContentTemplate, repaymentNumber.PadLeft(40, '0'),		//{0}
-																	repayAmount.PadLeft(9, '0'),			//{1}
-																	repayAmount.PadLeft(11, '0'),			//{2}
-																	fees.PadLeft(7, '0'),					//{3}
-																	fees.PadLeft(11, '0'),					//{4}
-																	fileSequence.PadLeft(6,'0')				//{5}
-																	);
-
-			IncomingBankGatewayFile incomingBankGatewayFile = new IncomingBankGatewayFile();
-
+			dynamic incomingBankGatewayFile = new ExpandoObject();
 			incomingBankGatewayFile.FileData = new ASCIIEncoding().GetBytes(fileContent);
 			incomingBankGatewayFile.FileName = TEST_FILE3;
 			incomingBankGatewayFile.Gateway = TEST_GATEWAY;
-
 			//Act
-			Drive.Db.QaData.IncomingBankGatewayFiles.InsertOnSubmit(incomingBankGatewayFile);
-			incomingBankGatewayFile.Submit();
+			_incomingBankGatewayFileTable.Insert(incomingBankGatewayFile);
 
 			app =  Do.With.Timeout(20).Until(() => Drive.Db.Payments.Applications.Single(a => a.ExternalId == application.Id && a.ClosedOn != null));
 
@@ -194,11 +162,14 @@ namespace Wonga.QA.Tests.BankGateway
 		private string CreateEasypayTestFile(DateTime valueDate, int fileSequence, decimal repayAmount, decimal fees, string repaymentNumber, string accountNumber)
 		{
 			string fileContentTemplate =
-										"01{6}{5}539004{12}093398\n" + //Header
+										"01{6}{5}{18}{19}{12}093398\n" + //Header
 										"40{6}{7}{11}{15}{13}098568\n" + //Transaction
 										"50{6}{7}{0}+{1}+{3}138522\n" + //Payment
 										"64{6}{7}{8}+{1}+{17}134192\n" + //Tender
 										"99{9}+{2}+{4} {10}+{2}+{16}{14}121113\n";  //Trailer
+
+			string fileVersion = "04";
+			string receiverId = "5390";
 
 			string valueDateStr = valueDate.ToString("yyyyMMdd");
 			string timeStr = valueDate.ToString("HHmmss");
@@ -248,7 +219,9 @@ namespace Wonga.QA.Tests.BankGateway
 																	filler3.PadLeft(11, ' '),					//{14}
 																	POS.PadRight(8, ' '),						//{15}
 																	bankCostTotalStr.PadLeft(11, '0'),			//{16}
-																	bankCostStr.PadLeft(7, '0')					//{17}
+																	bankCostStr.PadLeft(7, '0'),				//{17}
+																	receiverId,									//{18}
+																	fileVersion									//{19}
 																	);
 			return fileContent;
 		}
