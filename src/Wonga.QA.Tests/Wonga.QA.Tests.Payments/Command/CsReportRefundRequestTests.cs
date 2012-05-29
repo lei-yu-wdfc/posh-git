4f677e84f4bfa9f1dbf4bae25e0e5eb720f72ff2
Piotr Walat <piotr.walat@wonga.com>
2012-05-29 13:26:39 +0100

    UK-1815 Acceptance test

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Command/CsReportRefundRequestTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Command/CsReportRefundRequestTests.cs
@@ -1,6 +1,11 @@
 using System;
 using MbUnit.Framework;
 using Wonga.QA.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.Cs;
+using Wonga.QA.Framework.Db.Ops;
+using Wonga.QA.Framework.ThirdParties;
+
 
 namespace Wonga.QA.Tests.Payments.Command
 {
@@ -9,14 +14,34 @@ namespace Wonga.QA.Tests.Payments.Command
     public class CsReportRefundRequestTests
     {
         [Test]
-        public void Test()
+        public void ReportRefundCommand_Changes_SalesforceApplicationStatus_To_Refund()
         {
-            Customer customer = CustomerBuilder.New().Build();
-            Application app = ApplicationBuilder.New(customer).Build();
             Guid caseId = Guid.NewGuid();
+            Customer customer = CustomerBuilder.New().Build();
+            Application application = ApplicationBuilder.New(customer).Build();
+            var command = new CsReportRefundRequestCommand()
+                                                       {
+                                                           ApplicationId = application.Id,
+                                                           CaseId = caseId,
+                                                       };
+            Drive.Cs.Commands.Post(command);
+
+            var sfUsername = Drive.Data.Ops.Db.ServiceConfigurations.FindByKey("Salesforce.UserName");
+            var sfPassword = Drive.Data.Ops.Db.ServiceConfigurations.FindByKey("Salesforce.Password");
+            var sfUrl = Drive.Data.Ops.Db.ServiceConfigurations.FindByKey("Salesforce.Url");
+            var sales = Drive.ThirdParties.Salesforce;
+            sales.SalesforceUsername = sfUsername.Value;
+            sales.SalesforcePassword = sfPassword.Value;
+            sales.SalesforceUrl = sfUrl.Value;
 
 
-            //Drive.Cs.Commands.Post();
+            // wait until sales force moves to Refund
+            Do.Until(() =>
+            {
+                var app = sales.GetApplicationById(application.Id);
+                return app.Status_ID__c != null && app.Status_ID__c == (double)Framework
+                    .ThirdParties.Salesforce.ApplicationStatus.Refund;
+            });
         }
     }
 }
