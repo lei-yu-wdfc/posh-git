1d7c491834d64ebbf5c649f41970f640493c7edf
Adrian Murphy <adrian.murphy@wonga.com>
2012-05-29 15:27:12 +0100

    Readded CollectionsTests

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointAccountNumberApplicationsAcceptableTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointAccountNumberApplicationsAcceptableTests.cs
@@ -41,7 +41,7 @@ namespace Wonga.QA.Tests.Risk.Checkpoints
 			AssertCheckpointAndVerificationExecution(lnApplication);
 		}
 
-		[Test, AUT(AUT.Za, AUT.Ca), JIRA("ZA-2228", "CA-1879"), Timeout(0)]
+		[Test, AUT(AUT.Za, AUT.Ca), JIRA("ZA-2228", "CA-1879"), Timeout(0), Explicit("Long running time")]
 		public void LnNumberOfApplicationDailyOverThresholdIsDeclined()
 		{
 			var customerLn = CustomerBuilder.New().WithEmployer(RiskMask.TESTEmployedMask).WithEmployerStatus("Unemployed").Build();
@@ -58,7 +58,7 @@ namespace Wonga.QA.Tests.Risk.Checkpoints
 			AssertApplicationDeclinedWithCorrectCheckPointAndVerification(lnApplication);
 		}
 
-		[Test, AUT(AUT.Za, AUT.Ca), JIRA("ZA-2228", "CA-1879"), Timeout(0)]
+		[Test, AUT(AUT.Za, AUT.Ca), JIRA("ZA-2228", "CA-1879"), Timeout(0), Explicit("Long running time")]
 		public void LnNumberOfApplicationMonthlyOverThresholdIsDeclined()
 		{
 			//make one app per day up until the monthly threshold so that the daily limit is not exceeded
