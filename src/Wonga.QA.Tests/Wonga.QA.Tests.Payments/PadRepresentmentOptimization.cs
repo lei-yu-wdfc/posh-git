e336fa7d2225172953c119834f23cfed96227ed4
Luke Rickard <luke.rickard@wonga.com>
2012-05-29 17:37:49 +0100

    CA-1962: PadRepresentmentOptimization updates

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/PadRepresentmentOptimization.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/PadRepresentmentOptimization.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MbUnit.Framework;
+using Wonga.QA.Framework;
 using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.Msmq;
 using Wonga.QA.Tests.Core;
 
 namespace Wonga.QA.Tests.Payments
 {
     public class PadRepresentmentOptimization
     {
-        [Test, AUT(AUT.Ca), JIRA("CA-1962"), Ignore()]
-        public void WhenACustomerGoesThenAnAttemptToRetrieve33PercentOfTheBalanceShouldBeMadeOnTheNextPayDate()
-        {
+        private readonly dynamic _opsSagasPaymentsInArrears = Drive.Data.OpsSagas.Db.PaymentsInArrearsSagaEntity;
 
+        [Test, AUT(AUT.Ca), JIRA("CA-1962"), Ignore()]
+        public void WhenACustomerGoesIntoArrearsThenAnAttemptToRetrieve33PercentOfTheBalanceShouldBeMadeOnTheNextPayDate()
+        {
+            var customer = CustomerBuilder.New().Build();
+            var application = ApplicationBuilder.New(customer).Build();
+
+            application.PutApplicationIntoArrears();
+
+            Assert.IsTrue(GetRepresentmentsSent(application.Id) == "1");
+            Assert.IsTrue(GetNextPayDate(application.Id) == customer.GetNextPayDate());
+
+            TimeoutMultipleRepresentmentsForPaymentsInArrearsSagaEntity(application.Id);
+
+            Assert.IsTrue(GetRepresentmentsSent(application.Id) == "2");
         }
 
         [Test, AUT(AUT.Ca), JIRA("CA-1962"), Ignore()]
@@ -44,6 +58,24 @@ namespace Wonga.QA.Tests.Payments
         public void WhenTheThirdPadRepresentmentForACustomerInArrearsFailsThenThereShouldBeNoMoreAttemptsToRetrieveMoneyFromTheCustomer()
         {
 
+        }
+
+        private void TimeoutMultipleRepresentmentsForPaymentsInArrearsSagaEntity(Guid applicationGuid)
+        {
+            var multipleRepresentmentSaga = Do.Until(() => _opsSagasPaymentsInArrears.FindByApplicationId(applicationGuid));
+            Drive.Msmq.Payments.Send(new TimeoutMessage { SagaId = multipleRepresentmentSaga.Id });
+        }
+
+        private String GetRepresentmentsSent(Guid applicationGuid)
+        {
+            var multipleRepresentmentSaga = Do.Until(() => _opsSagasPaymentsInArrears.FindByApplicationId(applicationGuid));
+            return multipleRepresentmentSaga.RepresentmentsSent;
+        }
+
+        private String GetNextPayDate(Guid applicationGuid)
+        {
+            var multipleRepresentmentSaga = Do.Until(() => _opsSagasPaymentsInArrears.FindByApplicationId(applicationGuid));
+            return multipleRepresentmentSaga.NextPayDate;
         }
     }
 }
