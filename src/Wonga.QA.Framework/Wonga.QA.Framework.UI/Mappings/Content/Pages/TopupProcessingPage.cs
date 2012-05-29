a80265ad9a446b5e3c008bbac8c70dab74455e8f
Ethan Chiddicks <ethan.chiddicks@wonga.com>
2012-05-29 16:39:09 +0100

    Adding Content and CSS selector mapping for Repay, Topup and Extension Processing Pages. Also solving ambiguousreference clusterf*ck for pages that have both types of mapping.

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Content/Pages/TopupProcessingPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.Framework.UI.Mappings.Content
+{
+    public class TopupProcessingPage
+    {
+        public String WaitMessage { get; set; }
+    }
+}
