62e392b749b2caa03d3be022d3de143ca8c9ee00
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-05-30 13:20:29 +0100

    Za- release: fixes for tests

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Content/ErrorsMessages/MobilePinVerificationSection.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Mappings/Content/ErrorsMessages/MobilePinVerificationSection.cs
@@ -11,5 +11,6 @@ namespace Wonga.QA.Framework.UI.Mappings.Content
     {
         public String ResendPinMessage1 { get; set; }
         public String ResendPinMessage2 { get; set; }
+        public String ResendPinMessage3 { get; set; }
     }
 }
