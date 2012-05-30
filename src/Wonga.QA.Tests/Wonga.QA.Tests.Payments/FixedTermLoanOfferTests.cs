0b5f830b6cb9b130af4cb232a5b2ccd33c247d7e
Adrian Murphy <adrian.murphy@wonga.com>
2012-05-30 15:51:14 +0100

    Chained InArrearsNoticeTests. Readded to run.

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/FixedTermLoanOfferTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/FixedTermLoanOfferTests.cs
@@ -13,7 +13,7 @@ namespace Wonga.QA.Tests.Payments
 	[TestFixture, Parallelizable(TestScope.All)]
     public class FixedTermLoanOfferTests
     {
-		[Test, AUT(AUT.Za), JIRA("ZA-2024"), Pending("ZA-2565")]
+		[Test, AUT(AUT.Za), JIRA("ZA-2024")]
         [Row("2012-2-23", 30, 36, Order = 0)]
         [Row("2012-3-1", 23, 30, Order = 1)]
         [Row("2012-8-1", 24, 30, Order = 2)]
