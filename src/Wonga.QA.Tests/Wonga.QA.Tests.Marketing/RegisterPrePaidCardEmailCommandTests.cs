107e9510dcc1f132be281e6bfc0dbf0460df597d
Pedro Silva <pedro.silva@wonga.com>
2012-05-31 22:31:05 +0100

    CA-2266: first acceptance tests

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Marketing/RegisterPrePaidCardEmailCommandTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Marketing
+{
+	[Parallelizable(TestScope.All)]
+	public class RegisterPrePaidCardEmailCommandTests
+	{
+		private static readonly dynamic _marketingDb = Drive.Data.Marketing.Db;
+
+		[Test, AUT(AUT.Ca), JIRA("CA-2266")]
+		public void RegisterPrePaidCardEmailCommandShouldSaveEmailInDatabase()
+		{
+			string newEmail = Get.GetEmail(50);
+
+			var command = new RegisterPrePaidCardEmailCommand
+			              	{
+								Email = newEmail
+			              	};
+
+			Drive.Api.Commands.Post(command);
+
+			var registereEmail = Do.Until(() => _marketingDb.RegisteredPrePaidCardEmails.FindByEmail(newEmail));
+
+			Assert.IsNull(registereEmail);
+
+		}
+
+		//TODO: test with empty email
+
+		//TODO: test with invalid email
+
+		//TODO: test with existing email
+
+	}
+}
