2c042d5ccaad1baae3bcc173d8d08f6c91c64347
rmhwong21 <raymond.wong@wonga.com>
2012-05-31 12:35:08 +0100

    AT for PayU ZA-2570 and ZA-2571

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Command/PayUCommandTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Command/PayUCommandTests.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
+<<<<<<< HEAD
 using System.Xml.Linq;
+=======
+>>>>>>> AT for PayU ZA-2570 and ZA-2571
 using MbUnit.Framework;
 using Wonga.QA.Framework;
 using Wonga.QA.Framework.Api;
@@ -46,7 +49,11 @@ namespace Wonga.QA.Tests.Payments.Command
 			var customer = CustomerBuilder.New().Build();
 			var app = ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
 
+<<<<<<< HEAD
 			var command = new Wonga.QA.Framework.Api.SavePayURequestZaCommand
+=======
+			var command = new SavePayURequestZaCommand
+>>>>>>> AT for PayU ZA-2570 and ZA-2571
 			{
 				ApplicationId = app.Id,
 				 MerchantReferenceNumber = merchantReferenceNumber,
@@ -75,7 +82,11 @@ namespace Wonga.QA.Tests.Payments.Command
 			var customer = CustomerBuilder.New().Build();
 			var app = ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
 
+<<<<<<< HEAD
 			var saveRequestcommand = new Wonga.QA.Framework.Api.SavePayURequestZaCommand
+=======
+			var saveRequestcommand = new SavePayURequestZaCommand
+>>>>>>> AT for PayU ZA-2570 and ZA-2571
 			{
 				ApplicationId = app.Id,
 				MerchantReferenceNumber = merchantReferenceNumber,
@@ -87,12 +98,20 @@ namespace Wonga.QA.Tests.Payments.Command
 
 			var incomingPartnerPayment = Do.Until(() => _incomingPartnerPaymentsDB.FindAll(_incomingPartnerPaymentsDB.PaymentReference == merchantReferenceNumber.ToString()).FirstOrDefault());
 
+<<<<<<< HEAD
 			var saveResponsecommand = new Wonga.QA.Framework.Api.SavePayUResponseZaCommand
+=======
+			var saveResponsecommand = new SavePayUResponseZaCommand
+>>>>>>> AT for PayU ZA-2570 and ZA-2571
 			{
 				ApplicationId = app.Id,
 				MerchantReferenceNumber = merchantReferenceNumber,
 				TransactionAmount = transactionAmount,
+<<<<<<< HEAD
 				RawRequestResponse = "RawRequestResponseData", 
+=======
+				RawRequestResponse = "RawRequestResponseData",
+>>>>>>> AT for PayU ZA-2570 and ZA-2571
 			};
 
 			//Act
@@ -107,6 +126,10 @@ namespace Wonga.QA.Tests.Payments.Command
 			Assert.IsNotNull(incomingPartnerPaymentResponses);
 			Assert.AreEqual("RawRequestResponseData", incomingPartnerPaymentResponses.RawRequestResponse);
 			Assert.IsNotNull(incomingPartnerPayment);
+<<<<<<< HEAD
 		}
+=======
+		}		
+>>>>>>> AT for PayU ZA-2570 and ZA-2571
 	}
 }
