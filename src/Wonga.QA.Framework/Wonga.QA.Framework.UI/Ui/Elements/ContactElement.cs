2075cb155e32cda628562953fae79eddfe8f276c
Petr.Tarasenko <petr.tarasenko@softheme.com>
2012-05-31 15:06:16 +0100

    QA-291

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Elements/ContactElement.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Elements/ContactElement.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using OpenQA.Selenium;
+using Wonga.QA.Framework.Core;
 using Wonga.QA.Framework.UI.UiElements.Pages;
 using Wonga.QA.Framework.UI.Mappings;
 using Wonga.QA.Framework.UI.UiElements.Pages.Common;
@@ -9,6 +12,8 @@ namespace Wonga.QA.Framework.UI.Elements
     public class ContactElement : BaseElement
     {
         private IWebElement _contactTitle;
+        private IWebElement _popup;
+        private ReadOnlyCollection<IWebElement> _hrefs;
 
         public ContactElement(BasePage page)
             : base(page)
@@ -27,6 +32,22 @@ namespace Wonga.QA.Framework.UI.Elements
 
                 return false;
             }
+        }
+
+        public bool CheckWbEmailsOnPopup()
+        {
+            var local = true;
+            _popup = Do.Until(()=>Page.Client.Driver.FindElement(By.CssSelector(UiMap.Get.ContactElement.FormId)));
+            _hrefs =  _popup.FindElements(By.CssSelector("a"));
+            foreach (var href in _hrefs)
+            {
+                if (!href.Text.Contains("@wongabusiness.com"))
+                {
+                    Console.WriteLine(href.Text);
+                    local = false;
+                }
+            }
+            return local;
         }
     }
 }
