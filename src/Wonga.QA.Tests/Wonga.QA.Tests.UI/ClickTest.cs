cd97bd1ebe09439abca17f0d07bdd43335343385
Michael.Nowicki <michael.nowicki@wonga.com>
2012-05-31 18:19:59 +0100

    ClickTest.cs

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/ClickTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using MbUnit.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Ui
+{
+    class ClickTest : UiTest
+    {
+
+        [Test, AUT(AUT.Ca, AUT.Za), JIRA("CA-2326")]
+        public void VerifyIsClickCodeLoaded()
+        {
+            var homePage = Client.Home();
+
+            Assert.IsTrue(Regex.IsMatch(homePage.Source, @"http.*click.*.js", RegexOptions.IgnoreCase));
+
+        }
+
+        [Test, AUT(AUT.Ca, AUT.Za), JIRA("CA-2326")]
+        public void VerifyIsClickParamsSet()
+        {
+            var homePage = Client.Home();
+            Assert.IsTrue(Regex.IsMatch(homePage.Source, @"_clickconfiguration\.push\(\['tids'", RegexOptions.IgnoreCase));
+            Assert.IsTrue(Regex.IsMatch(homePage.Source, @"_clickconfiguration\.push\(\['region'", RegexOptions.IgnoreCase));
+            Assert.IsTrue(Regex.IsMatch(homePage.Source, @"_clickconfiguration\.push\(\['devicetype'", RegexOptions.IgnoreCase));
+            Assert.IsTrue(Regex.IsMatch(homePage.Source, @"_clickconfiguration\.push\(\['devicegroup'", RegexOptions.IgnoreCase));
+
+        }
+
+    }
+}
