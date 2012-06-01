53db009addbfbd8dd8d4af65f4956161c3f67946
Seamus Hoban <seamus.hoban@wonga.com>
2012-06-01 14:44:31 +0100

    Update to Application MakeDueToday so that it uses the UseLoanDurationSaga feature switch to determine how to move the application to the due today state.

--- src/Wonga.QA.Framework/Wonga.QA.Framework/Application.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework/Application.cs
@@ -182,13 +182,25 @@ namespace Wonga.QA.Framework
 		public void MakeDueToday(ApplicationEntity application)
 		{
 			RewindAppDates(application);
-            LoanDueDateNotificationSagaEntity ldd = Drive.Db.OpsSagas.LoanDueDateNotificationSagaEntities.Single(s => s.ApplicationId == Id);
-            Drive.Msmq.Payments.Send(new TimeoutMessage { SagaId = ldd.Id });
-            Do.With.While(ldd.Refresh);
 
-			FixedTermLoanSagaEntity ftl = Drive.Db.OpsSagas.FixedTermLoanSagaEntities.Single(s => s.ApplicationGuid == Id);
-			Drive.Msmq.Payments.Send(new TimeoutMessage { SagaId = ftl.Id });
-			Do.With.While(ftl.Refresh);
+            if (Drive.Data.Ops.GetServiceConfiguration<bool>("Payments.FeatureSwitches.UseLoanDurationSaga") == false)
+            {
+                LoanDueDateNotificationSagaEntity ldd = Drive.Db.OpsSagas.LoanDueDateNotificationSagaEntities.Single(s => s.ApplicationId == Id);
+                Drive.Msmq.Payments.Send(new TimeoutMessage { SagaId = ldd.Id });
+                Do.With.While(ldd.Refresh);
+
+                FixedTermLoanSagaEntity ftl = Drive.Db.OpsSagas.FixedTermLoanSagaEntities.Single(s => s.ApplicationGuid == Id);
+                Drive.Msmq.Payments.Send(new TimeoutMessage { SagaId = ftl.Id });
+                Do.With.While(ftl.Refresh);
+            }
+            else
+            {
+                //We should timeout the LoanDurationSaga...
+                dynamic loanDurationSagaEntities = Drive.Data.OpsSagas.Db.LoanDurationSagaEntity;
+                var loanDurationSaga = loanDurationSagaEntities.FindAllByAccountGuid(AccountGuid: application.AccountId).FirstOrDefault();
+
+                Drive.Msmq.Payments.Send(new TimeoutMessage { SagaId = loanDurationSaga.Id });
+            }
 		}
 
 		public virtual Application PutApplicationIntoArrears(uint daysInArrears)
