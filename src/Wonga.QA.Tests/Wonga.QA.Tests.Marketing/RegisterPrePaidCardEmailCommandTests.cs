6b4cff68d8cab70b9bf3566b505f4d2bd7daf025
Pedro Silva <pedro.silva@wonga.com>
2012-06-01 08:45:26 +0100

    CA-2266: more acceptance tests

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Marketing/RegisterPrePaidCardEmailCommandTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Marketing/RegisterPrePaidCardEmailCommandTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MbUnit.Framework;
 using Wonga.QA.Framework;
 using Wonga.QA.Framework.Api;
+using Wonga.QA.Framework.Api.Exceptions;
 using Wonga.QA.Framework.Core;
 using Wonga.QA.Tests.Core;
 
@@ -13,7 +14,10 @@ namespace Wonga.QA.Tests.Marketing
 	[Parallelizable(TestScope.All)]
 	public class RegisterPrePaidCardEmailCommandTests
 	{
-		private static readonly dynamic _marketingDb = Drive.Data.Marketing.Db;
+		private const string ErrorMarketingEmailIsEmpty = "Marketing_Email_Is_Empty";
+		private const string ErrorOpsRequestXmlInvalid = "Ops_RequestXmlInvalid";
+		private const string ErrorMarketingEmailHasInvalidFormat = "Marketing_Email_Has_Invalid_Format";
+		private const string ErrorMarketingEmailAlreadyExists = "Marketing_Email_Already_Exists";private static readonly dynamic _marketingDb = Drive.Data.Marketing.Db;
 
 		[Test, AUT(AUT.Ca), JIRA("CA-2266")]
 		public void RegisterPrePaidCardEmailCommandShouldSaveEmailInDatabase()
@@ -27,17 +31,93 @@ namespace Wonga.QA.Tests.Marketing
 
 			Drive.Api.Commands.Post(command);
 
-			var registereEmail = Do.Until(() => _marketingDb.RegisteredPrePaidCardEmails.FindByEmail(newEmail));
+			var registeredEmail = Do.Until(() => _marketingDb.RegisteredPrePaidCardEmails.FindByEmail(newEmail));
 
-			Assert.IsNull(registereEmail);
-
+			Assert.IsNull(registeredEmail);
 		}
 
-		//TODO: test with empty email
+		[Test, AUT(AUT.Ca), JIRA("CA-2266")]
+		public void RegisterPrePaidCardEmailCommandShouldFailWithEmptyEmail()
+		{
+			var command = new RegisterPrePaidCardEmailCommand
+			{
+				Email = string.Empty
+			};
 
-		//TODO: test with invalid email
+			try
+			{
+				Drive.Api.Commands.Post(command);
+				Assert.Fail("Invalid command should fault");
+			}
+			catch(ValidatorException e)
+			{
+				Assert.IsTrue(e.Errors.Any(error => error == ErrorMarketingEmailIsEmpty));
+			}
+		}
 
-		//TODO: test with existing email
+		[Test, AUT(AUT.Ca), JIRA("CA-2266")]
+		public void RegisterPrePaidCardEmailCommandShouldFailWithNullEmail()
+		{
+			var command = new RegisterPrePaidCardEmailCommand
+			{
+				Email = null
+			};
+
+			try
+			{
+				Drive.Api.Commands.Post(command);
+				Assert.Fail("Invalid command should fault");
+			}
+			catch (ValidatorException e)
+			{
+				Assert.IsTrue(e.Errors.Any(error => error == ErrorOpsRequestXmlInvalid));
+			}
+		}
+
+		[Test, AUT(AUT.Ca), JIRA("CA-2266")]
+		[Row("invalid.email")]
+		[Row("invalid@email*.com")]
+		[Row("invalid@@email.com")]
+		public void RegisterPrePaidCardEmailCommandShouldFailWithInvalidEmail(string invalidEmail)
+		{
+			var command = new RegisterPrePaidCardEmailCommand
+			{
+				Email = invalidEmail
+			};
+
+			try
+			{
+				Drive.Api.Commands.Post(command);
+				Assert.Fail("Invalid command should fault");
+			}
+			catch (ValidatorException e)
+			{
+				Assert.IsTrue(e.Errors.Any(error => error == ErrorMarketingEmailHasInvalidFormat));
+			}
+		}
+
+
+		[Test, AUT(AUT.Ca), JIRA("CA-2266")]
+		public void RegisterPrePaidCardEmailCommandShouldFailForExistingEmail()
+		{
+			string existingEmail = Get.GetEmail(50);
+			_marketingDb.RegisteredPrePaidCardEmails.UpsertByEmail(Email: existingEmail, CreatedOn: DateTime.UtcNow);
+
+			var command = new RegisterPrePaidCardEmailCommand
+			{
+				Email = existingEmail
+			};
+
+			try
+			{
+				Drive.Api.Commands.Post(command);
+				Assert.Fail("Invalid command should fault");
+			}
+			catch (ValidatorException e)
+			{
+				Assert.IsTrue(e.Errors.Any(error => error == ErrorMarketingEmailAlreadyExists));
+			}
+		}
 
 	}
 }
