fc0993facfb3e42b6863f030cde91d78b38a4a0e
Adrian Murphy <adrian.murphy@wonga.com>
2012-06-07 17:09:54 +0100

    Readded InDuplumViolationTest

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/InDuplumViolationTest.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/InDuplumViolationTest.cs
@@ -20,7 +20,7 @@ namespace Wonga.QA.Tests.Payments
 		private dynamic _applications = Drive.Data.Payments.Db.Applications;
 		private Customer _customer;
 
-		[Test, AUT(AUT.Za), JIRA("ZA-2201"), Pending("ZA-2565")]
+		[Test, AUT(AUT.Za), JIRA("ZA-2201")]
 		public void Close_InDuplumViolation_Application_Creates_CompensatingTransaction()
 		{
 			_customer = CustomerBuilder.New().Build();
@@ -78,65 +78,11 @@ namespace Wonga.QA.Tests.Payments
 			Assert.AreEqual(-124.1m ,compensatingTransaction.Amount);
 		}
 
-		[Test, AUT(AUT.Za), JIRA("ZA-2360"), DependsOn("Close_InDuplumViolation_Application_Creates_CompensatingTransaction"), Pending("ZA-2565")]
+		[Test, AUT(AUT.Za), JIRA("ZA-2360"), DependsOn("Close_InDuplumViolation_Application_Creates_CompensatingTransaction")]
 		public void Close_InDuplumViolation_Application_BalanceShouldBeZero()
 		{
 			Do.Until(() => decimal.Parse(GetLoanAgreements(_customer).Values["TodaysBalance"].SingleOrDefault()) == 0);
 			Do.Until(() => decimal.Parse(GetLoanAgreements(_customer).Values["FinalBalance"].SingleOrDefault()) == 0);
-		}
-
-		[Test, AUT(AUT.Za), JIRA("ZA-2201"), Pending("ZA-2565")]
-		public void Close_NonInDuplumViolation_Application_DoesNotCreate_CompensatingTransaction()
-		{
-			var customer = CustomerBuilder.New().Build();
-			var app = ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Accepted).WithLoanAmount(100).
-				Build();
-
-			var loanApp = Do.Until(() => _applications.FindAllByExternalId(app.Id).Single());
-
-
-			//fire refund transaction which cause InDuplum violation
-			Drive.Msmq.Payments.Send(new CreateTransactionCommand
-			{
-				Amount = 10,
-				ApplicationId = app.Id,
-				Currency = CurrencyCodeIso4217Enum.ZAR,
-				ExternalId = Guid.NewGuid(),
-				ComponentTransactionId = Guid.Empty,
-				PostedOn = DateTime.UtcNow,
-				Scope = PaymentTransactionScopeEnum.Debit,
-				Source = PaymentTransactionSourceEnum.System,
-				Type = PaymentTransactionEnum.Refund
-			});
-
-
-			//fire Direct Bank payment transaction to close off application so that compensating transaction 
-			//will be created.
-			Drive.Msmq.Payments.Send(new CreateTransactionCommand
-			{
-				Amount = -184.1m, //Just enought to pay off Loan
-				ApplicationId = app.Id,
-				Currency = CurrencyCodeIso4217Enum.ZAR,
-				ExternalId = Guid.NewGuid(),
-				ComponentTransactionId = Guid.Empty,
-				PostedOn = DateTime.UtcNow,
-				Scope = PaymentTransactionScopeEnum.Credit,
-				Source = PaymentTransactionSourceEnum.System,
-				Type = PaymentTransactionEnum.DirectBankPayment
-			});
-
-			//ClosedApplicationSaga timesout after 30sec before closing off application
-			//Therefore delay is necessay
-			Do.Until(() => app.IsClosed == false);
-
-			//Assert
-			var compensatingTransaction = _transactions.FindAll(_transactions.ApplicationId == loanApp.ApplicationId && 
-																_transactions.Type == PaymentTransactionEnum.WriteOff.ToString())
-																.FirstOrDefault();
-
-			loanApp = Do.Until(() => _applications.FindAllByExternalId(app.Id).Single());
-
-			Assert.IsNull(compensatingTransaction);
 		}
 
 		#region Helpers
