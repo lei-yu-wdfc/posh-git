4a683d04de0b9e9e934b36ef0cfda2a3b523f838
Robin Dewson <robin.dewson@wonga.com>
2012-06-14 08:50:58 +0100

    DI-689 a few tweaks for the HDS server connection

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Data/DataDriver.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Data/DataDriver.cs
@@ -24,13 +24,13 @@ namespace Wonga.QA.Framework.Data
         private Lazy<CallReportDatabase> _callReport = new Lazy<CallReportDatabase>(() => new CallReportDatabase(Config.Db.CallReport));
         private Lazy<CallValidateDatabase> _callValidate = new Lazy<CallValidateDatabase>(() => new CallValidateDatabase(Config.Db.CallValidate));
         private Lazy<CardPaymentDatabase> _cardPayment = new Lazy<CardPaymentDatabase>(() => new CardPaymentDatabase(Config.Db.CardPayment));
-        private Lazy<CdcDatabase> _cdc = new Lazy<CdcDatabase>(() => new CdcDatabase(Config.Db.Cdc));
+        private Lazy<CdcDatabase> _cdc = new Lazy<CdcDatabase>(() => new CdcDatabase(Config.HDSDb.Cdc));
         private Lazy<ColdStorageDatabase> _coldStorage = new Lazy<ColdStorageDatabase>(() => new ColdStorageDatabase(Config.Db.ColdStorage));
         private Lazy<ContactManagementDatabase> _contactManagement = new Lazy<ContactManagementDatabase>(() => new ContactManagementDatabase(Config.Db.ContactManagement));
         private Lazy<ExperianDatabase> _experian = new Lazy<ExperianDatabase>(() => new ExperianDatabase(Config.Db.Experian));
         private Lazy<ExperianBulkDatabase> _experianBulk = new Lazy<ExperianBulkDatabase>(() => new ExperianBulkDatabase(Config.Db.ExperianBulk));
         private Lazy<FileStorageDatabase> _fileStorage = new Lazy<FileStorageDatabase>(() => new FileStorageDatabase(Config.Db.FileStorage));
-        private Lazy<HdsDatabase> _hds = new Lazy<HdsDatabase>(() => new HdsDatabase(Config.Db.Hds));
+        private Lazy<HdsDatabase> _hds = new Lazy<HdsDatabase>(() => new HdsDatabase(Config.HDSDb.Hds));
         private Lazy<HpiDatabase> _hpi = new Lazy<HpiDatabase>(() => new HpiDatabase(Config.Db.Hpi));
         private Lazy<IpLookupDatabase> _ipLookup = new Lazy<IpLookupDatabase>(() => new IpLookupDatabase(Config.Db.IpLookup));
         private Lazy<QaDataDatabase> _qaData = new Lazy<QaDataDatabase>(() => new QaDataDatabase(Config.Db.QaData));
@@ -80,5 +80,6 @@ namespace Wonga.QA.Framework.Data
         public PrepaidCardDatabase PrepaidCard { get { return _prepaidCard.Value; } }
         public PpsDatabase Pps { get { return _pps.Value; } }
         public string NameOfServer { get { return Config.Db.ServerName; } }
+        public string NameOfHdsServer { get { return Config.HDSDb.HdsServerName; } }
     }
 }
