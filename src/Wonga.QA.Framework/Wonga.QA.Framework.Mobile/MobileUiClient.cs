b303ad845879c98559a829d4bd4e4748ef4bca67
Benjamin Ifie <ben.ifie@wonga.com>
2012-06-14 15:41:56 +0100

    added mobile framework project

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Mobile/MobileUiClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading;
+using Gallio.Framework;
+using Gallio.Framework.Assertions;
+using MbUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Wonga.QA.Framework.Core;
+
+
+namespace Wonga.QA.Framework.Mobile
+{
+    public class MobileUiClient
+    {
+    }
+}
