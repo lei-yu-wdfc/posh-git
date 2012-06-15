48654a15ea6b04a0205b663821f738991476d3ca
whytepaul <paul.whyte@wonga.com>
2012-06-15 14:10:35 +0100

    UKRISK-72 Removed IsEmployed QAF TESTS for Lukasz to re-implement

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointCustomerIsEmployedTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointCustomerIsEmployedTests.cs
@@ -15,42 +15,6 @@ namespace Wonga.QA.Tests.Risk.Checkpoints
 	[Parallelizable(TestScope.All), AUT(AUT.Za,AUT.Uk)]
 	public class CheckpointCustomerIsEmployedTests
 	{
-        private const RiskMask TestMask = RiskMask.TESTCustomerIsEmployed;
-		private Application l0Application;
-
-		[Test]
-        [JIRA("UK-1566")]
-		public void L0_CustomerIsEmployed_LoanIsAccepted()
-		{
-			var customer = CustomerBuilder.New().WithEmployer(TestMask).WithEmployerStatus(EmploymentStatusEnum.EmployedFullTime.ToString()).Build();
-			l0Application =  ApplicationBuilder.New(customer).Build();
-
-            var riskWorkflows = Drive.Db.GetWorkflowsForApplication(l0Application.Id, RiskWorkflowTypes.MainApplicant);
-            Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
-            Assert.Contains(Drive.Db.GetExecutedCheckpointDefinitionNamesForRiskWorkflow(riskWorkflows[0].WorkflowId, RiskCheckpointStatus.Verified), Get.EnumToString(RiskCheckpointDefinitionEnum.CustomerIsEmployed));
-		}
-
-		[Test]
-        [JIRA("UK-1566")]
-		public void L0_CustomerIsUnemployed_LoanIsDeclined()
-		{
-			var customer = CustomerBuilder.New().WithEmployer(TestMask).WithEmployerStatus(EmploymentStatusEnum.Unemployed.ToString()).Build();
-            var application = ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Declined).Build();
-
-            var riskWorkflows = Drive.Db.GetWorkflowsForApplication(application.Id, RiskWorkflowTypes.MainApplicant);
-            Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
-            Assert.Contains(Drive.Db.GetExecutedCheckpointDefinitionNamesForRiskWorkflow(riskWorkflows[0].WorkflowId, RiskCheckpointStatus.Failed), Get.EnumToString(RiskCheckpointDefinitionEnum.CustomerIsEmployed));
-		}
-
-		[Test, DependsOn("L0_CustomerIsEmployed_LoanIsAccepted"), JIRA("UK-1566")]
-        public void Ln_CustomerIsEmployed_LoanIsAccepted()
-		{
-			l0Application.RepayOnDueDate();
-
-            var lnApplication = ApplicationBuilder.New(l0Application.GetCustomer()).WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
-            var riskWorkflows = Drive.Db.GetWorkflowsForApplication(lnApplication.Id, RiskWorkflowTypes.MainApplicant);
-            Assert.AreEqual(riskWorkflows.Count, 1, "There should be 1 risk workflow");
-            Assert.Contains(Drive.Db.GetExecutedCheckpointDefinitionNamesForRiskWorkflow(riskWorkflows[0].WorkflowId, RiskCheckpointStatus.Verified), Get.EnumToString(RiskCheckpointDefinitionEnum.CustomerIsEmployed));
-        }
+        
 	}
 }
