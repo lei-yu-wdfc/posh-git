689ea60464772b92469da0aef6e6eec9e9bcd0d6
lukasztyde <lukasz.tyde@wonga.com>
2012-06-15 16:10:29 +0100

    redone UKRISK-72

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointCustomerIsEmployedTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/Checkpoints/CheckpointCustomerIsEmployedTests.cs
@@ -12,9 +12,38 @@ using Wonga.QA.Tests.Core;
 
 namespace Wonga.QA.Tests.Risk.Checkpoints
 {
-	[Parallelizable(TestScope.All), AUT(AUT.Za,AUT.Uk)]
-	public class CheckpointCustomerIsEmployedTests
-	{
-        
-	}
+    [TestFixture]
+    [Parallelizable(TestScope.All), AUT(AUT.Uk,AUT.Ca)]
+    public class CheckpointCustomerIsEmployedTests
+    {
+        private const RiskMask TestMask = RiskMask.TESTCustomerIsEmployed;
+        private Application _loanApplication;
+
+        [Test]
+        [JIRA("UKRISK-72"), Description("Scenario 1: Customer is uneploymend, application declined")]
+        public void L0CustomerIsUnemployedThenApplicationDeclined()
+        {
+            var customer = CustomerBuilder.New().WithEmployer(TestMask).WithEmployerStatus(EmploymentStatusEnum.Unemployed.ToString()).Build();
+            ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Declined).Build();
+        }
+
+        [Test]
+        [JIRA("UKRISK-72"), Description("Scenario 2: Customer is eploymend, application accepted")]
+        public void L0CustomerEmployedThenApplicationAccepted()
+        {
+            var customer = CustomerBuilder.New().WithEmployer(TestMask).WithEmployerStatus(EmploymentStatusEnum.EmployedFullTime.ToString()).Build();
+            _loanApplication = ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build();
+        }
+
+        [Test]
+        [JIRA("UKRISK-72"), DependsOn("L0CustomerEmployedThenApplicationAccepted")]
+        public void LnCustomerEmployedThenApplicationAccepted()
+        {
+            _loanApplication.RepayOnDueDate();
+            ApplicationBuilder.New(_loanApplication.GetCustomer()).WithExpectedDecision(
+                ApplicationDecisionStatus.Accepted).Build();
+
+        }
+
+    }
 }
