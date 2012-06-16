0ca13ce4e5a3f16fe4ad41cf2a297d5f20752702
JonHurdWonga <jon.hurd@wonga.com>
2012-06-16 19:54:41 +0100

    code gen stuff

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Requests/Risk/Commands/Pl/SaveEmploymentDetailsPlCommand.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Requests/Risk/Commands/Pl/SaveEmploymentDetailsPlCommand.cs
@@ -23,28 +23,3 @@ namespace Wonga.QA.Framework.Api.Requests.Risk.Commands.Pl
         public Object MonthsAtEmployer { get; set; }
     }
 }
-
-using System;
-using System.Xml.Serialization;
-
-namespace Wonga.QA.Framework.Api
-{
-    /// <summary> Wonga.Risk.Commands.Pl.SaveEmploymentDetailsPl </summary>
-    [XmlRoot("SaveEmploymentDetailsPl")]
-    public partial class SaveEmploymentDetailsPlCommand : ApiRequest<SaveEmploymentDetailsPlCommand>
-    {
-        public Object AccountId { get; set; }
-        public Object NetMonthlyIncome { get; set; }
-        public Object IncomeFrequency { get; set; }
-        public Object NextPayDate { get; set; }
-        public Object Status { get; set; }
-        public Object StatusOther { get; set; }
-        public Object EmploymentIndustry { get; set; }
-        public Object EmploymentIndustryOther { get; set; }
-        public Object UniversityType { get; set; }
-        public Object EmployerName { get; set; }
-        public Object YearsAtEmployer { get; set; }
-        public Object MonthsAtEmployer { get; set; }
-        public Object PaidDirectDeposit { get; set; }
-    }
-}
