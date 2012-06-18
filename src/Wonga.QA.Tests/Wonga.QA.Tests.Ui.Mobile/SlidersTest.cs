6042ef7d8428a5593e4f72f3bf52e7fbc71f7a80
Benjamin Ifie <ben.ifie@wonga.com>
2012-06-18 18:02:04 +0100

    added Sliders tests for mobile

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Ui.Mobile/SlidersTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using MbUnit.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Ui.Mobile
+{
+    public class SlidersTests : UiMobileTest
+    {
+        [Test, AUT(AUT.Za)]
+        public void IncreaseThenDecreaseAmountAndDurationValuesUsingSliders()
+        {
+            var homepage = Client.MobileHome();
+            homepage.Sliders.HowMuch = "200";
+            homepage.Sliders.HowLong = "10";
+            //drag sliders increase values
+            homepage.Sliders.MoveAmountSlider = 300;
+            homepage.Sliders.MoveDurationSlider = 250;
+            Assert.GreaterThan(Convert.ToInt32(homepage.Sliders.HowMuch), 200);
+            Assert.GreaterThan(Convert.ToInt32(homepage.Sliders.HowLong), 10);
+            var amtAfterSliderIncrease = homepage.Sliders.HowMuch;
+            var durationAfterSliderIncrease = homepage.Sliders.HowLong;
+            //drag sliders decrease values
+            homepage.Sliders.MoveAmountSlider = -200;
+            homepage.Sliders.MoveDurationSlider = -150;
+            Assert.GreaterThan(Convert.ToInt32(amtAfterSliderIncrease), Convert.ToInt32(homepage.Sliders.HowMuch));
+            Assert.GreaterThan(Convert.ToInt32(durationAfterSliderIncrease), Convert.ToInt32(homepage.Sliders.HowLong));
+        }
+
+        [Test, AUT(AUT.Za)]7
+        public void SlideAmountSlidersUntilMaximumValueIsReached()
+        {
+            var homepage = Client.MobileHome();
+            homepage.Sliders.HowMuch = "0";
+            //check that value defaults to 100 after 0 is entered in amount field
+            Assert.AreEqual(homepage.Sliders.HowMuch, "100");
+            //drag sliders until value is 2000
+            do
+            {
+                Thread.Sleep(1000);
+                homepage.Sliders.MoveAmountSlider = 90;
+            } while (Convert.ToInt32(homepage.Sliders.HowMuch) < 2000);
+        }
+    }
+}
