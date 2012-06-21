1d820e01f5a70c3fc3c3d1c656fdc95eed24d7c8
Michael.Nowicki <michael.nowicki@wonga.com>
2012-06-21 17:25:06 +0100

    VerifyIsClickParamsSet -> ENABLE

--- src/Wonga.QA.Tests/Wonga.QA.Tests.UI/ClickTest.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.UI/ClickTest.cs
@@ -22,7 +22,7 @@ namespace Wonga.QA.Tests.Ui
 
         }
 
-        [Test, AUT(AUT.Ca, AUT.Za), JIRA("CA-2326"), Ignore("Feature turned off for this release")]
+        [Test, AUT(AUT.Ca, AUT.Za), JIRA("CA-2326")]
         public void VerifyIsClickParamsSet()
         {
             var homePage = Client.Home();
