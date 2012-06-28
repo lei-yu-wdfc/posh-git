c9372f10766e0a00939b9a3b933cb9063aaec48b
Mihail <mihail.podobivsky@softheme.com>
2012-06-28 17:38:35 +0300

    add Emailer project

---
+++ src/Wonga.QA.Emailer/Wonga.QA.Emailer.Plugins.SendFailingSmokeTestEmails/Emailer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using Wonga.QA.Emailer.Domain;
+using Wonga.QA.Emailer.XmlParser;
+
+namespace Wonga.QA.Emailer.Plugins.SendFailingSmokeTestEmails
+{
+    public class FailingSmokeTestsEmailer : ISendEmails
+    {
+        public bool SendEmail(List<TestReport> reports, String email, SmtpClient client)
+        {
+            Console.WriteLine(CreateEmailText(reports, email));
+           // client.Send(SmtpSettingsReader.GetAccount(), email, "Your tests are failed", CreateEmailText(reports, email));
+            return false;
+        }
+
+        private string CreateEmailText(List<TestReport> reports, String email)
+        {
+            string autor = email.Remove(email.IndexOf("@"));
+
+            autor = autor.Replace(autor[0].ToString(), autor[0].ToString().ToUpper());
+            if (autor.Contains("."))
+            {
+                autor = autor.Replace(".", " ");
+                autor = autor.Replace(autor[autor.IndexOf(" ") + 1].ToString(), autor[autor.IndexOf(" ") + 1].ToString().ToUpper());
+            }
+
+            string message = "Hi " + autor + " your tests are fails.\n\n";
+
+            foreach (TestReport testReport in reports)
+            {
+                if (testReport.OwnerEmails.Contains(email))
+                {
+                    DateTime date = DateTime.Parse(testReport.StartTime.Remove(9));
+                    string time = testReport.StartTime.Remove(0, 11).Remove(8);
+                    message += testReport.TestName + ", was failed.\n\n" +
+                               "Test was run " + date.ToString("dd MMMM yyyy") + " at " + time + ".\n\n";
+                    if (testReport.JIRA != null)
+                    {
+                        message += "Tiket in JIRA: " + testReport.JIRA + ".\n\n";
+                    }
+                    message += "Test location: " + testReport.FullTestName + ".\n\n" +
+                               "Fail message: \n\n" + testReport.Message + ".\n\n";
+                }
+            }
+            message += "Please look on your tests.\n\n";
+            message += "This email was generated automatically, don't reply on it.\n\n";
+            return message;
+        }
+    }
+}
---
+++ src/Wonga.QA.Emailer/Wonga.QA.Emailer/ISendEmails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using Wonga.QA.Emailer.Domain;
+
+namespace Wonga.QA.Emailer
+{
+    public interface ISendEmails
+    {
+        bool SendEmail(List<TestReport> reports, String email, SmtpClient client);
+    }
+}
