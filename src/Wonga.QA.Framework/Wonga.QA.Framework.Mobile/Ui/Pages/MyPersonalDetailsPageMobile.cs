de02bba6bfb1d032ca0248ff7f7875586025fee1
Benjamin Ifie <ben.ifie@wonga.com>
2012-06-29 11:03:30 +0100

    Popup element classes added

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Mobile/Ui/Pages/MyPersonalDetailsPageMobile.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Mobile/Ui/Pages/MyPersonalDetailsPageMobile.cs
@@ -7,6 +7,7 @@ using MbUnit.Framework;
 using OpenQA.Selenium;
 using Wonga.QA.Framework.Core;
 using Wonga.QA.Framework.Mobile.Mappings.Ui;
+using Wonga.QA.Framework.Mobile.Ui.Elements;
 
 namespace Wonga.QA.Framework.Mobile.Ui.Pages
 {
@@ -17,14 +18,6 @@ namespace Wonga.QA.Framework.Mobile.Ui.Pages
         private readonly IWebElement _passwordLink;
         private readonly IWebElement _phoneLink;
         private readonly IWebElement _communicationLink;
-        private IWebElement _popUp;
-
-        private IWebElement _currentPassword;
-        private IWebElement _editPassword;
-        private IWebElement _editPasswordConfirm;
-        private IWebElement _submit;
-
-        private IWebElement _popUpTitle;
         
         public MyPersonalDetailsPageMobile(MobileUiClient client) : base(client)
         {
@@ -37,19 +30,8 @@ namespace Wonga.QA.Framework.Mobile.Ui.Pages
         public void EditPassword(string currentPassword, string newPassword)
         {
             _passwordLink.Click();
-            _popUp = Do.Until(() => Client.Driver.FindElement(By.CssSelector("div#fancybox-content")));
-            _popUpTitle = Do.Until(() => _popUp.FindElement(By.CssSelector(".modal h1")));
-            Assert.IsTrue(_popUpTitle.Text.Equals("Change your password"));
-            _currentPassword = _popUp.FindElement(By.CssSelector("#edit-current-password"));
-            _editPassword = _popUp.FindElement(By.CssSelector("#edit-password"));
-            _editPasswordConfirm = _popUp.FindElement(By.CssSelector("#edit-password-confirm"));
-            _currentPassword.SendKeys(currentPassword);
-            _editPassword.SendKeys(newPassword);
-            _editPasswordConfirm.SendKeys(newPassword);
-            _submit = _popUp.FindElement(By.CssSelector("#edit-submit"));
-            _submit.Click();
-            Do.Until(() => Client.Driver.FindElement(By.CssSelector(".modal h1")).Text.Equals("Success"));
-            Client.Driver.FindElement(By.CssSelector("div#fancybox-wrap a#fancybox-close")).Click();
+            var editPasswordPopUp = Do.Until(() => new EditPasswordPopUpElement(this));
+            editPasswordPopUp.EditPassword(currentPassword, newPassword);
         }
     }
 }
