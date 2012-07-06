ced724bd1d9eeee14d0721d7a0907958a3f9cfd1
Muhammad Qureshi <muhammad.qureshi@wonga.com>
2012-07-06 09:34:24 +0100

    migration test additions

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Data/MigrationHelper.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Data/MigrationHelper.cs
@@ -12,19 +12,49 @@ namespace Wonga.QA.Framework.Data
     {
         private readonly DataDriver _drive = new DataDriver();
 
-        public string GetMigratedAccountLogin()
+        public string GetMigratedAccountLogin(dynamic callUser = null)
         {
             var opsAccounts = _drive.Ops.Db.Accounts;
 
             var totalUsers = opsAccounts.All().Count();
 
-            var user = new Random();
-            
-            var callUser = user.Next(1, totalUsers);
+            if (callUser == null || Convert.ToInt64(callUser) > Convert.ToInt64(totalUsers) || Convert.ToInt64(callUser) == 0)
+            {
+                var user = new Random();
+
+                callUser = user.Next(1, totalUsers);
+            }
 
             var userName = opsAccounts.FindByAccountId(callUser);
 
             return userName.Login;
+        }
+
+        public List<int> GetListOfMigratedAccounts(string year = null, string range =null)
+        {
+
+            return null;
+        }
+
+        public List<dynamic> GetMigratedUserWithPasswords()
+        {
+            List<dynamic> returnThis = new List<dynamic>();
+            var opsAccounts = _drive.Ops.Db.Accounts;
+
+            var totalUsers = opsAccounts.All().Count();
+
+            for (int user = 1; user <= totalUsers; user++)
+            {
+                var userName = GetMigratedAccountLogin(user);
+                var password = GetMigratedAccountLoginPassword(userName);
+
+                if (password.Length <= 6)
+                {
+                    returnThis.Add(userName + " " + password);    
+                }
+            }
+
+            return returnThis;
         }
 
         public string GetMigratedAccountLoginPassword(string login)
@@ -32,7 +62,7 @@ namespace Wonga.QA.Framework.Data
             var greyfaceUsers = _drive.WongaWholeStaging.Db.greyface.Users;
 
             var passwordHashKey = greyfaceUsers.FindByuser_name(login).password;
-            
+
             DataSet dsDecryptedPassword = new DataSet();
             String query = "select * from [GreyfaceShell].[dbo].[GetUserPassword](@password)"; //GetUserPassword is table-valued n it can't be done with simple.Data
             SqlCommand cmd = new SqlCommand(query, new SqlConnection("Data Source='mig-int-v2.test.wonga.com';Initial Catalog=GreyfaceShell;Integrated Security=True"));
