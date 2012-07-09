02611e96a1424f42ef9121124ed9386383cb1cbc
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-07-09 14:06:48 +0300

    few changes in tools

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Core/Class.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Wonga.QA.Framework.Core
+{
+    public abstract class Class
+    {
+        private static byte[] KEY = new byte[] { 90, 22, 35, 10, 233, 194, 5, 118, 28, 13, 153, 39, 22, 165, 23, 200, 132, 21, 17, 155, 97, 31, 179, 36, 200, 167, 34, 136, 252, 130, 119, 50 };
+        private static byte[] IV = new byte[] { 18, 230, 69, 209, 5, 184, 39, 165, 27, 255, 241, 136, 83, 204, 210, 199 };
+        public static string Encrypt(string inputString)
+        {
+            if (String.IsNullOrEmpty(inputString))
+            {
+                throw new ArgumentException("Input string should be defined");
+            }
+            string result = "";
+            using (Rijndael rd = Rijndael.Create())
+            {
+                rd.Key = KEY;
+                rd.IV = IV;
+                ICryptoTransform encryptor = rd.CreateEncryptor(rd.Key, rd.IV);
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var streamWriter = new StreamWriter(cryptoStream))
+                        {
+                            streamWriter.Write(inputString);
+                        }
+                        var encryptedArray = memoryStream.ToArray();
+                        foreach (var b in encryptedArray)
+                        {
+                            result += b.ToString() + "-";
+                        }
+                    }
+                }
+            }
+            return result.Remove(result.Length - 1);
+        }
+
+        public static string Decrypt(string inputString)
+        {
+            if (String.IsNullOrEmpty(inputString))
+            {
+                throw new ArgumentException("Input string should be defined");
+            }
+            string result = "";
+            var stringArray = inputString.Split('-');
+            byte[] byteArray = new byte[stringArray.Length];
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                byteArray[i] = Byte.Parse(stringArray[i]);
+            }
+            using (Rijndael rd = Rijndael.Create())
+            {
+                rd.Key = KEY;
+                rd.IV = IV;
+
+                ICryptoTransform decryptor = rd.CreateDecryptor(rd.Key, rd.IV);
+
+                using (var memoryStream = new MemoryStream(byteArray))
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var streamReader = new StreamReader(cryptoStream))
+                        {
+                            result = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Wonga.QA.Tools/Program/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Wonga.QA.Framework.Core;
+
+namespace Program
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            string inputString = "";
+
+            var opt = new ConsoleKeyInfo();
+            var opt2 = new ConsoleKeyInfo();
+            while (opt.KeyChar != '0')
+            {
+                Console.Clear();
+                Console.WriteLine("Enter an option");
+                Console.WriteLine("\t1. Encrypt a string");
+                Console.WriteLine("\t2. Decrypt a string");
+                opt = Console.ReadKey();
+                switch (opt.KeyChar)
+                {
+                    case ('1'):
+                        string encrypted = "";
+                        Console.WriteLine();
+                        Console.Write("Press 1 to paste string from clipboard or press 2 to enter it: ");
+                        opt2 = Console.ReadKey();
+                        switch (opt2.KeyChar)
+                        {
+                            case ('1'):
+                                Console.WriteLine(Clipboard.GetText());
+                                encrypted = Class.Encrypt(Clipboard.GetText());
+                                break;
+
+                            case ('2'):
+                                Console.WriteLine();
+                                Console.Write("String to encrypt: ");
+                                inputString = Console.ReadLine();
+                                encrypted = Class.Encrypt(inputString);
+                                break;
+                        }
+                        Console.WriteLine(encrypted);
+                        Clipboard.SetText(encrypted);
+                        Console.WriteLine("Encrypted string copied to clipboard");
+                        break;
+                    case ('2'):
+                        string decrypted = "";
+                        Console.WriteLine();
+                        Console.Write("Press 1 to paste string from clipboard or press 2 to enter it: ");
+                        opt2 = Console.ReadKey();
+                        switch (opt2.KeyChar)
+                        {
+                            case ('1'):
+                                Console.WriteLine(Clipboard.GetText());
+                                decrypted = Class.Decrypt(Clipboard.GetText());
+                                break;
+
+                            case ('2'):
+                                Console.WriteLine();
+                                Console.Write("String to decrypt: ");
+                                inputString = Console.ReadLine();
+                                decrypted = Class.Decrypt(inputString);
+                                break;
+                        }
+                        Console.WriteLine(decrypted);
+                        Clipboard.SetText(decrypted);
+                        Console.WriteLine("Decrypted string copied to clipboard");
+                        break;
+                }
+                Console.WriteLine("\nPress 0 to exit or other key to proceed");
+                opt = Console.ReadKey();
+
+            }
+        }
+    }
+}
