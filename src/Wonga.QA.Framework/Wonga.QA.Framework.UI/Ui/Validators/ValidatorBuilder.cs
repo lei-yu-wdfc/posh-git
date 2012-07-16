2a1f8b5735cda793277568c9b7fac1710897538e
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2012-07-16 14:19:36 +0300

    Validators added

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Validators/ValidatorBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.Framework.UI.Ui.Validators
+{
+    public static class ValidatorBuilder
+    {
+        
+        public static Validator Validator;
+        
+        public static Validator New(UiClient client)
+        {
+            Validator = new Validator(client); 
+            return Validator;
+        }
+
+        public static Validator Default(UiClient client)
+        {
+            Validator = new Validator(client);
+            Validator.Checks.Add(Validator.ErrorsCheck);
+            Validator.Checks.Add(Validator.InvalidFormErrorCheck);
+            Validator.Checks.Add(Validator.HeadersCheck);
+            Validator.Checks.Add(Validator.TitleCheck);
+            return Validator;
+        }
+
+        public static Validator WithoutErrorsCheck()
+        {
+            Validator.Checks.Remove(Validator.ErrorsCheck);
+            return Validator;
+        }
+
+
+    }
+}
