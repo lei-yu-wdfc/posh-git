b74a65124ec469ab2e3a9c472b7af030f89b77e0
Konstantinos Konstantinidis <konstantinos.konstantinidis@wonga.com>
2012-07-26 14:27:38 +0100

    removing some warnings

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Tests/Core/DoTests.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Tests/Core/DoTests.cs
@@ -15,8 +15,10 @@ namespace Wonga.QA.Framework.Tests.Core
         {
             var ex = Assert.Throws<DoException>(() => Do.With.Interval(1).Timeout(TimeSpan.FromSeconds(3)).Until(() =>
                          {
-                             throw new Exception();
-                             return 0;
+                             var i = 0;
+                             if(0 == i)
+                                throw new Exception();
+                             return i;
                          }));
             Assert.AreEqual(2, ex.Exceptions.Count);
         }
@@ -32,7 +34,7 @@ namespace Wonga.QA.Framework.Tests.Core
                     i++;
                     throw new Exception();
                 }
-                throw new ExecutionEngineException();
+                else if(i > 0) throw new NotImplementedException();
                 return 0;
             }));
             Assert.AreEqual(3, ex.Exceptions.Count);
