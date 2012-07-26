6888df533b60303acd0570f644b084df92f7ff02
muhammad83 <muhammad.qureshi@wonga.com>
2012-07-26 16:02:22 +0100

    Migration Tests solution separated - new files added

---
+++ src/Wonga.QA.MigrationTests/Wonga.QA.Tests.Mig.UI/MigrationGenderTests.cs
@@ -0,0 +1,21 @@
+﻿using Wonga.QA.Framework.Api.Enums;
+using Wonga.QA.Framework.UI;
+using Wonga.QA.Framework.UI.UiElements.Pages.Common;
+using MbUnit.Framework;
+using Wonga.QA.Tests.Ui;
+
+namespace Wonga.QA.MigrationTests
+{
+    [Parallelizable(TestScope.All)]
+    public class MigrationGenderTests : UiTest
+    {
+        [Test]
+        public void AddCustomerWithTitleMrAndGenderFemale()
+        {
+            var journey = JourneyFactory.GetL0Journey(Client.Home())
+                .WithGender(GenderEnum.Female);
+
+            var acceptedPage = journey.Teleport<AcceptedPage>() as AcceptedPage;
+        }
+    }
+}
