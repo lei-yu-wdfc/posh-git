f179cfc28145838705261c813883a8b5791f9f5a
pavithran80 <pavithran.vangiti@wonga.com>
2012-07-26 17:57:06 +0100

    Added missing file

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Data/Enums/Sms/UkSmsStatuses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.Framework.Data.Enums.Sms
+{
+    public enum UkSmsStatuses
+    {
+        New = 0,
+        Acked = 1,
+        Delivered = 2,
+        Failed = 3,
+        BufferedPhone = 4,
+        BufferedSmsc = 5,
+        NonDelivered = 6,
+        LostNotification = 7,
+        UnknownNotification = 8
+    }
+}
