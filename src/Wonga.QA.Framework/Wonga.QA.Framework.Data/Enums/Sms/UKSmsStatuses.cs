56310323b7ab039b871ccbda1d396984ba0d164e
pavithran80 <pavithran.vangiti@wonga.com>
2012-07-27 11:20:01 +0100

    fixed

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Data/Enums/Sms/UKSmsStatuses.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Data/Enums/Sms/UKSmsStatuses.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace Wonga.QA.Framework.Data.Enums.Sms
 {
-    public enum UkSmsStatuses
+    public enum UKSmsStatuses
     {
         New = 0,
         Acked = 1,
