d4654f07c9dcb13ed450cc65d52172cffb283e7f
Taras Kudryavtsev <gdieho@gmail.com>
2012-07-31 14:38:51 +0300

    FIX CA-2444

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/AdditionalPayFrequencyCalculateTest.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Risk/AdditionalPayFrequencyCalculateTest.cs
@@ -10,6 +10,7 @@ using Wonga.QA.Tests.Payments.Helpers.Ca;
 
 namespace Wonga.QA.Tests.Risk
 {
+    [Parallelizable(TestScope.All)]
     class AdditionalPayFrequencyCalculateTest
     {
         [Test, AUT(AUT.Ca), JIRA("CA-2444")]
