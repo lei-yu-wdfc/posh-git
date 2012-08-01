1f69c401e4b8976a17bfc709364b7ac4db6b7346
Konstantinos Konstantinidis <konstantinos.konstantinidis@wonga.com>
2012-08-01 16:00:42 +0100

    addingi the generators to the build rake

--- run/ruby/build.rb
+++ run/ruby/build.rb
@@ -2,35 +2,43 @@ require 'albacore'
 require './ruby/common'
 
 #-- build tasks for each solution  
+desc 'Builds the framework'
 msbuild :framework do |msb|
   msb.solution = File.join(SRC, FRAMEWORK,  "#{FRAMEWORK}.sln")
 end
   
+ desc 'Builds the backend tests'
 msbuild :tests do |msb|
   msb.solution = File.join(SRC, TESTS, "#{TESTS}.sln")
 end
   
+ desc 'Builds the service tests'
 msbuild :service_tests do |msb|
   msb.solution = File.join(SRC, SERVICETESTS, "#{SERVICETESTS}.sln")
 end
   
+  desc 'Builds the data_tests'
 msbuild :data_tests do |msb|
   msb.solution = File.join(SRC, DATATESTS, "#{DATATESTS}.sln")
 end
 
+desc 'Builds the ui_tests'
 msbuild :ui_tests do |msb|
   msb.solution = File.join(SRC, UITESTS, "#{UITESTS}.sln")
 end
-  
+
+desc 'Builds the tools'  
 msbuild :tools do |msb|
   msb.solution = File.join(SRC, TOOLS, "#{TOOLS}.sln")
 end
 
+desc 'Builds the generators'
 msbuild :generators do |msb|
   msb.solution = File.join(SRC, GENERATORS, "#{GENERATORS}.sln")
 end
 #--
 
+desc 'Builds everything'
 task :build => [:framework, :tests, :ui_tests, :service_tests, :data_tests, :tools, :generators, :pre_generate_serializers]
 #--
 
