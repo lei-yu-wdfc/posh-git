aa2695121f37ec1a93e9ed1f74410653623b5dd8
Mihail <mihail.podobivsky@softheme.com>
2012-08-01 14:15:14 +0300

    QA-330, QA-329, QA-328

---
+++ src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/ApplicationOperationsTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+
+namespace Wonga.QA.UiTests.Web
+{
+    class ApplicationOperationsTests
+    {
+        [Test]
+        public void Froud()
+        {
+            var customer = CustomerBuilder.New().Build();
+            var application = ApplicationBuilder.New(customer).Build();
+            ApplicationOperations.ConfirmFraud(application,customer,Guid.NewGuid());
+        }
+
+        [Test]
+        public void HardShip()
+        {
+            var customer = CustomerBuilder.New().Build();
+            var application = ApplicationBuilder.New(customer).Build();
+            ApplicationOperations.ReportHardship(application, Guid.NewGuid());
+        }
+
+        [Test]
+        public void ManagementReview()
+        {
+            var customer = CustomerBuilder.New().Build();
+            var application = ApplicationBuilder.New(customer).Build();
+            ApplicationOperations.ManagementReview(application, Guid.NewGuid());
+        }
+
+        [Test]
+        public void Refund()
+        {
+            var customer = CustomerBuilder.New().Build();
+            var application = ApplicationBuilder.New(customer).Build();
+            ApplicationOperations.Refundrequest(application, Guid.NewGuid());
+        }
+    }
+}
