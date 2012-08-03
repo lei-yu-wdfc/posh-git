5dadf17b4956ae1f75022c2d80ea8deee15387f4
sova4life <sova4life@me.com>
2012-08-03 10:03:56 +0300

    Add Financial Assessment Income page test

--- src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/Region/Uk/FinancialAssessment/FinancialAssessmentIncomeTest.cs
+++ src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/Region/Uk/FinancialAssessment/FinancialAssessmentIncomeTest.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MbUnit.Framework;
+using Wonga.QA.Framework;
 using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.UI;
+using Wonga.QA.Framework.UI.UiElements.Pages.FinancialAssessment;
 using Wonga.QA.Tests.Core;
 using Wonga.QA.UiTests.Web;
 
@@ -12,9 +15,27 @@ namespace Wonga.QA.Tests.Ui.FinancialAssessment
     [TestFixture, Parallelizable(TestScope.All)]
     public class FinancialAssessmentIncomeTest : UiTest
     {
-        [Test, AUT(AUT.Uk), Category(TestCategories.SmokeTest), Pending("Financial Assessment")]
-        public void PrepopulatedNameCheck()
+        public Customer Init()
         {
+            var email = Get.RandomEmail();
+            Customer customer = CustomerBuilder.New().WithEmailAddress(email).Build();
+            ApplicationBuilder.New(customer).WithLoanTerm(4).Build();
+            return customer;
+        }
+
+        [Test, AUT(AUT.Uk), JIRA("UKOPS-720"), MultipleAsserts, Owner(Owner.SaveliyProkopenko), Category(TestCategories.SmokeTest)]
+        public void NextClickWithValidDataAndCheckDataAfterGoBack()
+        {
+            var loginPage = Client.Login();
+            Customer customer = Init();
+            loginPage.LoginAs(customer.Email);
+
+            var journeyLn = JourneyFactory.GetFaLnJourney(Client.FinancialAssessment());
+            var faincomepage = journeyLn.FillAndStop().WithEmployer(Get.GetEmployerName()).Teleport<FAIncomePage>() as FAIncomePage;
+
+            faincomepage.NextClick();
+
+          
         }
     }
 }
