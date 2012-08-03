41eab51cda92fd219d551345fc078b1425663b31
Konstantinos Konstantinidis <konstantinos.konstantinidis@wonga.com>
2012-08-03 18:12:09 +0100

    adding core_uitest task

--- run/ruby/test.rb
+++ run/ruby/test.rb
@@ -10,4 +10,8 @@ task :backend_test => [:config, :meta_test] do
   test 'Tests.*', 'Tests.Meta'
 end
 
+task :core_uitest => [:config] do
+  test 'UiTests.Web', '','Category:CoreTest'
+end
+
 task :sanity_test => [:config, :meta_test, :core_test]
