d1f1b8a1134e3abc19c144f20c00dbe47e078813
Mihail <mihail.podobivsky@softheme.com>
2012-08-06 17:04:05 +0300

    move QA-341, 342

--- src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/LnJourneyTests.cs
+++ src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/LnJourneyTests.cs
@@ -16,49 +16,6 @@ namespace Wonga.QA.UiTests.Web
     [TestFixture, Parallelizable(TestScope.All)]
     internal class LnJourneyTests : UiTest
     {
-        [Test, AUT(AUT.Uk), JIRA("QA-341"), Owner(Owner.MihailPodobivsky)]
-        public void CustomerOnTheDayBeforeDueDateShouldntBeAbleToTakeExtraCash()
-        {
-            var loginPage = Client.Login();
-            string email = Get.RandomEmail();
-            string name = Get.GetName();
-            string surname = Get.RandomString(10);
-            Customer customer = CustomerBuilder
-                .New()
-                .WithEmailAddress(email)
-                .WithForename(name)
-                .WithSurname(surname)
-                .Build();
-            Application application = ApplicationBuilder
-                .New(customer)
-                .WithLoanTerm(30)
-                .Build();
-            application.RewindApplicationDatesForDays(29);
-            var mySummaryPage = loginPage.LoginAs(email);
-            Assert.IsFalse(mySummaryPage.LookForTopupSliders());
-        }
-
-        [Test, AUT(AUT.Uk), JIRA("QA-342"), Owner(Owner.MihailPodobivsky)]
-        public void CustomerInArrearsShoudntBeAbleToTakeExtraCredit()
-        {
-            var loginPage = Client.Login();
-            string email = Get.RandomEmail();
-            string name = Get.GetName();
-            string surname = Get.RandomString(10);
-            Customer customer = CustomerBuilder
-                .New()
-                .WithEmailAddress(email)
-                .WithForename(name)
-                .WithSurname(surname)
-                .Build();
-            Application application = ApplicationBuilder
-                .New(customer)
-                .Build();
-            application.PutIntoArrears(10);
-            var mySummaryPage = loginPage.LoginAs(email);
-            Assert.IsFalse(mySummaryPage.LookForTopupSliders());
-        }
-
         [Test, AUT(AUT.Za), JIRA("QA-196"), Pending("ZA-2510"), Category(TestCategories.SmokeTest)]
         public void LnCustomerTakesNewLoanAndChangesTheMobilePhoneThenChangesShouldBeReflected()
         {
