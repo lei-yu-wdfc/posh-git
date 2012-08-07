af80a603e86fd4912ddaf7b6a327e00ead0d04de
muhammad83 <muhammad.qureshi@wonga.com>
2012-08-07 10:15:53 +0100

    Migrated users function additions

--- src/Wonga.QA.MigrationTests/Wonga.QA.MigrationTests/MigrationUiTests.cs
+++ src/Wonga.QA.MigrationTests/Wonga.QA.MigrationTests/MigrationUiTests.cs
@@ -30,9 +30,9 @@ namespace Wonga.QA.MigrationTests
 
         private string GetFunctionName()
         {
-            StackTrace stackTrace = new StackTrace();
-            StackFrame stackFrame = stackTrace.GetFrame(1);
-            MethodBase methodBase = stackFrame.GetMethod();
+            var stackTrace = new StackTrace();
+            var stackFrame = stackTrace.GetFrame(1);
+            var methodBase = stackFrame.GetMethod();
 
             return methodBase.Name;
         }
@@ -102,66 +102,20 @@ namespace Wonga.QA.MigrationTests
 
 
         [Test, AUT(AUT.Uk), Owner(Owner.MuhammadQureshi)]
-        [Row(2, "2010")]
-        public void TestMigratedUserCanTakeANewLoan(int noOfTimesToRun, string userCreatedInYear)
+        public void TestMigratedUserCanTakeANewLoan()
         {
             _testName = GetFunctionName();
-
             //migHelper.FillAcceptanceTestControlTable();
+            _testStartTime = DateTime.Now;
+            //_migratedUser = new MigratedUser();
+            //_migratedUser = _migHelper.GetMigratedAccountLogin(null, null);
 
-            Console.WriteLine(
-                "UKMIG-226, As an existing migrated customer, I want to take out a new loan so that I can get extra cash when I need it, " +
-                _testName);
+            var loginPage = Client.Login();
+            var email = CustomerBuilder.RandomLnCustomerEmail();
+            loginPage.LoginAs("qa.wonga.com+BUILD-WIN7-ccf7b794-1ab4-4536-991c-9bfc95ee0652@gmail.com", "Passw0rd");
+            var journey = JourneyFactory.GetLnJourney(Client.Home()).WithAmount(10).WithDuration(1);
+            var page = journey.Teleport<MySummaryPage>() as MySummaryPage;
 
-            for (int usersToCheck = 1; usersToCheck < noOfTimesToRun; usersToCheck++)
-            {
-                _testStartTime = DateTime.Now;
-                _migratedUser = new MigratedUser();
-                _migratedUser = _migHelper.GetMigratedAccountLogin(null, null);
-
-                using (var loginPage = new UiClient())
-                {
-                    try
-                    {
-                        loginPage.Login().LoginAs(_migratedUser.Login, _migratedUser.Password);
-                        var journey = JourneyFactory.GetLnJourney(Client.Home());
-                        var page = journey.Teleport<MySummaryPage>() as MySummaryPage;
-                        _loginStatus = 1;
-                    }
-                    catch (Exception)
-                    {
-                        _loginStatus = 0;
-                    }
-                    finally
-                    {
-                        _testEndTime = DateTime.Now;
-                        StoreTestResults();
-                        //Console.WriteLine("\n{0}: {1} Login = {2} \nUser Created in Year = {3}", usersToCheck,
-                        //                loginStatus, migratedUser, userCreatedInYear);
-                        //_migHelper.StoreTestResults(_batchId.ToString(CultureInfo.InvariantCulture), _testName,
-                        //                           migratedUser, _testStartTime, _testEndTime, _loginStatus);
-                    }
-                }
-
-            }
-        }
-
-        [Test]
-        public void MigratedApiLnJournery()
-        {
-            _migratedUser = new MigratedUser();
-            _migratedUser = _migHelper.GetMigratedAccountLogin();
-            var customer = new Customer(Guid.Parse
-                                            (Drive.Api.Queries.Post
-                                                 (new GetAccountQuery { Login = _migratedUser.Login, Password = _migratedUser.Password }).
-                                                 //{ Login = "qa.wonga.com+BUILD-WIN21-7af66e12-7664-4bec-8a51-f5ffb17f41b2@gmail.com", Password = "Passw0rd"}).//
-                                                 Values["AccountId"].Single()));
-            
-            //var promiseDate = new Date(DateTime.Now.AddDays(10));
-            //var loanAmmount = new decimal(1000);
-
-            //ApplicationBuilder.New(customer,promiseDate,loanAmmount).Build();
-            ApplicationBuilder.New(customer).Build();
         }
 
         [Test]
