e8c99448ff1fa1a08fe784e6e69c9004862e1884
Michael.Nowicki <michael.nowicki@wonga.com>
2012-08-09 15:27:25 +0100

    paylater loginpage fix

--- src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/PayLater/PayLaterLoginTest.cs
+++ src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/PayLater/PayLaterLoginTest.cs
@@ -7,21 +7,21 @@ namespace Wonga.QA.UiTests.Web.PayLater
 {
     public class PayLaterLoginTest : UiTest
     {
-        [Test, AUT(AUT.Uk), JIRA("QA-77, 79, 80, 81"), Pending("Fails trying to open http://dev.paylater.com/")]
+        [Test, AUT(AUT.Uk), JIRA("QA-77, 79, 80, 81"), Explicit("works on local dev only")]
         public void InspectElementsHomePage()
         {
             var payLaterStartPage = Client.PayLaterStart();
             var payLaterHome = payLaterStartPage.InspectElemrnts();
 
             Assert.AreEqual("Representative APR 28.7%", payLaterHome["Apr"]);
-            Assert.AreEqual("Sign Up", payLaterHome["SingUp"]);
+            Assert.AreEqual("Sign up", payLaterHome["SingUp"]);
             Assert.AreEqual("£11.68", payLaterHome["Fee"]);
             Assert.AreEqual("Forgotten your password", payLaterHome["ForgottenPassword"]);
             Assert.AreEqual("£57.43", payLaterHome["PaymentAmount"]);
         }
 
 
-        [Test, AUT(AUT.Uk), JIRA("QA-78"), Pending("Fails trying to open http://dev.paylater.com/")]
+        [Test, AUT(AUT.Uk), JIRA("QA-78"), Explicit("works on local dev only")]
         public void LoginPagePayments()
         {
             const string user = "qa.wongatest+2012425163553@gmail.com";
@@ -42,7 +42,7 @@ namespace Wonga.QA.UiTests.Web.PayLater
             Thread.Sleep(2000);
         }
 
-        [Test, AUT(AUT.Uk), JIRA("QA-82, 75, 84, 86, 89"), Pending("Fails trying to open http://dev.paylater.com/")]
+        [Test, AUT(AUT.Uk), JIRA("QA-82, 75, 84, 86, 89"), Explicit("works on local dev only")]
         public void InspectElementsSubmitionPage()
         {
             const string footerIn = @"Paylater is a trading name of Wonga.com Ltd Copyright © 2008 - 2012 Wonga.com. Registered in England and Wales. 
@@ -69,7 +69,7 @@ namespace Wonga.QA.UiTests.Web.PayLater
             Assert.AreEqual("You'll pay £11.68 today and £57.43 on 27th May and £57.43 on 27th June and £57.43 on 27th July ?", elementsSubmition["RepaymentDetails"]);
         }
 
-        [Test, AUT(AUT.Uk), JIRA("QA-92"), Pending("Fails trying to open http://dev.paylater.com/")]
+        [Test, AUT(AUT.Uk), JIRA("QA-92"), Explicit("works on local dev only")]
         public void InspectElementsThanksPage()
         {
             const string user = "qa.wongatest+2012425163553@gmail.com";
