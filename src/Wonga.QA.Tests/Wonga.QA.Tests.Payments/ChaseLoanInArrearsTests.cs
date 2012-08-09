4d2dde0fa8106b3fc35d131bfccb33adc362d7b6
anil krishnamaneni <anil.krishnamanei@wonga.com>
2012-08-09 18:29:52 +0100

    Updating test UkOps-149

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/ChaseLoanInArrearsTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/ChaseLoanInArrearsTests.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using MbUnit.Framework;
 using Wonga.QA.Framework;
 using Wonga.QA.Framework.Core;
@@ -15,21 +12,30 @@ namespace Wonga.QA.Tests.Payments
     public class ChaseLoanInArrearsTests
     {
         private static readonly dynamic ChaseLoanInArrearsSagaEntities = Drive.Data.OpsSagas.Db.ChaseLoanInArrearsSagaEntity;
+        private Customer _customer;
+        private Application _application;
+        private Application _secondApplication;
+        private dynamic _sagaEntityFiveAm;
+        private dynamic _sagaSecondEntityFail;
+        private dynamic _sagaEntityFail;
+        private static dynamic _primaryPaymentCardId;
+        private decimal _amountDue;
+        private int _appInternalId;
+        private int _secondAppInternalId;
 
-        [Test, JIRA("UKOPS-419"),Owner(Owner.JonHurd)]
+        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
         public void PaymentsShouldCreateNewTransactionWhenFiveAmCollectionSucceeds()
         {
             // Arrange
-            var customer = GetCustomer();            
-            var application = GetApplicationInArrears( customer);
-            SetCardExpiryDate(customer.GetPaymentCard(), DateTime.Now.AddYears(1));            
+            var customer = GetCustomer();
+            var application = GetApplicationInArrears(customer);
+            SetCardExpiryDate(customer.GetPaymentCard(), DateTime.Now.AddYears(1));
             var appInternalId = ApplicationOperations.GetAppInternalId(application);
-            
-            var sagaEntity = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
 
-            // Act
+            _sagaEntityFiveAm = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
+
             // Call timeout with a state of 0 representing the first ping at 5 am ping
-            Drive.Msmq.Payments.Send(message: new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = sagaEntity.Id, State = 0 });
+            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = _sagaEntityFiveAm.Id, State = 0 });
 
             // Assert there should be a credit transaction in the table representing the payment
             var amountDue = application.GetDueDateBalance();
@@ -40,67 +46,92 @@ namespace Wonga.QA.Tests.Payments
                                                                                 Reference: "Automatic Ping (Card)").Count() == 1);
         }
 
-        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
+        [Test, JIRA("UKOPS-419"), DependsOn("PaymentsShouldCreateNewTransactionWhenFiveAmCollectionSucceeds"), Owner(Owner.JonHurd)]
         public void PaymentsShouldMarkSagaAsCompleteWhenFiveAmCollectionSucceeds()
         {
-            // Arrange
-            var customer = GetCustomer();
-            var application = GetApplicationInArrears(customer);
-            SetCardExpiryDate(customer.GetPaymentCard(), DateTime.Now.AddYears(1));
-            var appInternalId = ApplicationOperations.GetAppInternalId(application);
-            var sagaEntity = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
-
-            // Act
-            // Call timeout with a state of 0 representing the first ping at 5 am ping
-            Drive.Msmq.Payments.Send( new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = sagaEntity.Id, State = 0 });
-
             // Assert that saga is complete
-            Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindById(sagaEntity.Id) == null);
-
-
+            Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindById(_sagaEntityFiveAm.Id) == null);
         }
 
         [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
-        public void PaymentsShouldCreateNewTransactionWhenEightAmCollectionSucceeds()
+        public void PaymentsShouldAddRecordToPaymentCardRepaymentRequestWhenFirstPingFails()
         {
             // Arrange
-            var customer = GetCustomer();
-            var application = GetApplicationInArrears(customer);
-            SetCardExpiryDate(customer.GetPaymentCard(), DateTime.Now.AddYears(1));
-            var appInternalId = ApplicationOperations.GetAppInternalId(application);
-            var sagaEntity = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
+            _customer = GetCustomer();
+            _application = GetApplicationInArrears(_customer);
+            _primaryPaymentCardId =
+                Drive.Data.Payments.Db.AccountPreferences.FindAllByAccountId(_application.AccountId).SingleOrDefault().
+                    PrimaryPaymentCardId;
+            _appInternalId = ApplicationOperations.GetAppInternalId(_application);
 
-            // Act
-            // Call timeout with a state of 1 representing the second ping at 8 am ping
-            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = sagaEntity.Id, State = 1 });
+            _customer.AddBadCard();
+            _sagaEntityFail = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == _appInternalId).Single());
+
+            // Call timeout with a state of 0 representing the first ping at 5 am ping
+            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = _sagaEntityFail.Id, State = 0 });
 
             // Assert there should be a credit transaction in the table representing the payment
-            var amountDue = application.GetDueDateBalance();
-            Do.With.Timeout(2).Until<bool>(() => Drive.Data.Payments.Db.Transactions.FindAllBy(ApplicationId: appInternalId,
+           _amountDue = _application.GetDueDateBalance();
+            Do.With.Timeout(2).Until<bool>(() => Drive.Data.Payments.Db.PaymentCardRepaymentRequests.FindAllBy(ApplicationId: _appInternalId,
+                                                                                Amount: _amountDue,
+                                                                                StatusDescription: "Request Declined"
+                                                                                 ).Count() == 1);
+        }
+
+        [Test, JIRA("UKOPS-419"), DependsOn("PaymentsShouldAddRecordToPaymentCardRepaymentRequestWhenFirstPingFails"), Owner(Owner.JonHurd)]
+        public void PaymentsShouldCreateNewTransactionWhenEightAmCollectionSucceedsAfterFivePingFails()
+        {
+            UpdatePrimaryCard(_application.AccountId);
+            SetCardExpiryDate(_customer.GetPaymentCard(), DateTime.Now.AddYears(1));
+            // Call timeout with a state of 1 representing the second ping at 8 am ping
+            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = _sagaEntityFail.Id, State = 1 });
+
+            // Assert there should be a credit transaction in the table representing the payment
+            
+            Do.With.Timeout(2).Until<bool>(() => Drive.Data.Payments.Db.Transactions.FindAllBy(ApplicationId: _appInternalId,
                                                                                 Type: PaymentTransactionEnum.CardPayment.ToString(),
-                                                                                Amount: amountDue * -1,
+                                                                                Amount: _amountDue * -1,
                                                                                 Scope: (int)PaymentTransactionScopeEnum.Credit,
                                                                                 Reference: "Automatic Ping (Card)").Count() == 1);
         }
 
-        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
-        public void PaymentsShouldMarkSagaAsCompleteWhenEightAmCollectionSucceeds()
+        [Test, JIRA("UKOPS-419"), DependsOn("PaymentsShouldCreateNewTransactionWhenEightAmCollectionSucceedsAfterFivePingFails"), Owner(Owner.JonHurd)]
+        public void PaymentsShouldMarkSagaAsCompleteWhenEightAmCollectionSucceedsAfterFirstPingFail()
         {
-            // Arrange
-            var customer = GetCustomer();
-            var application = GetApplicationInArrears(customer);
-            SetCardExpiryDate(customer.GetPaymentCard(), DateTime.Now.AddYears(1));
-            var appInternalId = ApplicationOperations.GetAppInternalId(application);
-            var sagaEntity = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
-
-            // Act
-            // Call timeout with a state of 1 representing the second ping at 8 am ping
-            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = sagaEntity.Id, State = 1 });
-
             // Assert that saga is complete
-            Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindById(sagaEntity.Id) == null);
+            Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindById(_sagaEntityFail.Id) == null);
+        }
 
+        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
+        public void PaymentsFirstPingFailsToMakeSecondPingAlsoFail()
+        {
+            var customer = GetCustomer();
+            _secondApplication = GetApplicationInArrears(customer); // get application and force it into arrears, this will trigger the ChaseLoanInArrearsSaga
+            _secondAppInternalId = ApplicationOperations.GetAppInternalId(_secondApplication);
+            customer.AddBadCard(); // add a card with an account number that the mock will deliberately fail, the previous card is expired
 
+            _sagaSecondEntityFail = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == _secondAppInternalId).Single());
+            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = _sagaSecondEntityFail.Id, State = 0 });
+        }
+
+        [Test, JIRA("UKOPS-419"), DependsOn("PaymentsFirstPingFailsToMakeSecondPingAlsoFail"), Owner(Owner.JonHurd)]
+        public void PaymentsRepaymentRequestSecondPingFailsAfterFirstPingFailed()
+        {
+            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = _sagaSecondEntityFail.Id, State = 1 });
+
+            // Assert there is a request declined record in PaymentCardRepaymentRequests
+            var amountDue = _secondApplication.GetDueDateBalance();
+            Do.With.Timeout(2).Until<bool>(() => Drive.Data.Payments.Db.PaymentCardRepaymentRequests.FindAllBy(ApplicationId: _secondAppInternalId,
+                                                                                Amount: amountDue,
+                                                                                StatusDescription: "Request Declined"
+                                                                                 ).Count() == 2);
+        }
+
+        [Test, JIRA("UKOPS-419"), DependsOn("PaymentsRepaymentRequestSecondPingFailsAfterFirstPingFailed"), Owner(Owner.JonHurd)]
+        public void PaymentsShouldMarkSagaAsCompleteWhenFirstAndSecondPingFails()
+        {
+            // Assert that saga is complete
+            Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindById(_sagaSecondEntityFail.Id) == null);
         }
 
         [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
@@ -113,77 +144,37 @@ namespace Wonga.QA.Tests.Payments
             var appInternalId = ApplicationOperations.GetAppInternalId(application);
             var sagaEntity = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
 
-            // Act
             // Call timeout with a state of 2. The only valid values are 0 and 1 so saga should complete
             Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = sagaEntity.Id, State = 2 });
 
             // Assert that saga is complete
             Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindById(sagaEntity.Id) == null);
-
-
         }
 
-        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
-        public void PaymentsShouldAddRecordToPaymentCardRepaymentRequestWhenFirstPingFails()
+        #region pending test
+        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd),Pending("Ticket Not Implemented")]
+        public void PaymentsShouldSuppressFiveAmPingIfSuppressionsAreActive()
         {
-            // Arrange
-            dynamic ChaseLoanInArrearsSagaEntities = Drive.Data.OpsSagas.Db.ChaseLoanInArrearsSagaEntity;
-            var customer = GetCustomer();
-            var application = GetApplicationInArrears(customer); // get application and force it into arrears, this will trigger the ChaseLoanInArrearsSaga
-            var appInternalId = ApplicationOperations.GetAppInternalId(application);
-            customer.AddBadCard(); // add a card with an account number that the mock will deliberately fail, the previous card is expired
-
-            var sagaEntity = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
-
-            // Act
-            // Call timeout with a state of 0 representing the first ping at 5 am ping
-            Drive.Msmq.Payments.Send( new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = sagaEntity.Id, State = 0 });
-
-            // Assert there is a request declined record in PaymentCardRepaymentRequests
-            var amountDue = application.GetDueDateBalance();
-            Do.With.Timeout(2).Until<bool>(() => Drive.Data.Payments.Db.PaymentCardRepaymentRequests.FindAllBy(ApplicationId: appInternalId,
-                                                                                Amount: amountDue ,                                                                                
-                                                                                StatusDescription:"Request Declined"
-                                                                                 ).Count() == 1);
+            // To Do .. not implemented yet
         }
 
-        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
-        public void PaymentsShouldAddRecordToPaymentCardRepaymentRequestWhenSecondPingFails()
+        [Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd), Pending("Ticket Not Implemented")]
+        public void PaymentsShouldSuppressEightAmPingIfSuppressionsAreActive()
         {
-            // Arrange
-            var customer = GetCustomer();
-            var application = GetApplicationInArrears(customer); // get application and force it into arrears, this will trigger the ChaseLoanInArrearsSaga
-            var appInternalId = ApplicationOperations.GetAppInternalId(application);
-            customer.AddBadCard(); // get a card with an account number that the mock will deliberately fail, the previous card is expired
-            var sagaEntity = Do.With.Timeout(2).Until(() => ChaseLoanInArrearsSagaEntities.FindAll(ChaseLoanInArrearsSagaEntities.ApplicationId == appInternalId).Single());
-
-            // Act
-            // Call timeout with a state of 0 representing the first ping at 5 am ping
-            Drive.Msmq.Payments.Send(new Framework.Msmq.TimeoutMessage { Expires = DateTime.UtcNow, SagaId = sagaEntity.Id, State = 1 });
-
-            // Assert there is a request declined record in PaymentCardRepaymentRequests
-            var amountDue = application.GetDueDateBalance();
-            Do.With.Timeout(2).Until<bool>(() => Drive.Data.Payments.Db.PaymentCardRepaymentRequests.FindAllBy(ApplicationId: appInternalId,
-                                                                                Amount: amountDue,
-                                                                                StatusDescription: "Request Declined"
-                                                                                 ).Count() == 1);
+            // To Do .. not implemented yet
         }
+         #endregion 
 
-        //[Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
-        //public void PaymentsShouldSuppressFiveAmPingIfSuppressionsAreActive()
-        //{
-        //    // To Do .. not implemented yet
-        //}
-
-        //[Test, JIRA("UKOPS-419"), Owner(Owner.JonHurd)]
-        //public void PaymentsShouldSuppressEightAmPingIfSuppressionsAreActive()
-        //{
-        //    // To Do .. not implemented yet
-        //}
-
+        #region Helpers
         private static void SetCardExpiryDate(Guid card, DateTime expiryDate)
         {
             Drive.Data.Payments.Db.PaymentCardsBase.UpdateByExternalId(ExternalId: card, ExpiryDate: expiryDate, DeactivatedOn : null);
+        }
+
+        private static void UpdatePrimaryCard(Guid id)
+        {
+            Drive.Data.Payments.Db.AccountPreferences.UpdateByAccountId(AccountId: id,
+                                                                        PrimaryPaymentCardId: _primaryPaymentCardId);
         }
 
         private static Application GetApplicationInArrears( Customer customer)
@@ -197,14 +188,13 @@ namespace Wonga.QA.Tests.Payments
 
             return application;
         }
-
-
+        
         private static Customer GetCustomer()
         {
             var customer = CustomerBuilder.New().Build();
             return customer;
         }
-
+        #endregion
 
     }
 
@@ -215,7 +205,6 @@ namespace Wonga.QA.Tests.Payments
             const string cardType = "visa";
             const string cardNumber = "9999888877776666";
             customer.AddPaymentCard(cardType, cardNumber, "777", DateTime.Now.AddMonths(3), true);
-
             return customer;
         }
     }
