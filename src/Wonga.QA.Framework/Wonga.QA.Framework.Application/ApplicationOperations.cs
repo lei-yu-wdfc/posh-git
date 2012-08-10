8224b51aeb7a6a651b1b676ca13d4ae182127f45
Adrian Murphy <adrian.murphy@wonga.com>
2012-08-10 16:11:52 +0100

    More Skeleton code

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Application/ApplicationOperations.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Application/ApplicationOperations.cs
@@ -1,10 +1,12 @@
-﻿using Wonga.QA.Framework.Application.Consumer;
+﻿using Wonga.QA.Framework.Application.Business;
+using Wonga.QA.Framework.Application.Consumer;
 using Wonga.QA.Framework.Application.PayLater;
 
 namespace Wonga.QA.Framework.Application
 {
 	public static class ApplicationOperations
 	{
+		public static BusinessApplicationOperations Business = new BusinessApplicationOperations();
 		public static ConsumerApplicationOperations Consumer = new ConsumerApplicationOperations();
 		public static PayLaterApplicationOperations PayLater = new PayLaterApplicationOperations();
 	}
--- src/Wonga.QA.Framework/Wonga.QA.Framework.Application/ApplicationQueries.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Application/ApplicationQueries.cs
@@ -1,6 +1,13 @@
-﻿namespace Wonga.QA.Framework.Application
+﻿using Wonga.QA.Framework.Application.Business;
+using Wonga.QA.Framework.Application.Consumer;
+using Wonga.QA.Framework.Application.PayLater;
+
+namespace Wonga.QA.Framework.Application
 {
 	public static class ApplicationQueries
 	{
+		public static BusinessApplicationQueries Business = new BusinessApplicationQueries();
+		public static ConsumerApplicationQueries Consumer = new ConsumerApplicationQueries();
+		public static PayLaterApplicationQueries PayLater = new PayLaterApplicationQueries();
 	}
 }
