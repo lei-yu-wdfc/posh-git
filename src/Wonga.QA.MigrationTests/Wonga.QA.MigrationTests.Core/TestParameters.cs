c419ffe90279d87b676a4e3adcf0ff10646aad44
muhammad83 <muhammad.qureshi@wonga.com>
2012-08-13 10:48:43 +0100

    Migration New UI test covering Ln Journey and Paying back

---
+++ src/Wonga.QA.MigrationTests/Wonga.QA.MigrationTests.Core/TestParameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wonga.QA.MigrationTests.Core
+{
+    public class TestParameters
+    {
+        public string Exception { get; set; }
+        public string ParametersUsed { get; set; }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+            str.Append("Exception: ");
+            str.Append(Exception);
+            str.Append("Parameters Used: ");
+            str.Append(ParametersUsed);
+            return str.ToString();
+        }
+    }
+}
