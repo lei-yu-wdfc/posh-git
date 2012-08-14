c1da9385314efea6bab9d2d0436d61ee7248e6be
lukasz-wrobel-wonga <lukasz.wrobel@wonga.com>
2012-08-14 12:25:27 +0100

    PAYLATER-842 added staging to the enum

--- src/Wonga.QA.Framework/Wonga.QA.Framework.Core/Config.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Core/Config.cs
@@ -14,7 +14,7 @@ using Microsoft.Win32;
 namespace Wonga.QA.Framework.Core
 {
     public enum AUT { Uk, Za, Ca, Wb, Pl }
-    public enum SUT { Dev, WIP, UAT, RC, WIPRelease, RCRelease, Live, WIPDI, Perf }
+    public enum SUT { Dev, WIP, UAT, RC, WIPRelease, RCRelease, Live, WIPDI, Perf, Staging }
 
     public static class Config
     {
