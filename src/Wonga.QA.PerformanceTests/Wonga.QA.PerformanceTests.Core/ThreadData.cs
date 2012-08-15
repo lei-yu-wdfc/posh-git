4bf87ae47df2b189381ac2827b7d69ac175ad2dd
Francis xavier Chelladurai <francis.chelladurai@wonga.com>
2012-08-15 11:47:13 +0100

    Recreated the core project to have correct configurations/settings.

---
+++ src/Wonga.QA.PerformanceTests/Wonga.QA.PerformanceTests.Core/ThreadData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Wonga.QA.PerformanceTests.Core
+{
+    public class ThreadData
+    {
+        public Thread Thread;
+        public String Name;
+        public DateTime StartTime;
+        public DateTime EndTime;
+        public String Status;
+
+        public ThreadData(Thread thread, String name, DateTime startTime, String status)
+        {
+            Thread = thread;
+            Name = name;
+            StartTime = startTime;
+            Status = status;
+        }
+    }
+}
