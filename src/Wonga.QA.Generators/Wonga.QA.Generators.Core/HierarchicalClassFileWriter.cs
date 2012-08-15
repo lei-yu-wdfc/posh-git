e462d1b3f21af7bed4e59306e8c274145ef6ea8a
Adrian Murphy <adrian.murphy@wonga.com>
2012-08-15 17:02:29 +0100

    Updated Api generators

---
+++ src/Wonga.QA.Generators/Wonga.QA.Generators.Core/HierarchicalClassFileWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Wonga.QA.Generators.Core
+{
+	public static class HierarchicalClassFileWriter
+	{
+		private static DirectoryInfo _parentDirectory;
+		private static DirectoryInfo _commandsDirectory;
+		private static DirectoryInfo _queriesDirectory;
+
+		private static readonly String[] Regions = new [] {"Ca", "Pl", "Uk", "Za"};
+
+		public static void WriteClassFilesToDisk(DirectoryInfo parentDirectory, List<MessageClassDefinition> classes )
+		{
+			WriteFiles(_commandsDirectory, classes);
+		}
+
+		private static void CreateDirectoryIfDoesntExist(DirectoryInfo directory)
+		{
+			if( !Directory.Exists(directory.FullName))
+			{
+				Directory.CreateDirectory(directory.FullName);
+			}
+		}
+
+		private static void WriteFiles(DirectoryInfo parentDirectory, List<MessageClassDefinition> classes)
+		{
+			foreach (var c in classes)
+			{
+				WriteFile(parentDirectory, c);
+			}
+		}
+
+		private static void WriteFile(DirectoryInfo parentDirectory, MessageClassDefinition messageClassDefinition)
+		{
+			var fileFullName = GetDirectory(parentDirectory, messageClassDefinition);
+			var file = new FileInfo(fileFullName);
+
+			using (StreamWriter writer = file.CreateText())
+				writer.Write(messageClassDefinition.ClassBody);
+		}
+
+		private static String GetDirectory(DirectoryInfo parentDirectory, MessageClassDefinition messageClassDefinition)
+		{
+			return Path.Combine(parentDirectory.FullName, messageClassDefinition.Component, messageClassDefinition.MessageType, messageClassDefinition.Region, messageClassDefinition.FileName);
+		}
+	}
+}
---
+++ src/Wonga.QA.Generators/Wonga.QA.Generators.Core/MessageClassDefinition.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Wonga.QA.Generators.Core
+{
+	public class MessageClassDefinition
+	{
+		public String Namespace { get; private set; }
+		public String ClassName { get; private set; }
+		public String ClassBody { get; private set; }
+		public String FileName { get; private set; }
+		public String Region { get; private set; }
+		public String MessageType { get; private set; }
+		public String Component { get; private set; }
+
+		private static readonly String[] Regions = new[] { "Ca", "Pl", "Uk", "Za" };
+		private static readonly String[] MessageTypes = new[] {"Command", "Query"};
+
+		public MessageClassDefinition(String @namespace, String className, String classBody)
+		{
+			Namespace = @namespace;
+			ClassName = className;
+			ClassBody = classBody;
+			FileName = String.Format("{0}.cs", ClassName);
+			MessageType = GetMessageType();
+			Component = GetComponent();
+		}
+
+		private String GetRegion()
+		{
+			foreach (var region in Regions)
+			{
+				if (MessageTypes.Any(type => ClassName.Contains(region + type)))
+				{
+					return region;
+				}
+			}
+			return String.Empty;
+		}
+
+		private String GetMessageType()
+		{
+			foreach (var messageType in MessageTypes)
+			{
+				if (ClassName.Contains(messageType))
+				{
+					return messageType;
+				}
+			}
+
+			throw new Exception("Message must be of a defined type");
+		}
+
+		private String GetComponent()
+		{
+			return Namespace.Split('.')[1];
+		}
+	}
+}
