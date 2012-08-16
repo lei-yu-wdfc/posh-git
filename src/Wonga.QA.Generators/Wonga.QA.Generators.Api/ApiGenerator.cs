2f5d63e4a72ffe054a7ad9d84ef606b8e07cb89b
Adrian Murphy <adrian.murphy@wonga.com>
2012-08-16 10:02:11 +0100

    Refactor to Topup builder

--- src/Wonga.QA.Generators/Wonga.QA.Generators.Api/ApiGenerator.cs
+++ src/Wonga.QA.Generators/Wonga.QA.Generators.Api/ApiGenerator.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Wonga.QA.Generators.Core;
 
 namespace Wonga.QA.Generators.Api
