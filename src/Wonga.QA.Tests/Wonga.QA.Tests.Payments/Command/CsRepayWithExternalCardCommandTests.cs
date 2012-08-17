ec895612fe2cffda6959776c55f58b6aac976b96
Shane McHugh <shane.mchugh@wonga.com>
2012-08-17 15:21:11 +0100

    adding est for the CsRepayWithExternalCardCommand & CsRepayWithPaymentCardCommand

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Command/CsRepayWithExternalCardCommandTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/Command/CsRepayWithExternalCardCommandTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using MbUnit.Framework;
 using Wonga.QA.Framework;
+using Wonga.QA.Framework.Api.Exceptions;
 using Wonga.QA.Framework.Core;
 using Wonga.QA.Framework.Cs.Requests.Payments.Csapi.Commands;
 using Wonga.QA.Framework.Old;
 using Wonga.QA.Tests.Core;
+using Wonga.QA.Tests.Salesforce;
 
 namespace Wonga.QA.Tests.Payments.Command
 {
@@ -33,6 +35,7 @@ namespace Wonga.QA.Tests.Payments.Command
         {
             protected decimal _startingBalance;
             protected decimal _paymentAmount;
+            protected DateTime _expiryDate = new DateTime(DateTime.Now.Year + 1, 1, 31);
             protected dynamic _paymentCardRepaymentRequests = Drive.Data.Payments.Db.PaymentCardRepaymentRequests;
             protected Guid _paymentId = Guid.NewGuid();
 
@@ -42,30 +45,31 @@ namespace Wonga.QA.Tests.Payments.Command
                 base.Setup();
 
                 _startingBalance = _application.GetBalance();
-                //Issue a payment command.
-                Drive.Cs.Commands.Post( GetExternalCard() );
             }
 
             protected virtual CsRepayWithExternalCardCommand GetExternalCard()
             {
-                return new CsRepayWithExternalCardCommand { AccountId = _application.AccountId,
-                                                            AddressLine1 = "line 1",
-                                                            AddressLine2 = "line 2",
-                                                            Amount = _paymentAmount,
-                                                            CardNumber = "5411111111111111", 
-                                                            CardType = "visaDebit",
-                                                            Country = "UK",
-                                                            County = "county",
-                                                            SalesforceUser = "csUser",
-                                                            Currency = "GBP",
-                                                            CV2 = "121",
-                                                            ExpiryDate = new DateTime(DateTime.Today.Year + 1, 1, 31),
-                                                            HolderName = "holder name", 
-                                                            PostCode = "12345",
-                                                            Town = "town", 
-                                                            PaymentId = _paymentId };
+                return new CsRepayWithExternalCardCommand
+                {
+                    AccountId = _application.AccountId,
+                    AddressLine1 = "line 1",
+                    AddressLine2 = "line 2",
+                    Amount = _paymentAmount,
+                    CardNumber = "5411111111111111",
+                    CardType = "visaDebit",
+                    Country = "UK",
+                    County = "county",
+                    SalesforceUser = "csUser",
+                    Currency = "GBP",
+                    CV2 = "121",
+                    ExpiryDate = _expiryDate,
+                    HolderName = "holder name",
+                    PostCode = "12345",
+                    Town = "town",
+                    PaymentId = _paymentId
+                };
             }
-            
+
             [Parallelizable(TestScope.Self)]
             public class GivenAPaymentHasBeenRequestedWithAValidCard : GivenACustomerWithAnApprovedLoan
             {
@@ -74,6 +78,8 @@ namespace Wonga.QA.Tests.Payments.Command
                 {
                     _paymentAmount = 50;
                     base.Setup();
+                    //Issue a payment command.
+                    Drive.Cs.Commands.Post(GetExternalCard());
                 }
 
                 [Test, AUT(AUT.Uk)]
@@ -87,6 +93,45 @@ namespace Wonga.QA.Tests.Payments.Command
 
                     Do.With.Timeout(5).Interval(20).Until(() => _application.GetBalance() == _startingBalance - _paymentAmount);
                 }
+
+                [Test, AUT(AUT.Uk), JIRA("UKOPS-862"), Pending("External Card is added as a card when CsRepayWithExternalCard is called, this hsould not be the case")]
+                public void TheCardIsNotAddedToTheAccount()
+                {
+                    dynamic paymentCardsBase = Drive.Data.Payments.Db.PaymentCardsBase;
+                    dynamic paymentCardId = paymentCardsBase.FindAll(paymentCardsBase.ExternalId == _cardId).Single().PaymentCardId;
+                    dynamic personalPaymentCards = Drive.Data.Payments.Db.PersonalPaymentCards;
+                    dynamic row =
+                        personalPaymentCards.FindAll(personalPaymentCards.AccountId == _application.AccountId &&
+                                                     personalPaymentCards.PaymentCardId == paymentCardId).FirstOrDefault();
+                    Assert.IsNull(row);
+                }
+            }
+
+            [Parallelizable(TestScope.Self)]
+            public class GivenAPaymentHasBeenRequestedWithAValidCardAndLoanIsInArrears : GivenACustomerWithAnApprovedLoan
+            {
+                [SetUp]
+                public override void Setup()
+                {
+                    _paymentAmount = 50;
+                    base.Setup();
+                    _application.PutIntoArrears(5);
+                    //Issue a payment command.
+                    Drive.Cs.Commands.Post(GetExternalCard());
+                }
+
+                [Test, AUT(AUT.Uk), JIRA("UKOPS-109"), Owner(Owner.ShaneMcHugh)]
+                public void TheLoanAmountHasBeenReducedByPaymentAmountPaymentRequestAdded()
+                {
+                    Do.With.Timeout(2).Interval(20).Until(() => null != _paymentCardRepaymentRequests.FindAll(_paymentCardRepaymentRequests.Applications.ExternalId == _application.Id &&
+                                                                                                              _paymentCardRepaymentRequests.ExternalId == _paymentId &&
+                                                                                                              _paymentCardRepaymentRequests.Amount == _paymentAmount &&
+                                                                                                              _paymentCardRepaymentRequests.FailedOn != null &&
+                                                                                                              _paymentCardRepaymentRequests.SuccessOn == null).FirstOrDefault());
+                    //This is hardcoded in UK so have to hardcode here.
+                    const int defaultCharge = 20;
+                    Do.With.Timeout(5).Interval(20).Until(() => _application.GetBalance() == (_startingBalance + defaultCharge) - _paymentAmount);
+                }
             }
 
             [Parallelizable(TestScope.Self)]
@@ -97,16 +142,18 @@ namespace Wonga.QA.Tests.Payments.Command
                 {
                     _paymentAmount = 1000;
                     base.Setup();
+                    //Issue a payment command.
+                    Drive.Cs.Commands.Post(GetExternalCard());
                 }
 
                 [Test, AUT(AUT.Uk), JIRA("UKOPS-109"), Owner(Owner.ShaneMcHugh), Pending("Currently payment higher than the balance is allowed and should not be UKOPS-846")]
                 public void TheLoanAmountRemainsTheSameFailedPaymentRequestAdded()
                 {
-                   Do.With.Timeout(2).Interval(20).Until(() => null != _paymentCardRepaymentRequests.FindAll(_paymentCardRepaymentRequests.Applications.ExternalId == _application.Id &&
-                                                                            _paymentCardRepaymentRequests.ExternalId == _paymentId &&
-                                                                            _paymentCardRepaymentRequests.Amount == _paymentAmount &&
-                                                                            _paymentCardRepaymentRequests.FailedOn != null &&
-                                                                            _paymentCardRepaymentRequests.SuccessOn == null).FirstOrDefault());
+                    Do.With.Timeout(2).Interval(20).Until(() => null != _paymentCardRepaymentRequests.FindAll(_paymentCardRepaymentRequests.Applications.ExternalId == _application.Id &&
+                                                                             _paymentCardRepaymentRequests.ExternalId == _paymentId &&
+                                                                             _paymentCardRepaymentRequests.Amount == _paymentAmount &&
+                                                                             _paymentCardRepaymentRequests.FailedOn == null &&
+                                                                             _paymentCardRepaymentRequests.SuccessOn != null).FirstOrDefault());
 
                     Do.With.Timeout(5).Interval(20).Until(() => _application.GetBalance() == _startingBalance);
                 }
@@ -120,6 +167,8 @@ namespace Wonga.QA.Tests.Payments.Command
                 {
                     _paymentAmount = 13m; //Make it fail.
                     base.Setup();
+                    //Issue a payment command.
+                    Drive.Cs.Commands.Post(GetExternalCard());
                 }
 
                 [Test, AUT(AUT.Uk)]
@@ -129,9 +178,67 @@ namespace Wonga.QA.Tests.Payments.Command
                                                                                                               _paymentCardRepaymentRequests.ExternalId == _paymentId &&
                                                                                                               _paymentCardRepaymentRequests.Amount == _paymentAmount &&
                                                                                                               _paymentCardRepaymentRequests.FailedOn != null &&
-                                                                                                              _paymentCardRepaymentRequests.SuccessOn == null).FirstOrDefault() );
+                                                                                                              _paymentCardRepaymentRequests.SuccessOn == null).FirstOrDefault());
 
                     Assert.IsTrue(_application.GetBalance() == _startingBalance);
+                }
+            }
+
+            [Parallelizable(TestScope.Self)]
+            public class GivenAPaymentHasBeenRequestedWithAnExpiredCard : GivenACustomerWithAnApprovedLoan
+            {
+                private ValidatorException _exception;
+
+                [SetUp]
+                public override void Setup()
+                {
+                    _paymentAmount = 50m;
+                    _expiryDate = new DateTime(DateTime.Now.Year - 1, 1, 31);
+                    base.Setup();
+                    //Issue a payment command.
+                    _exception = Assert.Throws<ValidatorException>(() => Drive.Cs.Commands.Post(GetExternalCard()));
+                }
+
+                [Test, AUT(AUT.Uk)]
+                public void TheLoanAmountRemainsTheSameFailedPaymentRequestAdded()
+                {
+                    Assert.Contains(_exception.Errors, "Payments_PaymentCard_Expired");
+                    Do.With.Timeout(2).Interval(20).Until(() => null != _paymentCardRepaymentRequests.FindAll(_paymentCardRepaymentRequests.Applications.ExternalId == _application.Id &&
+                                                                                                              _paymentCardRepaymentRequests.ExternalId == _paymentId &&
+                                                                                                              _paymentCardRepaymentRequests.Amount == _paymentAmount &&
+                                                                                                              _paymentCardRepaymentRequests.FailedOn != null &&
+                                                                                                              _paymentCardRepaymentRequests.SuccessOn == null).FirstOrDefault());
+
+                    Assert.IsTrue(_application.GetBalance() == _startingBalance);
+                }
+            }
+
+            [Parallelizable(TestScope.Self)]
+            public class GivenAPaymentInFullHasBeenRequestedWithAValidCard : GivenACustomerWithAnApprovedLoan
+            {
+                [SetUp]
+                public override void Setup()
+                {
+                    base.Setup();
+                    _paymentAmount = _application.GetBalance();
+                    //Issue a payment command.
+                    Drive.Cs.Commands.Post(GetExternalCard());
+                }
+
+                [Test, AUT(AUT.Uk), JIRA("UKOPS-109"), Owner(Owner.ShaneMcHugh)]
+                public void TheLoanAmountHasBeenReducedByPaymentAmountPaymentRequestAdded()
+                {
+                    Do.With.Timeout(2).Interval(20).Until(() => null != _paymentCardRepaymentRequests.FindAll(_paymentCardRepaymentRequests.Applications.ExternalId == _application.Id &&
+                                                                                                              _paymentCardRepaymentRequests.ExternalId == _paymentId &&
+                                                                                                              _paymentCardRepaymentRequests.Amount == _paymentAmount &&
+                                                                                                              _paymentCardRepaymentRequests.FailedOn == null &&
+                                                                                                              _paymentCardRepaymentRequests.SuccessOn != null).FirstOrDefault());
+
+                    dynamic applicationStatusHistory = Drive.Data.BiCustomerManagement.Db.ApplicationStatusHistory;
+                    Do.With.Timeout(2).Interval(20).Until(() => null != applicationStatusHistory.FindAll(applicationStatusHistory.ApplicationId == _application.Id &&
+                                                                                                         applicationStatusHistory.CurrentStatus == Framework.ThirdParties.Salesforce.ApplicationStatus.PaidInFull).FirstOrDefault());
+
+                    Do.With.Timeout(5).Interval(20).Until(() => _application.GetBalance() == _startingBalance - _paymentAmount);
                 }
             }
         }
