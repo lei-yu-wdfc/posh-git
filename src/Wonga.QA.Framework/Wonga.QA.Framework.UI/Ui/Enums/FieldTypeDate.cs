274b3ab86dda80497821629d08240f0e725c12c4
gotik88 <dmytro.romanii@gmail.com>
2012-08-17 10:28:33 +0300

    Date enum fix

--- src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Enums/FieldTypeDate.cs
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.UI/Ui/Enums/FieldTypeDate.cs
@@ -10,20 +10,6 @@ namespace Wonga.QA.Framework.UI.Ui.Enums
         Equal = 1,
         Past = 2,
         Future = 3,
-
-        // UK formats
-        //  dd Month yyyy
-        UkFullNow = 4,
-        UkFullRandom = 5,
-        // dd Month yyyy
-        UkLongNow = 6,
-        UkLongRandom = 7,
-        // dd-Mon-yyyy
-        UkMediumNow = 8,
-        UkMediumRandom = 9,
-        // dd/mm/yy
-        UkShortNow = 10,
-        UkShortRandom = 11,
-
+        Format = 4
     }
 }
