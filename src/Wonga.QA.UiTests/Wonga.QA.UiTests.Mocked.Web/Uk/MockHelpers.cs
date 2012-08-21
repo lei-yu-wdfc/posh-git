fcc49c1b2244cc0eb8634c3a2447c540946a6b68
Stanislav Desyatnikov <stasde@gmail.com>
2012-08-21 14:21:02 +0100

    UK-785: Created mocked My Summary tests

---
+++ src/Wonga.QA.UiTests/Wonga.QA.UiTests.Mocked.Web/Uk/MockHelpers.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using Wonga.QA.Framework.UI;
+using Wonga.QA.UiTests.Web;
+
+namespace Wonga.QA.UiTests.Mocked.Web.Uk
+{
+    public class MockHelpers: UiTest
+    {
+        /// <summary>
+        /// Selects a scenario in the Mock Settings menu
+        /// </summary>
+        /// <param name="scenarionName"></param>
+        public void SelectMockedScenario(string scenarionName)
+        {
+            IWebElement mockedScenariosMenu = Client.Driver.FindElement(By.Id("staticmock-trigger"));
+            mockedScenariosMenu.Click();
+            IWebElement mockedScenariosDropBox = Client.Driver.FindElement(By.Id("edit-user-status"));
+            mockedScenariosDropBox.SelectOption(scenarionName);
+            Client.Driver.FindElement(By.XPath("//input[@value='Save settings']")).Click();
+        }
+    }
+}
