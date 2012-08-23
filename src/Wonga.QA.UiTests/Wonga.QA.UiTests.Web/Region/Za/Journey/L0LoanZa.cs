db77bf8ad107138802ecbf4f74895e254cbdc449
Stanislav Desyatnikov <stasde@gmail.com>
2012-08-23 15:06:22 +0100

    ZA-2776: Created test for L0 Journey Self Employed

--- src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/Region/Za/Journey/L0LoanZa.cs
+++ src/Wonga.QA.UiTests/Wonga.QA.UiTests.Web/Region/Za/Journey/L0LoanZa.cs
@@ -1,4 +1,6 @@
-﻿using MbUnit.Framework;
+﻿using System;
+using MbUnit.Framework;
+using Wonga.QA.Framework.Api.Enums;
 using Wonga.QA.Framework.Core;
 using Wonga.QA.Framework.UI;
 using Wonga.QA.Framework.UI.UiElements.Pages.Common;
@@ -33,6 +35,47 @@ namespace Wonga.QA.UiTests.Web.Region.Za.Journey
                 .WithDeclineDecision();
             var declinedPage = journey.Teleport<DeclinedPage>() as DeclinedPage;
             declinedPage.LookForHeaderLinks();
+        }
+
+        [Test, AUT(AUT.Za), JIRA("ZA-2776"), Pending("Awaiting fix of ZA-2776"), Owner(Owner.StanDesyatnikov)]
+        public void ZaL0SelfEmployedMonthlyIncome()
+        {
+            var email = Get.RandomEmail();
+            var employerName = Get.EnumToString(RiskMask.TESTEmployedMask);
+            var dateOfBirth = new DateTime(1957, 10, 30);
+            var gender = GenderEnum.Female;
+            Console.WriteLine("Email: {0}\n EmployerName={1}", email, employerName);
+
+            var journey = JourneyFactory.GetL0Journey(Client.Home())
+                .WithEmployerName(employerName);
+            var applyPage = journey.Teleport<PersonalDetailsPage>() as PersonalDetailsPage;
+
+            var personalDetailsPage = new PersonalDetailsPage(Client);
+
+            personalDetailsPage.YourName.FirstName = Get.GetName();
+            personalDetailsPage.YourName.MiddleName = Get.GetMiddleName();
+            personalDetailsPage.YourName.LastName = Get.GetName();
+            personalDetailsPage.YourName.Title = "Mrs";
+            personalDetailsPage.YourDetails.Number = Get.GetNationalNumber(dateOfBirth, gender == GenderEnum.Female);
+            personalDetailsPage.YourDetails.DateOfBirth = dateOfBirth.ToString("d/MMM/yyyy");
+            personalDetailsPage.YourDetails.Gender = gender.ToString();
+            personalDetailsPage.YourDetails.HomeStatus = "Owner Occupier";
+            personalDetailsPage.YourDetails.HomeLanguage = "English";
+            personalDetailsPage.YourDetails.NumberOfDependants = "0";
+            personalDetailsPage.YourDetails.MaritalStatus = "Single";
+            personalDetailsPage.EmploymentDetails.EmploymentStatus = "Self Employed";
+            personalDetailsPage.EmploymentDetails.SelfEmployedMonthlyIncome= "3000";
+            personalDetailsPage.EmploymentDetails.WorkPhone = Get.GetPhone();
+            personalDetailsPage.EmploymentDetails.SalaryPaidToBank = true;
+            personalDetailsPage.EmploymentDetails.SelfNextPayDate = DateTime.Now.Add(TimeSpan.FromDays(5)).ToString("d/MMM/yyyy");
+            personalDetailsPage.EmploymentDetails.IncomeFrequency = "Monthly";
+            personalDetailsPage.ContactingYou.CellPhoneNumber = Get.GetMobilePhone();
+            personalDetailsPage.ContactingYou.EmailAddress = email;
+            personalDetailsPage.ContactingYou.ConfirmEmailAddress = email;
+            personalDetailsPage.PrivacyPolicy = true;
+            personalDetailsPage.CanContact = "Yes";
+            personalDetailsPage.MarriedInCommunityProperty ="I am not married in community of property (I am single, married with antenuptial contract, divorced etc.)";
+            var addressDetailsPage = personalDetailsPage.Submit() as AddressDetailsPage;
         }
        
     }
