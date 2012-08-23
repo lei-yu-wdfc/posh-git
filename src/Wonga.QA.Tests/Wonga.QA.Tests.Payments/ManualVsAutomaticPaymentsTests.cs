4702e78162023c88e70b9f8c6e3bc0d403e6692f
Shane McHugh <shane.mchugh@wonga.com>
2012-08-23 10:19:35 +0100

    Adding ManualVsAutomaticPayments Tests

---
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/ManualVsAutomaticPaymentsTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using MbUnit.Framework;
+using Wonga.QA.Framework;
+using Wonga.QA.Framework.Core;
+using Wonga.QA.Framework.Cs.Requests.Payments.Csapi.Commands;
+using Wonga.QA.Framework.Msmq.Enums.Integration.Payments.Enums;
+using Wonga.QA.Tests.Core;
+
+namespace Wonga.QA.Tests.Payments
+{
+	[TestFixture, Parallelizable((TestScope.All))]
+	public class ManualVsAutomaticPaymentsTests
+	{
+		private static readonly dynamic Transactions = Drive.Data.Payments.Db.Transactions;
+
+		[Test, AUT(AUT.Uk), JIRA("UKOPS-488"), Owner(Owner.ShaneMcHugh)]
+		public void ShouldCreateAutomaticTransactionWhenApplicationIsDueToday()
+		{
+			var customer = CustomerBuilder.New().Build();
+			var application = ApplicationBuilder.New(customer).Build();
+
+			application.MakeDueToday();
+
+			CheckDbForTransactionEntry(application, "Automatic Ping (Card)");
+		}
+
+		[Test, AUT(AUT.Uk), JIRA("UKOPS-488"), Owner(Owner.ShaneMcHugh)]
+		public void ShouldCreateManualTransactionWhenCsRepayWithPaymentCardCommandIsValid()
+		{
+			var customer = CustomerBuilder.New().Build();
+			var application = ApplicationBuilder.New(customer).Build();
+
+			Guid paymentId = Guid.NewGuid();
+			Drive.Cs.Commands.Post(new CsRepayWithPaymentCardCommand
+			{
+				AccountId = application.AccountId,
+				Amount = 100.00m,
+				SalesforceUser = "csUser",
+				CV2 = "111",
+				Currency = "GBP",
+				PaymentCardId = customer.GetPaymentCard(),
+				PaymentId = paymentId
+			});
+
+			CheckDbForTransactionEntry(application, "Payment card repayment from CS");
+		}
+
+		[Test, AUT(AUT.Uk), JIRA("UKOPS-488"), Owner(Owner.ShaneMcHugh)]
+		public void ShouldCreateManualTransactionWhenCsRepayWithExternalCardCommandIsValid()
+		{
+			var customer = CustomerBuilder.New().Build();
+			var application = ApplicationBuilder.New(customer).Build();
+
+			Guid paymentId = Guid.NewGuid();
+			Drive.Cs.Commands.Post(new CsRepayWithExternalCardCommand
+				{
+					AccountId = application.AccountId,
+					AddressLine1 = "line 1",
+					AddressLine2 = "line 2",
+					Amount = 100.00m,
+					CardNumber = "5411111111111111",
+					CardType = "visaDebit",
+					Country = "UK",
+					County = "county",
+					SalesforceUser = "csUser",
+					Currency = "GBP",
+					CV2 = "121",
+					ExpiryDate = new DateTime(DateTime.Now.Year + 1, 1, 31),
+					HolderName = "holder name",
+					PostCode = "12345",
+					Town = "town",
+					PaymentId = paymentId
+				});
+
+			CheckDbForTransactionEntry(application, "External payment card repayment from CS");
+		}
+
+		#region Helpers#
+
+		private void CheckDbForTransactionEntry(dynamic application, string reference)
+		{
+			int appId = ApplicationOperations.GetAppInternalId(application);
+			int scope = (int)PaymentTransactionScopeEnum.Credit;
+			string type = PaymentTransactionEnum.CardPayment.ToString();
+			Do.Until(() => Transactions.FindAllBy(Reference: reference, ApplicationId: appId, Scope: scope, Type: type, Amount: application.GetDueDateBalance()));
+		}
+
+		#endregion helpers#
+	}
+
+
+}
