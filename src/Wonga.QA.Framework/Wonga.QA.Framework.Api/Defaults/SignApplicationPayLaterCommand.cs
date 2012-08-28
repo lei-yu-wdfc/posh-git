a251271f3f2d23e8faae933bc9ba42b4ba81ba95
gearoid-wonga <gearoid.healy@wonga.com>
2012-08-28 18:02:54 +0100

    PAYLATER-902: use new PayLater Sign Application command in QAF +review

---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Defaults/SignApplicationPayLaterCommand.cs
@@ -0,0 +1,13 @@
+﻿using Wonga.QA.Framework.Core;
+
+namespace Wonga.QA.Framework.Api.Requests.Payments.PayLater.Commands.Uk
+{
+    public partial class SignApplicationPayLaterCommand
+    {
+        public override void Default()
+        {
+            AccountId = Get.GetId();
+            ApplicationId = Get.GetId();
+        }
+    }
+}
---
+++ src/Wonga.QA.Framework/Wonga.QA.Framework.Api/Requests/Payments/PayLater/Commands/Uk/SignApplicationPayLaterCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Wonga.QA.Framework.Api.Requests.Payments.PayLater.Commands.Uk
+{
+    [XmlRoot("SignPayLaterApplication")]
+    public partial class SignApplicationPayLaterCommand : ApiRequest<SignApplicationPayLaterCommand>
+    {
+        public Object AccountId { get; set; }
+        public Object ApplicationId { get; set; }
+    }
+}
