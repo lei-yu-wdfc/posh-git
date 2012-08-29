3ef0cac8a1fc025c3f932121abc5bb2fe39da517
anil krishnamaneni <anil.krishnamanei@wonga.com>
2012-08-29 15:03:59 +0100

    UKOPS-899 test

--- src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/PaymentsCollectionSuppression/BankruptcyCollectionSuppressionTests.cs
+++ src/Wonga.QA.Tests/Wonga.QA.Tests.Payments/PaymentsCollectionSuppression/BankruptcyCollectionSuppressionTests.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using MbUnit.Framework;
 using Wonga.QA.Framework;
-using Wonga.QA.Framework.Api;
-using Wonga.QA.Framework.Api.Enums;
 using Wonga.QA.Framework.Core;
-using Wonga.QA.Framework.Cs.Requests.Payments.Csapi.Commands;
 using Wonga.QA.Tests.Core;
 
 namespace Wonga.QA.Tests.Payments.PaymentsCollectionSuppression
@@ -13,75 +10,65 @@ namespace Wonga.QA.Tests.Payments.PaymentsCollectionSuppression
 	[Parallelizable(TestScope.All)]
 	[AUT(AUT.Uk)]
     [Description("Verifies that when an application has been paid off the status history will have PaidInFull as the current status.")]
-    [Pending("SF tests are failing because message congestion in SF TC queue, explicit until fixed")]
-	public class BankruptcyCollectionSuppressionTests
+    public class BankruptcyCollectionSuppressionTests
 	{
+
+		private const decimal Amount = 100.00m;
+	   
 		[Test]
-		public void PaymentsCollectionsShouldBeSuppressed_WhenCustomerIsInBankruptcyProvedState()
+		[AUT(AUT.Uk), JIRA("UKOPS-899"), Owner(Owner.AnilKrishnamaneni)]
+		public void LiveApplicationBankrupt()
+		{
+			var caseId = Guid.NewGuid();
+			var liveCustomer = CustomerBuilder.New().Build();
+			var liveApplication = ApplicationBuilder.New(liveCustomer).Build();
+			var liveApplicationInternalID = ApplicationOperations.GetAppInternalId(liveApplication);
+			var paymentCard = liveCustomer.GetPaymentCard();
+			ApplicationOperations.ReportBankrupt(liveApplication, caseId);
+			PaymentOperations.TakePayment(liveApplication.Id, paymentCard, Amount);
+			PaymentOperations.CheckPaymentsSupressionTransaction(liveApplicationInternalID, Amount);
+		}
+		
+		[Test]
+		[AUT(AUT.Uk), JIRA("UKOPS-899"), Owner(Owner.AnilKrishnamaneni)]
+		public void DueTodayApplicationBankrupt()
 		{
 			var caseId = Guid.NewGuid();
 			var customer = CustomerBuilder.New().Build();
 			var application = ApplicationBuilder.New(customer).Build();
-
-			var command = new CsReportBankruptcyCommand
-			              	{CaseId = caseId, AccountId = customer.Id, ApplicationId = application.Id};
-
-			Drive.Cs.Commands.Post(command);
-
-			dynamic app = null;
-
-			dynamic applicationsTable = Drive.Data.Payments.Db.Applications;
-            Do.Until(() => app = applicationsTable.FindBy(ExternalId: application.Id));
-
-			dynamic paymentCollectionSuppressions = Drive.Data.Payments.Db.PaymentCollectionSuppressions;
-			Do.Until(() => paymentCollectionSuppressions.FindBy(ApplicationId: app.ApplicationId, 
-																BankruptcySuppression: true,
-																FraudSuppression: false,
-																DCASuppression: false,
-																DMPRepaymentArrangementSuppression: false,
-																HardshipSuppression: false,
-																RepaymentArrangementSuppression: false,
-																ComplaintSuppression: false,
-																ClearMyBalanceSuppression: false,
-																DebtSoldSuppression: false));
+			var applicationId = ApplicationOperations.GetAppInternalId(application);
+			ApplicationOperations.ReportBankrupt(application, caseId);
+			application.MakeDueToday();
+			PaymentOperations.CheckPaymentsSupressionTransaction(applicationId, Amount);
 		}
 
-		[Test, AUT(AUT.Uk)]
-		public void ShouldNotTakeManualPayment_WhenPaymentCollectionsAreSuppressedDueToBankruptcy()
+		[Test]
+		[AUT(AUT.Uk), JIRA("UKOPS-899"), Owner(Owner.AnilKrishnamaneni)]
+		public void ArrearApplicationBankrupt()
 		{
+			var caseId = Guid.NewGuid();
 			var customer = CustomerBuilder.New().Build();
-			var application = ApplicationBuilder.New(customer).WithExpectedDecision(ApplicationDecisionStatus.Accepted).Build().PutIntoArrears();
+			var application = ApplicationBuilder.New(customer).Build();
+			var applicationId = ApplicationOperations.GetAppInternalId(application);
+			var paymentCard = customer.GetPaymentCard();
+			application.PutIntoArrears();
+			ApplicationOperations.ReportBankrupt(application, caseId);
+			PaymentOperations.TakePayment(application.Id, paymentCard, Amount);
+			PaymentOperations.CheckPaymentsSupressionTransaction(applicationId, Amount);
+		}
 
-			var applicationId = Drive.Data.Payments.Db.Applications.FindByExternalId(application.Id).ApplicationId;
-
-			Drive.Cs.Commands.Post(new CsReportBankruptcyCommand
-			{
-				AccountId = customer.Id,
-				ApplicationId = application.Id,
-				CaseId = Guid.NewGuid()
-			});
-
-			dynamic app = null;
-			dynamic applicationsTable = Drive.Data.Payments.Db.Applications;
-			Do.Until(() => app = applicationsTable.FindBy(ExternalId: application.Id));
-
-			dynamic paymentsSuppressionsTable = Drive.Data.Payments.Db.PaymentCollectionSuppressions;
-			Do.Until(() => paymentsSuppressionsTable.FindBy(ApplicationId: app.ApplicationId, BankruptcySuppression: true));
-
-			Drive.Cs.Commands.Post(new TakePaymentManualCommand
-			{
-				Amount = 100.00m,
-				ApplicationId = application.Id,
-				Currency = CurrencyCodeEnum.GBP,
-				PaymentCardId = customer.GetPaymentCard(),
-				PaymentId = Guid.NewGuid(),
-				SalesforceUser = "test.user@wonga.com"
-			});
-
-			var paymentRequests = Drive.Data.Payments.Db.PaymentCardRepaymentRequests;
-
-			Do.Until(() => paymentRequests.FindAll(paymentRequests.ApplicationId == applicationId &&
-												   paymentRequests.StatusDescription == "PaymentCollectionsSuppressed"));
+		[Test]
+		[AUT(AUT.Uk), JIRA("UKOPS-899"), Owner(Owner.AnilKrishnamaneni)]
+		public void RAApplicationBankrupt()
+		{
+			var caseId = Guid.NewGuid();
+			var customer = CustomerBuilder.New().Build();
+			var application = ApplicationBuilder.New(customer).Build();
+			var applicationId = ApplicationOperations.GetAppInternalId(application);
+			application.CreateRepaymentArrangement();
+			ApplicationOperations.ReportBankrupt(application, caseId);
+			PaymentOperations.TakePayment(application.Id, customer.GetPaymentCard(), Amount);
+			PaymentOperations.CheckPaymentsSupressionTransaction(applicationId, Amount);
 		}
 	}
 }
